14425c33783699ee76aacb17108d6644b313f4f3
metindokmen <kindmemento@gmail.com>
2023-09-15 00:10:04 +0300

    explain abstraction with an example in Go using interfaces

--- abstraction/car.go
+++ abstraction/car.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Let's look at abstraction.
 // Abstraction is a concept that allows for hiding the complexity and exposing only what's necessary.
 // A real-life example is a car.
@@ -9,7 +11,55 @@ package main
 
 // The car's interface (pedals, steering wheel, keys) is what you, the driver, interacts with.
 
-type Car struct {
+// We have a Car interface which contains three methods that we expect every instance of a car to possess: Start, Drive, and Stop.
+type Car interface {
+	Start()
+	Drive()
+	Stop()
+}
+
+type Sedan struct {
 	make  string
 	model string
+}
+
+func (s Sedan) Start() {
+	fmt.Println("Sedan is starting")
+}
+
+func (s Sedan) Drive() {
+	fmt.Println("Sedan is driving")
+}
+
+func (s Sedan) Stop() {
+	fmt.Println("Sedan is stopping")
+}
+
+type SUV struct {
+	make  string
+	model string
+}
+
+func (s SUV) Start() {
+	fmt.Println("SUV is starting")
+}
+
+func (s SUV) Drive() {
+	fmt.Println("SUV is driving")
+}
+
+func (s SUV) Stop() {
+	fmt.Println("SUV is stopping")
+}
+
+func main() {
+	var car1 Car = Sedan{make: "Honda", model: "Accord"}
+	car1.Start()
+	car1.Drive()
+	car1.Stop()
+
+	var car2 Car = SUV{make: "Ford", model: "Explorer"}
+	car2.Start()
+	car2.Drive()
+	car2.Stop()
 }
