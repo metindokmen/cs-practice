7b06135cbb962de76d46e69ceaf9bedddb540737
metindokmen <kindmemento@gmail.com>
2023-09-16 23:41:42 +0300

    explain key properties of a binary_tree in comments

---
+++ data_structures/binary_tree.kt
@@ -0,0 +1,9 @@
+// A binary tree is a data structure that consists of nodes where each node has at most two children: left and right.
+// The topmost node is called the root.
+
+// Key properties:
+// 1. Full Binary Tree: Every node has exactly 0 or 2 children.
+// 2. Complete Binary Tree: All levels, except possibly the last, are completely filled, and all nodes are as left as possible.
+// 3. Balanced Binary Tree: The depth of the two subtress of every node never differ by more than 1.
+// 4. Binary Search Tree (BST): For each node, all elements in the left subtree are less than the node, and all elements in the right subtree are greater than the node.
+
