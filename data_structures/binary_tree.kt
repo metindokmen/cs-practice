2ec4464101771706c1d1759243be8e31a799f98b
metindokmen <kindmemento@gmail.com>
2023-09-16 23:43:45 +0300

    give a real-life example using an organization

--- data_structures/binary_tree.kt
+++ data_structures/binary_tree.kt
@@ -7,3 +7,6 @@
 // 3. Balanced Binary Tree: The depth of the two subtress of every node never differ by more than 1.
 // 4. Binary Search Tree (BST): For each node, all elements in the left subtree are less than the node, and all elements in the right subtree are greater than the node.
 
+// Think of the hierarchy in an organization. The CEO is at the toop (root node). The CEO has two direct reports (two children): the CTO and the CFO.
+// The CTO might have two reports: VP of Engineering and VP of Product.
+// This can be visualized as a binary tree, where each person is a node, and their direct reports are their children.
