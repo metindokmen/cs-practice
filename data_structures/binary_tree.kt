61f5336cc7dbff23a9ae6c4a026c13e3b6bfbb3e
metindokmen <kindmemento@gmail.com>
2023-09-17 23:39:23 +0300

    add an insert function

--- data_structures/binary_tree.kt
+++ data_structures/binary_tree.kt
@@ -6,3 +6,15 @@
 // When each topic is limited to have only two subtopics for simplicity's sake, this structure resembles a binary tree.
 
 data class TreeNode<T>(var value: T, var left: TreeNode<T>? null, var right: TreeNode<T>? = null)
+
+fun <T> insert(root: TreeNode<T>?, value: T): TreeNode<T> {
+    if (root == null) return TreeNode(value)
+    if (root.left == null) [
+        root.left = TreeNode(value)
+    ] else if (root.right == null) {
+        root.right = TreeNode(value)
+    } else {
+        insert(root.left, value)
+    }
+    return root
+}
