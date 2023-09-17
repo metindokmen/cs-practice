792b8e9bfd844ebd11645a3602dbeb26983ce7d7
metindokmen <kindmemento@gmail.com>
2023-09-17 23:36:45 +0300

    change the example for a binary tree

--- data_structures/binary_tree.kt
+++ data_structures/binary_tree.kt
@@ -1,12 +1,6 @@
-// A binary tree is a data structure that consists of nodes where each node has at most two children: left and right.
-// The topmost node is called the root.
+// A binary tree is a hierarchical data structure where each node has at most two children, referred to as the left and right child.
 
-// Key properties:
-// 1. Full Binary Tree: Every node has exactly 0 or 2 children.
-// 2. Complete Binary Tree: All levels, except possibly the last, are completely filled, and all nodes are as left as possible.
-// 3. Balanced Binary Tree: The depth of the two subtress of every node never differ by more than 1.
-// 4. Binary Search Tree (BST): For each node, all elements in the left subtree are less than the node, and all elements in the right subtree are greater than the node.
-
-// Think of the hierarchy in an organization. The CEO is at the toop (root node). The CEO has two direct reports (two children): the CTO and the CFO.
-// The CTO might have two reports: VP of Engineering and VP of Product.
-// This can be visualized as a binary tree, where each person is a node, and their direct reports are their children.
+// Think of a book and its table of contents.
+// The main topics like "Introduction", "Chapter 1", "Chapter 2" can be seen as root or intermediate nodes.
+// Subtopics or sections within chapters can be seen as children.
+// When each topic is limited to have only two subtopics for simplicity's sake, this structure resembles a binary tree.
