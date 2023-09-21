b2057fb0b9d685f687f15dba7d975e2bb0939a9e
metindokmen <kindmemento@gmail.com>
2023-09-21 23:16:03 +0300

    explain heap data structure in comments

---
+++ data_structures/heap.kt
@@ -0,0 +1,8 @@
+// A heap is a specialized tree-based data structure that satisfies the heap property.
+
+// There are two types of heap:
+// 1. Max-Heap: For any given node I, the value of I is greater than or equal to the values of its children.
+// 2. Min-Heap: For any given node i, the value of I is less than or equal to the values of its children.
+
+// Think of a priority queue system in a hospital. Patients are assigned priority numbers, and the one with the highest priority (like critical condition) is treated first.
+// This system can be implemented using a max-heap where the patient with the maximum priority is always at the top.
