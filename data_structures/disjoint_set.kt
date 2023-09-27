b60b49572462a701b121d768f2244ccc177a974e
metindokmen <kindmemento@gmail.com>
2023-09-27 23:32:03 +0300

    explain disjoint sets in comments

---
+++ data_structures/disjoint_set.kt
@@ -0,0 +1,12 @@
+// A disjoint set is a data structure that keeps track of a partition of a set into disjoint subsets.
+// It supports two main operations:
+
+// 1. Union: Combine two subsets into a single subset.
+// 2. Find: Determine which subset a particular element is in.
+
+// Think of a social network platform where users can form groups.
+// Every user starts as their own group.
+// As users decide to merge their groups into larger communities, we need to be able to:
+// a) combine two groups.
+// b) check if two users are part of the same group.
+
