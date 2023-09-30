50c087794c952af7fde7e811f31cba61710f23b5
metindokmen <kindmemento@gmail.com>
2023-09-30 23:44:48 +0300

    give a real life example where circular buffer would be of use

--- data_structures/circular_buffer.kt
+++ data_structures/circular_buffer.kt
@@ -2,3 +2,8 @@
 // It behaves as if the memory is connected end-to-end, forming a circle.
 // When the buffer is full and a subsequent write occurs, the oldest data in the circular buffer is overwritten with the newest data.
 // It's useful in scenarios where old data has not significance after a certain point.
+
+// Think of a music player that has a feature to replay the last 10 seconds of a song.
+// The player doesn't need to store the entire song in memory, just the last 10 seconds.
+// A circular buffer can be used to constantly store the last 10 seconds of audio data.
+// When the buffer is full, the oldest data is overwritten with the newest chunk of audio.
