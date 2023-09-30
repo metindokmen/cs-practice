210bcb0e1603d7bbc2a2375a242702dd4c67a70d
metindokmen <kindmemento@gmail.com>
2023-09-30 23:48:06 +0300

    implement circular buffer class with private methods write, read, isFull && isEmpty

--- data_structures/circular_buffer.kt
+++ data_structures/circular_buffer.kt
@@ -7,3 +7,32 @@
 // The player doesn't need to store the entire song in memory, just the last 10 seconds.
 // A circular buffer can be used to constantly store the last 10 seconds of audio data.
 // When the buffer is full, the oldest data is overwritten with the newest chunk of audio.
+
+class CircularBuffer<T>(private val size: Int) {
+    private val buffer: Array<Any?> = arrayOfNulls(size)
+    private var start = 0
+    private var end = 0
+
+    fun write(value: T) {
+        if (end == start && buffer[end] != null) {
+            // Buffer is full, increment start to overwrite oldest data
+            start = (start + 1) % size
+        }
+        buffer[end] = value
+        end = (end + 1) % size
+    }
+
+    fun read(): T? {
+        if (start == end && buffer[start] == null) {
+            // Buffer is empty
+            return null
+        }
+        val value = buffer[start]
+        buffer[start] = null
+        start = (start + 1) % size
+        return value as T?
+    }
+
+    fun isFull() = (end == start && buffer[end] != null)
+    fun isEmpty() = (end == start && buffer[start] == null)
+}
