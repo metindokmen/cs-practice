277e75586e0dea062c118404b3a5b22b3a2deea6
metindokmen <kindmemento@gmail.com>
2023-09-30 23:43:39 +0300

    explain the data structure circular buffer in comments

---
+++ data_structures/circular_buffer.kt
@@ -0,0 +1,4 @@
+// A Circular Buffer is a fixed-size data structure that uses a single, continuous block of memory.
+// It behaves as if the memory is connected end-to-end, forming a circle.
+// When the buffer is full and a subsequent write occurs, the oldest data in the circular buffer is overwritten with the newest data.
+// It's useful in scenarios where old data has not significance after a certain point.
