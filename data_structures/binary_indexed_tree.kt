45d9a903ecd7140eab9a4d6cf02c2a20b84029a6
metindokmen <kindmemento@gmail.com>
2023-10-07 22:23:48 +0300

    explain binary indexed trees in comments

---
+++ data_structures/binary_indexed_tree.kt
@@ -0,0 +1,11 @@
+// A Binary Indexed Tree (also known as a Fenwick Tree) is a data structure providing efficient methods for calculation and manipulation of the prefix sums (cumulative sums) of a list of values.
+// Its primary uses are in computational geometry and for solving problems that require updating elements and querying range-sums.
+
+// Imagine you're managing a bookstore, and you're tracking the number of books sold each day.
+// At the end of the month, you might want to know:
+
+// 1. The total number of books sold between the 10th and 20th day.
+// 2. Update the sales for a particular day.
+
+// A Binary Indexed Tree can efficiently handle these operations.
+
