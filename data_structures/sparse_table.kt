a858ad15cbd52acb14e02d19833674b8d403a5b3
metindokmen <kindmemento@gmail.com>
2023-10-08 20:06:37 +0300

    explain sparse table in comments

---
+++ data_structures/sparse_table.kt
@@ -0,0 +1,5 @@
+// A Sparse Table is a data structure used primarily for range query problems, especially when the array is static (no updates).
+// Once the table is built, it allows for constant-time range queries, making it useful for problems like finding the minimum or maximum value in a given subarray.
+
+// Imagine you're analyzing stock prices over several days, and you want to quickly determine the hgihest price in any given range of days.
+// Building a Sparse Table on the stock prices can allow you to query the maximum price in any range of days in constant time.
