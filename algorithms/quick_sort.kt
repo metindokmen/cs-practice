2193a68bf9db3446055f8e25640b1386c31c1e25
metindokmen <kindmemento@gmail.com>
2023-10-10 21:11:34 +0300

    explain quickSort algo in comments

---
+++ algorithms/quick_sort.kt
@@ -0,0 +1,6 @@
+// QuickSort is an efficient, in-place, comparison-based sorting algorithm.
+// It works on the divide-and-conquer principle, where the main idea is to select a 'pivot' element,
+// and partition the array such that all elements smaller than the pivot are to its left,
+// and all elements greater than the pivot are to its right.
+// This process is recursively applied to the two sub-arrays, leading to a sorted list.
+
