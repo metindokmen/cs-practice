0d1d5be6899ba7968d37f2571ee6e8e9aa294d75
metindokmen <kindmemento@gmail.com>
2023-10-11 19:18:00 +0300

    explain dijkstra algo in comments

---
+++ algorithms/dijkstra.kt
@@ -0,0 +1,2 @@
+// Dijkstra's Algorithm is a graph search algorithm that solves the single-source shortest path problem for a graph with non-negative edge weights, producing the shortest path tree.
+// This algorithm is often used in routing and as a subroutine in other graph algorithms.
