9fb71f923acb855e9f27b9b89ee60f956e3a20ce
metindokmen <kindmemento@gmail.com>
2023-10-11 19:19:32 +0300

    give real life example where dijkstra algo is an optimal solution

--- algorithms/dijkstra.kt
+++ algorithms/dijkstra.kt
@@ -1,2 +1,6 @@
 // Dijkstra's Algorithm is a graph search algorithm that solves the single-source shortest path problem for a graph with non-negative edge weights, producing the shortest path tree.
 // This algorithm is often used in routing and as a subroutine in other graph algorithms.
+
+// Imagina you're using a GPS navigation system to find the shortest path from your home to your office in a city.
+// The city's road network can be represented as a graph where intersections are nodes, roads are edges, and the time or distance to travel each road segment is the edge weight.
+// Dijkstra's Algorithm helps the GPS find the shortest path (in terms of time or distance) from your home to your office.
