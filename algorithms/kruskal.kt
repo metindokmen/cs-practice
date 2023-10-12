921fe373c1f5f684ef2132e32da22fede511af6c
metindokmen <kindmemento@gmail.com>
2023-10-12 17:51:07 +0300

    add real life example as a use case

--- algorithms/kruskal.kt
+++ algorithms/kruskal.kt
@@ -1,3 +1,6 @@
 // Kruskal's Algorithm is a greedy algorithm that finds the minimum spanning tree for a connected weighted graph,
 // meaning it finds a subset of the edges that forms a tree that includes every vertex, where the total weight of all the edges in the tree is minimized.
 
+// Imagine a group of islands, and you're tasked with building bridges to connect all of them.
+// Bridges have different costs based on the distance and difficulty of construction.
+// Using Kruskal's Algorithm, you can find the most cost-effective way to connect all the islands without forming any cycles (loops).
