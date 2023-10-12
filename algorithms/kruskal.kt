206607836f232c65994eddaf42cac0f4d0a0bc08
metindokmen <kindmemento@gmail.com>
2023-10-12 17:49:52 +0300

    add definition for kruskal algo

---
+++ algorithms/kruskal.kt
@@ -0,0 +1,3 @@
+// Kruskal's Algorithm is a greedy algorithm that finds the minimum spanning tree for a connected weighted graph,
+// meaning it finds a subset of the edges that forms a tree that includes every vertex, where the total weight of all the edges in the tree is minimized.
+
