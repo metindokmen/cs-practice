6ca3b8ddf9433e21db6f8292b4d9b77837947ffc
metindokmen <kindmemento@gmail.com>
2023-10-13 13:55:34 +0300

    explain knapsack problem

---
+++ algorithms/knapsack.kt
@@ -0,0 +1,3 @@
+// The knapsack problem is an optimization problem where you have a set of items, each with a weight and a value.
+// You need to determine the number of each item to include in a knapsack so that the total weight doesn't exceed a given limit while maximizing the total value.
+
