0e85c807fcffa8b767ebe0be0103d50288b15829
metindokmen <kindmemento@gmail.com>
2023-10-13 13:56:54 +0300

    give real life example as a use case for knapsack

--- algorithms/knapsack.kt
+++ algorithms/knapsack.kt
@@ -1,3 +1,6 @@
 // The knapsack problem is an optimization problem where you have a set of items, each with a weight and a value.
 // You need to determine the number of each item to include in a knapsack so that the total weight doesn't exceed a given limit while maximizing the total value.
 
+// Imagine you're packing for a hiking trip and can only carry a limited amount of weight in your backpack.
+// You have several items, each with a specific weight and benefit (e.g. food provides energy, a tent provides shelter etc.).
+// You want to maximize the benefit you'll get from the items you bring without overloading your backpack.
