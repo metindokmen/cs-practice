43ae84c481923964ead3b627217d54887f834f35
metindokmen <kindmemento@gmail.com>
2023-10-14 20:34:18 +0300

    explain a start algorithm in comments

---
+++ algorithms/a_start.kt
@@ -0,0 +1,2 @@
+// A* is a popular pathfinding and graph traversal algorithm that is used to find the shortest path from starting point to a goal.
+// It uses a combination of the actual distance from the start and an estimate (heuristic) of the distance to the goal and prioritize nodes.
