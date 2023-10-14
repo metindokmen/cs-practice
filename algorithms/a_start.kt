84e80b78451498d2bbdf598cf4930292b05cb174
metindokmen <kindmemento@gmail.com>
2023-10-14 20:35:13 +0300

    add real life example for a star algo

--- algorithms/a_start.kt
+++ algorithms/a_start.kt
@@ -1,2 +1,5 @@
 // A* is a popular pathfinding and graph traversal algorithm that is used to find the shortest path from starting point to a goal.
 // It uses a combination of the actual distance from the start and an estimate (heuristic) of the distance to the goal and prioritize nodes.
+
+// Consider a GPS navigation system that helps you find the shortest route from your current location to a destination.
+// While there might be many possible routes, A* helps identify the most efficient one, taking into account the actual distance traveled and an estimate of the remaining distance.
