d7a6c4ea840b74e23a41e31b5633618456549d5e
metindokmen <kindmemento@gmail.com>
2023-10-15 19:11:05 +0300

    explain kmp in comments

---
+++ algorithms/knuth_morris_pratt.kt
@@ -0,0 +1,3 @@
+// KMP (Knuth-Morris-Pratt) Algorithm is an efficient string searching (or substring searching) algorithm that processes the pattern itself to determine how much of the comparison can be skipped.
+// It pre-processes the pattern to create a `lps` (Longest Prefix which is also Suffix) array that helps in skipping characters while matching.
+
