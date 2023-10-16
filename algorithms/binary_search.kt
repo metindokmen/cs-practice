3ceed6540746b0a800c1530cf879f016976e8bea
metindokmen <kindmemento@gmail.com>
2023-10-16 15:25:16 +0300

    implement binarySearch function

--- algorithms/binary_search.kt
+++ algorithms/binary_search.kt
@@ -6,3 +6,19 @@
 // you'd typically open the dictionary to a middle page and see if your word would lie before or after that page.
 // You then split the relevant half into another half, and continue until you find your word.
 // This method is faster than a linear search through every page, and is essentially a real-life binary search.
+
+fun binarySearch(arr: IntArray, target: Int): Int {
+    var left = 0
+    var right = arr.size - 1
+
+    while (left <= right) {
+        val mid = left + (right - left) / 2
+
+        when {
+            arr[mid] == target -> return mid
+            arr[mid] < target -> left = mid + 1
+            else -> right = mid - 1
+        }
+    }
+    return -1
+}
