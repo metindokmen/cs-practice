601be0792b1d168383de2bd3418748f216f813b8
metindokmen <kindmemento@gmail.com>
2023-10-16 15:24:07 +0300

    give real life example of a binary search algorithm use case

--- algorithms/binary_search.kt
+++ algorithms/binary_search.kt
@@ -1,3 +1,8 @@
 // Binary Search is a searching algorithm that finds the position of a target value within a sorted array or list.
 // It compares the garget value to the middle element of the array and eliminates half of the remaining search interval based on that comparison.
 // The process continues until the value is found or the interval is empty.
+
+// Imagine you're looking up a word in a dictionary. Instead of going through each word from the beginning to the end,
+// you'd typically open the dictionary to a middle page and see if your word would lie before or after that page.
+// You then split the relevant half into another half, and continue until you find your word.
+// This method is faster than a linear search through every page, and is essentially a real-life binary search.
