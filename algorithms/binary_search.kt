d955a85407af896e3fbe0b455fc2afa3d0701000
metindokmen <kindmemento@gmail.com>
2023-10-16 15:22:51 +0300

    explain binary search algorithm in comments

---
+++ algorithms/binary_search.kt
@@ -0,0 +1,3 @@
+// Binary Search is a searching algorithm that finds the position of a target value within a sorted array or list.
+// It compares the garget value to the middle element of the array and eliminates half of the remaining search interval based on that comparison.
+// The process continues until the value is found or the interval is empty.
