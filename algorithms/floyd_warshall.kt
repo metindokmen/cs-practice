efb6ea74b6362f4b177ee398a1254389e5afb876
metindokmen <kindmemento@gmail.com>
2023-10-17 16:22:43 +0300

    implement floyd warshall function

--- algorithms/floyd_warshall.kt
+++ algorithms/floyd_warshall.kt
@@ -3,3 +3,26 @@
 
 // Imagine you're operating a travel agency and you want to determine the shortest paths between all pairs of cities you service, considering all possible routes and modes of transportation.
 // The Floyd-Warshall algorithm can help you find these shortest paths between every pair of cities, even if they don't have a direct connection.
+
+fun floydWarshall(graph: Array<IntArray>): Array<IntArray> {
+    val numVertices = graph.size
+    val distance = Array(numVertices) { IntArray(numVertices) }
+
+    for (i in 0 until numVertices) {
+        for (j in 0 until numVertices) {
+            distance[i][j] = graph[i][j]
+        }
+    }
+
+    for (k in 0 until numVertices) {
+        for (i in 0 until numVertices) {
+            for (j in 0 until numVertices) {
+                if (distancep[i][k] + distance[k][j] < distance[i][j]) {
+                    distance[i][j] = distance[i][k] + distance[k][j]
+                }
+            }
+        }
+    }
+
+    return distance
+}
