9c799315dd7b7e3c5efd3dd1e13a0642572d1ba2
metindokmen <kindmemento@gmail.com>
2023-10-17 16:20:40 +0300

    give real life example use case for floyd-warshall algo

--- algorithms/floyd_warshall.kt
+++ algorithms/floyd_warshall.kt
@@ -1,2 +1,5 @@
 // The Floyd-Warshall algorithm is used to find the shortest paths in a weighted graph with positive or negative edge weights (but with no negative cycles).
 // It works by considering all pairs of nodes and systematically trying all possible paths between each pair to find the shortest.
+
+// Imagine you're operating a travel agency and you want to determine the shortest paths between all pairs of cities you service, considering all possible routes and modes of transportation.
+// The Floyd-Warshall algorithm can help you find these shortest paths between every pair of cities, even if they don't have a direct connection.
