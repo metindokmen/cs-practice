f5cedbdc21ac962b7d9459ab1539435156124cec
metindokmen <kindmemento@gmail.com>
2023-10-17 16:19:21 +0300

    explain floyd-warshall algo in comments

---
+++ algorithms/floyd_warshall.kt
@@ -0,0 +1,2 @@
+// The Floyd-Warshall algorithm is used to find the shortest paths in a weighted graph with positive or negative edge weights (but with no negative cycles).
+// It works by considering all pairs of nodes and systematically trying all possible paths between each pair to find the shortest.
