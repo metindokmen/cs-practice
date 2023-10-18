0eef6e7929c88f192022ef10ad92bd06f48aa6ca
metindokmen <kindmemento@gmail.com>
2023-10-18 17:43:45 +0300

    implement mergeSort algorithm

--- algorithms/merge_sort.kt
+++ algorithms/merge_sort.kt
@@ -6,3 +6,17 @@
 // To do this, you'd compare the cards at the top of the stacks, pick the smaller one, and place it on a new stack.
 // You'd repeat this until one of your stacks is empty, then place the remaining cards from the other stack on top.
 // This is the essence of the merge step in the Merge Sort algorithm.
+
+fun mergeSort(list: MutableList<Int>): MutableList<Int> {
+    if (list.size <= 1) {
+        return list
+    }
+
+    val middleIndex = list.size / 2
+    val leftList = list.subList(0, middleIndex)
+    val rightList = list.subList(middleIndex, list.size)
+
+    return merge(mergeSort(leftList), mergeSort(rightList))
+}
+
+fun merge() {} // @TODO: Implement
