15bc5dd52d4e97f588dd73eb6598c9ae5ff71f4b
metindokmen <kindmemento@gmail.com>
2023-10-18 17:39:57 +0300

    explain merge sort algorithm in comments

---
+++ algorithms/merge_sort.kt
@@ -0,0 +1,2 @@
+// Merge Sort is a divide-and-conquer algorithm that splits a list in half, recursively sorts the halves, and then merges them back together.
+// The key insight is that a list of lenght 1 is always sorted, and merging two sorted lists is linear in the total number of elements.
