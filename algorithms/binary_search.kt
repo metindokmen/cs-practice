9849bfa52b76621f1d4725e3ea6800577f23b336
metindokmen <kindmemento@gmail.com>
2023-10-29 22:57:39 +0300

    fix typo

--- algorithms/binary_search.kt
+++ algorithms/binary_search.kt
@@ -1,5 +1,5 @@
 // Binary Search is a searching algorithm that finds the position of a target value within a sorted array or list.
-// It compares the garget value to the middle element of the array and eliminates half of the remaining search interval based on that comparison.
+// It compares the target value to the middle element of the array and eliminates half of the remaining search interval based on that comparison.
 // The process continues until the value is found or the interval is empty.
 
 // Imagine you're looking up a word in a dictionary. Instead of going through each word from the beginning to the end,
