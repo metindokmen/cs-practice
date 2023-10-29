218d69d2058dc4504bd4f02c7e09a3c4df97fbe4
metindokmen <kindmemento@gmail.com>
2023-10-29 23:06:48 +0300

    add main function

--- algorithms/breadth-first_search.kt
+++ algorithms/breadth-first_search.kt
@@ -42,3 +42,17 @@ class Graph(private val numVertices: Int) {
         }
     }
 }
+
+fun main() {
+    val graph = Graph(4)
+
+    graph.addEdge(0, 1)
+    graph.addEdge(0, 2)
+    graph.addEdge(1, 2)
+    graph.addEdge(2, 0)
+    graph.addEdge(2, 3)
+    graph.addEdge(3, 3)
+
+    println("Breadth First Traversal starting from vertex 2:")
+    graph.BFS(2)
+}
