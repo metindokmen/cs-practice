2d1c48990212d4d895ce1a4307def2d74556642a
metindokmen <kindmemento@gmail.com>
2023-10-29 23:02:12 +0300

    give a real life example

--- algorithms/breadth-first_search.kt
+++ algorithms/breadth-first_search.kt
@@ -1,3 +1,7 @@
 // Breadth-First Search (BFS) is an algorithm for traversing or searching tree or graph data structures.
 // It starts at the tree root (or an abtirary node of a graph) and explores the neighbor nodes at the current depth prior to moving on to nodes at the next depth level.
 
+// Imagine you're playing a game where you're in a large maze and you need to find the shortest path to the exit.
+// The maze can be visualized as a graph where each room is a node, and each door connecting two rooms is an edge.
+// By using BFS, starting from your current room, you'd first explore all the immediate neighboring rooms, then the neighbors of those neighbors, and so on, until you find the exit.
+// This ensures you find the shortest path to the exit.
