9275a15051b2eacbd82b29832a1d50ad8b1ce1f6
metindokmen <kindmemento@gmail.com>
2023-10-31 18:30:30 +0300

    give a real life example use case for dfs

--- algorithms/depth-first_search.kt
+++ algorithms/depth-first_search.kt
@@ -1,2 +1,6 @@
 // Depth-First Search (DFS) is an algorithm used to traverse or search tree or graph data structures.
 // The algorithm starts at the root node (or an arbitrary node for graphs) and explores as far as possible along each branch before backtracking.
+
+// Supporse you're exploring a cave network. You start at the entrance and at each junction, you choose a tunnel to explore.
+// You go as deep as you can, backtracking only when you reach a dead end.
+// In computer science, this approach of exhaustive exploration is askin to the Depth-First Search algorithm.
