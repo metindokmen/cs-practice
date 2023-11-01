1a0beaa965cc55823ea9fdb50970d4b1bb121bd0
metindokmen <kindmemento@gmail.com>
2023-11-01 23:24:42 +0300

    add real life example use case for bubble sort

--- algorithms/bubble_sort.kt
+++ algorithms/bubble_sort.kt
@@ -1,3 +1,6 @@
 // Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements,
 // and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
 
+// Imagine you have a deck of cards, and you want to arrange them in ascending order based on their values.
+// You can use the Bubble Sort technique: repeatedly step through the deck, compare each pair of adjacent cards, and swap them if they are in the wrong order.
+// This process is repeated until no swaps are needed, indicating that the deck is sorted.
