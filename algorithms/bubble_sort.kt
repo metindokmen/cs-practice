d13b35aa9129ec4a9b6f852937e020ddb9a67a25
metindokmen <kindmemento@gmail.com>
2023-11-01 23:26:25 +0300

    implement bubble sort algorithm

--- algorithms/bubble_sort.kt
+++ algorithms/bubble_sort.kt
@@ -4,3 +4,26 @@
 // Imagine you have a deck of cards, and you want to arrange them in ascending order based on their values.
 // You can use the Bubble Sort technique: repeatedly step through the deck, compare each pair of adjacent cards, and swap them if they are in the wrong order.
 // This process is repeated until no swaps are needed, indicating that the deck is sorted.
+
+fun bubbleSort(arr: IntArray): IntArray {
+    var n = arr.size
+    var swapped: Boolean
+
+    do {
+        swapped = false
+        for (i in 1 until n) {
+            if (arr[i - 1] > arr[i]) {
+                // Swap arr[i-1] and arr[i]
+                val temp = arr[i - 1]
+                arr[i - 1] = arr[i]
+                arr[i] = temp
+
+                // Mark that a swap occurred
+                swapped = true
+            }
+        }
+        n--
+    } while (swapped)
+
+    return arr
+}
