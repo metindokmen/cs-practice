d326a8c92226cf50e5481e1d00ef01f163edef7b
metindokmen <kindmemento@gmail.com>
2023-11-06 22:46:40 +0300

    give real life example use case for heap sort algorithm

--- algorithms/heap_sort.kt
+++ algorithms/heap_sort.kt
@@ -1,3 +1,8 @@
 // Heap Sort is a comparison-based sorting technique based on a Binary Heap data structure.
 // It's similar to selection sort where we first find the maximum element and place the maximum element at the end.
 // We repeat the same process for the remaining elements.
+
+// Consider organizing a large number of documents by their importance and due date.
+// A good approach would be to arrange them into a pile (heap) where the most urgent document is always on top.
+// After you complete each document, you rearrange the pile to put the next most urgent document on top.
+// This is similar to how Heap sort works with numbers.
