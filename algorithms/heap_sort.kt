aebac83b77f8a1959532f59719f6bef97847b206
metindokmen <kindmemento@gmail.com>
2023-11-06 22:45:30 +0300

    define heap sort algo

---
+++ algorithms/heap_sort.kt
@@ -0,0 +1,3 @@
+// Heap Sort is a comparison-based sorting technique based on a Binary Heap data structure.
+// It's similar to selection sort where we first find the maximum element and place the maximum element at the end.
+// We repeat the same process for the remaining elements.
