18ed3b84160b5128c11bf2083cef976f20821110
metindokmen <kindmemento@gmail.com>
2023-11-07 18:04:17 +0300

    add real life example use case for counting sort algo

--- algorithms/counting_sort.kt
+++ algorithms/counting_sort.kt
@@ -1,3 +1,8 @@
 // Counting Sort is an integer sorting algorithm that operates by counting the number of objects that have each distinct key value.
 // It works by calculating the number of elements less than each key to determine the position of each key directly in the output array.
 // It's efficient for sorting a small range of integers.
+
+// Imagine you're a teacher with a stack of tests scored from 0 to 100, and you want to sort them by score.
+// Instead of comparing tests directly, you can create a tally of each score
+// (for instance, a list where the index represents the score and the value at that index is the number of tests with that score).
+// Then, you can easily reconstruct a sorted list of tests from this tally.
