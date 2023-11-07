8fcbce5644264d0ed6c2591c431786b240e4218c
metindokmen <kindmemento@gmail.com>
2023-11-07 18:02:51 +0300

    add definition for counting sort algorithm

---
+++ algorithms/counting_sort.kt
@@ -0,0 +1,3 @@
+// Counting Sort is an integer sorting algorithm that operates by counting the number of objects that have each distinct key value.
+// It works by calculating the number of elements less than each key to determine the position of each key directly in the output array.
+// It's efficient for sorting a small range of integers.
