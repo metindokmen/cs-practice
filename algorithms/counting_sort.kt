4c1b89960ec076b68334fb49dc7ba39ba8aa86da
metindokmen <kindmemento@gmail.com>
2023-11-07 18:06:01 +0300

    implement countingSort algorithm

--- algorithms/counting_sort.kt
+++ algorithms/counting_sort.kt
@@ -6,3 +6,27 @@
 // Instead of comparing tests directly, you can create a tally of each score
 // (for instance, a list where the index represents the score and the value at that index is the number of tests with that score).
 // Then, you can easily reconstruct a sorted list of tests from this tally.
+
+fun countingSort(arr: IntArray, maxVal: Int): IntArray {
+    val n = arr.size
+    val count = IntArray(maxVal + 1)
+    val output = IntArray(n)
+
+    // Count each element
+    for (element in arr) {
+        count[element]++
+    }
+
+    // Calculate cumulative count
+    for (i in 1..maxVal) {
+        count[i] += count[i - 1]
+    }
+
+    // Place elements in sorted order
+    for (element in arr.reversed()) {
+        output[count[element] - 1] = element
+        count[element]--
+    }
+
+    return output
+}
