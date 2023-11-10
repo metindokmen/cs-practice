f05058ef296e9b40f6f5a5d874fdccee1b9fa7a8
metindokmen <kindmemento@gmail.com>
2023-11-10 20:40:34 +0300

    give real life example use case in comments

--- algorithms/insertion_sort.kt
+++ algorithms/insertion_sort.kt
@@ -1,3 +1,7 @@
 // Insertion Sort is a simple sorting algorithm that build the final sorted array (or list) one item at a time.
 // It is much less efficient on lage lists than more advanced algorithms like quicksort, heapsort, or merge sort.
 // However, it has several advantages, such as simple implementation, efficient for small data sets, and more efficent in practice than most other simple quadratic algorithms (like selection sort or bubble sort)
+
+// Imagine you're palying a card game where you need to keep your hand of cards sorted at all times.
+// As you draw each new card, you insert it into its proper place within your already sorted hand, shifting the other cards as neccessary.
+// This process is similar to how the Insertion Sort algorithm works.
