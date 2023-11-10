88fd73883a5d2633c9938ddcda2fddaf205f7300
metindokmen <kindmemento@gmail.com>
2023-11-10 20:32:35 +0300

    add real life example for radix sort algo

--- algorithms/radix_sort.kt
+++ algorithms/radix_sort.kt
@@ -1,2 +1,6 @@
 // Radix Sort is a non-comparative integer sorting algorithm that sorts data with integer keys by grouping the keys by individual digits that share the same significant position and value.
 // It does this for each digit, starting from the least significant digit to the most significant digit.
+
+// Imagine you're organizing a library with thousands of books by their unique 10-digit ISBN numbers.
+// Instead of comparing entire numbers, you can sort the books based on each digit, starting from the least significant digit (rightmost) and moving to the most significant digit.
+// This way, you're effectively grouping and sorting books at each step based on a part of their ISBN, leading to a fully sorted collection.
