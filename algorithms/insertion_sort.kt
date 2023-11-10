cb55d27ef4dd808bcc847fdfd4ac71ad64291a43
metindokmen <kindmemento@gmail.com>
2023-11-10 20:39:25 +0300

    explain insertion sort algorithm

---
+++ algorithms/insertion_sort.kt
@@ -0,0 +1,3 @@
+// Insertion Sort is a simple sorting algorithm that build the final sorted array (or list) one item at a time.
+// It is much less efficient on lage lists than more advanced algorithms like quicksort, heapsort, or merge sort.
+// However, it has several advantages, such as simple implementation, efficient for small data sets, and more efficent in practice than most other simple quadratic algorithms (like selection sort or bubble sort)
