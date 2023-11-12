09723a12831d6a9a25b4f7a3f35c36899a6bddad
metindokmen <kindmemento@gmail.com>
2023-11-12 14:39:40 +0300

    implement selection sort function

--- algorithms/selection_sort.kt
+++ algorithms/selection_sort.kt
@@ -8,3 +8,22 @@
 // You start by finding the lightest basket and swapping it with the basket in the first position.
 // Then you look for the next lighest among the remaining baskets, swapping it with the basked in the second position, and so on.
 // This process is akin to how Selection Sort arranges data.
+
+fun selectionSort(arr: IntArray) {
+    val n = arr.size
+
+    for (i in 0 until n - 1) {
+        var minIdx = i
+
+        for (j in i + 1 until n) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        val temp = arr[minIdx]
+        arr[minIdx] = arr[i]
+        arr[i] = temp
+    }
+}
