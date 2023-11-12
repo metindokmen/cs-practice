ddc151d52cb5dff34080f7bf4c21371738df3a54
metindokmen <kindmemento@gmail.com>
2023-11-12 14:37:14 +0300

    define and explain selection sort algorithm in comments

---
+++ algorithms/selection_sort.kt
@@ -0,0 +1,5 @@
+// Selection Sort is a simple comparison-based sorting algorithm.
+// It divides the input list into two parts: a sorted sublist of items which is built up from left to right at the front (left) of the list,
+// and a sublist of the remaining unsorted items that occupy the rest of the list.
+// The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist,
+// exchanging (swapping) it with the leftmost unsorted element, and moving the sublist boundaries one element to the right.
