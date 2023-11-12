24665277d2d0f977ad33819b6992ab9e3524e0c7
metindokmen <kindmemento@gmail.com>
2023-11-12 14:38:22 +0300

    give real life example for selection sort algo

--- algorithms/selection_sort.kt
+++ algorithms/selection_sort.kt
@@ -3,3 +3,8 @@
 // and a sublist of the remaining unsorted items that occupy the rest of the list.
 // The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist,
 // exchanging (swapping) it with the leftmost unsorted element, and moving the sublist boundaries one element to the right.
+
+// Imaigne you're at a fruit market trying to arrange a row of fruit baskets in order of increasing weight.
+// You start by finding the lightest basket and swapping it with the basket in the first position.
+// Then you look for the next lighest among the remaining baskets, swapping it with the basked in the second position, and so on.
+// This process is akin to how Selection Sort arranges data.
