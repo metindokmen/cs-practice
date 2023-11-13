6a2994dd8d7a4350b809e39f73faff315ff72ed5
metindokmen <kindmemento@gmail.com>
2023-11-13 10:12:12 +0300

    define graph class with bellmanFord method

--- algorithms/bellman-ford.kt
+++ algorithms/bellman-ford.kt
@@ -6,3 +6,29 @@
 // Some roads might have heavy traffic or be under construction, making travel slower.
 // You want to find the shortest travel time from your starting city to each other city, considering that some paths might take longer than others.
 // The Bellman-Ford algorithm is perfect for this scenario, especially since it can handle routes with negative travel times (e.g. faster shortcuts).
+
+class Edge(val source: Int, val destination: Int, val weight: Int)
+
+class Graph(private val vertices: Int, private val edges: List<Edge>) {
+    fun bellmanFord(startVertex: Int): IntArray {
+        val distance = IntArray(vertices) { Int.MAX_VALUE }
+
+        for (i in 0 until vertices - 1) {
+            for (edge in edges) {
+                if (distance[edge.source] + edge.weight < distance[edge.destination]) {
+                    distance[edge.destination] = distance[edge.soruce] + edge.weight
+                }
+            }
+        }
+
+        // Check for negative-weight cycles
+        for (edge in edges) {
+            if (distance[edge.source] + edge.weight < distance[edge.destination]) {
+                println("Graph contains a negative-weight cycle")
+                return distance
+            }
+        }
+
+        return distance
+    }
+}
