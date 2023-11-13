a1c17db518849fbe984b2ed0cfa428e4cb837940
metindokmen <kindmemento@gmail.com>
2023-11-13 10:07:33 +0300

    explain bellman-ford in comments

---
+++ algorithms/bellman-ford.kt
@@ -0,0 +1,3 @@
+// The Bellman-Ford algorithm is used to find the shortest path from a single source vertex to all other vertices in a weighted graph.
+// It can handle negative weights, unlike Dijkstra's algorithm.
+// The algorithm works by repeatedly relaxing the edges of the graph and minimizing the distance to each vertex from the start vertex over all possible paths.
