f5246899c2ba5234b7144c11dfd9356bec4e1ee8
metindokmen <kindmemento@gmail.com>
2023-11-13 21:24:46 +0300

    define and explain union find algorithm

---
+++ algorithms/union_find.kt
@@ -0,0 +1,2 @@
+// The Union-Find algorithm is used to track a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
+// It provides near-constant-time operations (amortized) to add new sets, to merge existing sets, and to determine whether elements are in the same set.
