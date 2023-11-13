13a96417f9a8c1b9b3d1e1b1e5d051ec9401cc04
metindokmen <kindmemento@gmail.com>
2023-11-13 21:26:12 +0300

    give real life use case for union find algorihtm

--- algorithms/union_find.kt
+++ algorithms/union_find.kt
@@ -1,2 +1,6 @@
 // The Union-Find algorithm is used to track a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
 // It provides near-constant-time operations (amortized) to add new sets, to merge existing sets, and to determine whether elements are in the same set.
+
+// Imagine you're managing a social network where users form groups or communities.
+// You need a way to quickly determine whether two users are in the same group and to merge different groups when users form connections across them.
+// The Union-Find algorithm efficiently handles these queries, making it ideal for scenarios like social networking, where group dynamics change frequently.
