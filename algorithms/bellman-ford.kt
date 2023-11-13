cb8f548d283fae7779eceb19dc7fb3f9c1b98dbd
metindokmen <kindmemento@gmail.com>
2023-11-13 10:09:29 +0300

    give real life example use case for bellman-ford

--- algorithms/bellman-ford.kt
+++ algorithms/bellman-ford.kt
@@ -1,3 +1,8 @@
 // The Bellman-Ford algorithm is used to find the shortest path from a single source vertex to all other vertices in a weighted graph.
 // It can handle negative weights, unlike Dijkstra's algorithm.
 // The algorithm works by repeatedly relaxing the edges of the graph and minimizing the distance to each vertex from the start vertex over all possible paths.
+
+// Imagine you are planning a road trip through several cities, and you're looking at a map with the time it takes to travel between each pair of cities.
+// Some roads might have heavy traffic or be under construction, making travel slower.
+// You want to find the shortest travel time from your starting city to each other city, considering that some paths might take longer than others.
+// The Bellman-Ford algorithm is perfect for this scenario, especially since it can handle routes with negative travel times (e.g. faster shortcuts).
