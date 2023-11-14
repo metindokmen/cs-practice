1f2626314254e2a457643e1fc18db45176ca4d20
metindokmen <kindmemento@gmail.com>
2023-11-14 22:05:46 +0300

    give real life example use case for boyer-moore algorithm

--- algorithms/boyer-moore.kt
+++ algorithms/boyer-moore.kt
@@ -2,3 +2,7 @@
 // It preprocesses the pattern to create two different heuristics - the bad character heuristic and the good suffix heuristic.
 // These heuristics allow the algorithm to skip over positions of the text where the pattern cannot possible match, leading to fewer comparisons than simpler algorithms,
 // especially when the pattern length is long.
+
+// Imagine you're going through a large book looking for a specific word or phrase.
+// Instead of reading every word from start to finish, you might skip ahead multiple characters at a time, especially if you know the word you're looking for is quite distinct.
+// The Boyer-Moore algorithm uses a similar idea to efficiently find a substring in a larger string by skipping sections of the text that cannot possibly contain the search string.
