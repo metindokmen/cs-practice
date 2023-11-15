05be6b039c1c0d24dd34698b0fdf1106af29192b
metindokmen <kindmemento@gmail.com>
2023-11-15 23:21:17 +0300

    give real life use case for lcs algorithm

--- algorithms/longest_commen_subsequence.kt
+++ algorithms/longest_commen_subsequence.kt
@@ -1,3 +1,7 @@
 // The LCS algorithm is used in computer science to find the longest subsequence common to all sequences in a set of sequences (often just wo sequences).
 // A subsequence is a sequence that appears in the same relative order, but not necessarily contiguously.
 // The algorithm is useful in many applications, such as diff tools, bioinformatics for DNA sequencing, and in naturel language processing.
+
+// Suppose you're working with two versions of a text document and you want to find the longest sequence of words (or characters) that appears in both versions.
+// This is a common problem in version control systems and text comparison tools.
+// The Longest Commong Subsequence algorithm can efficiently find this sequence.
