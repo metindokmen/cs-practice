86e1ad8c7093b7333d8e68dcb8c1a0716e1ebd9d
metindokmen <kindmemento@gmail.com>
2023-11-15 23:20:16 +0300

    define lcs

---
+++ algorithms/longest_commen_subsequence.kt
@@ -0,0 +1,3 @@
+// The LCS algorithm is used in computer science to find the longest subsequence common to all sequences in a set of sequences (often just wo sequences).
+// A subsequence is a sequence that appears in the same relative order, but not necessarily contiguously.
+// The algorithm is useful in many applications, such as diff tools, bioinformatics for DNA sequencing, and in naturel language processing.
