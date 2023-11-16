27f11593367c83707de9a79155fcda96055172b8
metindokmen <kindmemento@gmail.com>
2023-11-16 23:55:38 +0300

    explain sieve of eratosthenes algorithm in comments

---
+++ algorithms/sieve_of_eratosthenes.kt
@@ -0,0 +1,3 @@
+// The Sieve of Eratosthenes is an ancient algorithm used to find all prime numbers up to any given limit.
+// It does so by iteratively marking the multiples of each prime number starting from 2.
+// The numbers which remain unmarked at the end of the algorithm are prime numbers.
