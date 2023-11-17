32d6f018b7b793c56f3cfc1db49b64bac4ec4191
metindokmen <kindmemento@gmail.com>
2023-11-17 15:56:27 +0300

    give real life example use case for rabin karp algorithm

--- algorithms/rabin-karp.kt
+++ algorithms/rabin-karp.kt
@@ -2,3 +2,8 @@
 // It works by calculating a hash value for the pattern and for each substring of the text of the same lenght as the pattern.
 // If the hash values match, the algorithm then checks if the actual substring and the pattern are identical.
 // This avoids the need for character-by-character comparisons in most cases.
+
+// Imagine you're trying to find a particular word or phrase in a long document.
+// Instead of comparing the search string to every substring of the document (which would be time-consuming),
+// you calculate a hash of the search string and then look for matches of this hash within the document.
+// This is the principle behind the Rabin-Karp algorithm.
