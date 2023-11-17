b52bf758cebfb80c96b375b272757927eb7d3db1
metindokmen <kindmemento@gmail.com>
2023-11-17 17:29:16 +0300

    implement rabin-karp algorithm

--- algorithms/rabin-karp.kt
+++ algorithms/rabin-karp.kt
@@ -7,3 +7,41 @@
 // Instead of comparing the search string to every substring of the document (which would be time-consuming),
 // you calculate a hash of the search string and then look for matches of this hash within the document.
 // This is the principle behind the Rabin-Karp algorithm.
+
+fun rabinKarp(text: String, pattern: String): Int {
+    val m = pattern.length
+    val n = text.length
+    val prime = 101
+    var patternHash = 0
+    var textHash = 0
+    var h = 1
+
+    // The value of h would be "pow(d, M-1)%q"
+    for (i in 0 until m - 1) {
+        h = (h * 256) % prime
+    }
+
+    // Calculate the hash value of pattern and first window of text
+    for (i in 0 until m) {
+        patternHash = (256 * patternHash + pattern[i].toInt()) % prime
+        textHash = (256 * textHash + text[i].toInt()) % prime
+    }
+
+    // Slide the pattern over text one by one
+    for (i in 0..n - m) {
+        if (patternHash == textHash) {
+            var j = 0
+            while (j < m) {
+                if (text[i + j] != pattern[j]) break
+                j++
+            }
+            if (j == m) return i
+        }
+
+        if (i < n - m) {
+            textHash = (256 * (textHash - text[i].toInt() * h) + text[i + m].toInt()) % prime
+            if (textHash < 0) textHash += prime
+        }
+    }
+    return -1
+}
