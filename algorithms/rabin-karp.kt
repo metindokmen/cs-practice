acd6a8d3835d376d7e7b0eb606fc5047b0ce256a
metindokmen <kindmemento@gmail.com>
2023-11-17 15:53:46 +0300

    explain rabin-karp algorithm in comments

---
+++ algorithms/rabin-karp.kt
@@ -0,0 +1,4 @@
+// The Rabin-Karp algorithm is a string-searching algorithm that uses hashing to find any one of a set of pattern strings in a text.
+// It works by calculating a hash value for the pattern and for each substring of the text of the same lenght as the pattern.
+// If the hash values match, the algorithm then checks if the actual substring and the pattern are identical.
+// This avoids the need for character-by-character comparisons in most cases.
