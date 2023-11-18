7c42217b0ac5890fe16b4b96b11fc008a08b5475
metindokmen <kindmemento@gmail.com>
2023-11-18 13:52:44 +0300

    explain kadanes algorithm in comments

---
+++ algorithms/kadanes.kt
@@ -0,0 +1,3 @@
+// Kadane's Algorithm is an efficient way to solve the "Maximum Subarray Problem",
+// which is the task of finding the largest possible sum of a contiguous subarray within a given one-dimensional array of numbers (which can be negative).
+// The key idea of Kadan'es algorithm is to look for all positive contiguous segments of the array and keep track of the maximum sum contiguous segment among all positive segments.
