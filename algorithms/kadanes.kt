5c37cf5c31a5e9b889a26a5e194b65d611cfb9e9
metindokmen <kindmemento@gmail.com>
2023-11-18 13:54:05 +0300

    give a real life example use case where kadanes algorithm would be useful

--- algorithms/kadanes.kt
+++ algorithms/kadanes.kt
@@ -1,3 +1,7 @@
 // Kadane's Algorithm is an efficient way to solve the "Maximum Subarray Problem",
 // which is the task of finding the largest possible sum of a contiguous subarray within a given one-dimensional array of numbers (which can be negative).
 // The key idea of Kadan'es algorithm is to look for all positive contiguous segments of the array and keep track of the maximum sum contiguous segment among all positive segments.
+
+// Imagine you're analyzing stock market prices over a period, and you want to find the best time to buy and sell to maximize profit.
+// You have a list of changes in stock price each day (which can be positive or negative).
+// Kadane's Algorithm can be used to find the time period (subarray) where the sum of these changes (profit or loss) is the highest.
