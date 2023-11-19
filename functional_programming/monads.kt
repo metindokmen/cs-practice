cf3fb25a655f95475e2f47d1b5a5e62004ea1bef
metindokmen <kindmemento@gmail.com>
2023-11-19 21:42:00 +0300

    briefly explain monads in comments

---
+++ functional_programming/monads.kt
@@ -0,0 +1,3 @@
+// Monads are a somewhat abstract and often misunderstood concept in programming, particularly prevalent in functional languages.
+// They are a type of abstract data type used to represent computations instead of data in domain model.
+
