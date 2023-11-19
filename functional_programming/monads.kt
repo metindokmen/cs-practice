2ce0b36c95e0bc7b4d5bf9d94db37f0e650abdd6
metindokmen <kindmemento@gmail.com>
2023-11-19 21:44:43 +0300

    give a real life example to explain monads in programming in continued comments

--- functional_programming/monads.kt
+++ functional_programming/monads.kt
@@ -1,3 +1,17 @@
 // Monads are a somewhat abstract and often misunderstood concept in programming, particularly prevalent in functional languages.
 // They are a type of abstract data type used to represent computations instead of data in domain model.
 
+// To understand monads in a real-world context, let's consider a scenario involving online shopping.
+// When you shop online, you go through a series of steps:
+
+// selecting items,
+// adding them to a cart,
+// applying discounts,
+// calculating taxes,
+// and finally making a payment.
+
+// Each step is dependent on the successful completion of the previous one.
+// If something goes wrong at any step (e.g., an item is out of stock, or payment fails), the process stops, and you might receive an error message.
+
+// In programming, monads help manage these kinds of sequential operations, where each step depends on the outcome of the previous one.
+// They can handle errors gracefully, allowing your program to continue when something goes wrong in the sequence.
