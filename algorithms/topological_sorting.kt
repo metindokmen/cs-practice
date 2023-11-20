a2d4cc61ef0d988936c8fd9c5bbaf94038d9c0ef
metindokmen <kindmemento@gmail.com>
2023-11-20 14:52:02 +0300

    explain topological sorting and its constraints

---
+++ algorithms/topological_sorting.kt
@@ -0,0 +1,3 @@
+// Topological Sorting of a directed graph is a linear ordering of its vertices such that for every directed edge UV from vertex U to vertex V, U comes before V in the ordering.
+// This algorithm is used in scenarios where there is a dependency between tasks or components.
+// It's important to note that topoplogical sorting is possible only if the graph has no directed cycles, i.e., if it's a Directed Acyclic Graph (DAG).
