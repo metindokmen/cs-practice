1fbe0023484136a4d7306387443403b1636b3f12
metindokmen <kindmemento@gmail.com>
2023-11-21 15:11:10 +0300

    explain tarjan's algorithm in comments

---
+++ algorithms/tarjan.kt
@@ -0,0 +1,2 @@
+// Tarjan's algorithm is used to find all SCCs in a directed graph. A SCC in a directed graph is a set of vertices where every vertex is reachable from every other vertex in the set.
+// The algorithm uses depth-first search (DFS) and has a linear time complexity relative to the number of vertices and edges.
