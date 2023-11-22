de0dfc3b0dd4faf41ee73e3652e65fe3de7393b2
metindokmen <kindmemento@gmail.com>
2023-11-22 14:13:46 +0300

    implement z algorithm

--- algorithms/z-algorithm.kt
+++ algorithms/z-algorithm.kt
@@ -5,3 +5,24 @@
 // Imagine you are developing a plagiarism detection tool for academic papers.
 // Your tool needs to quickly find occurrences of certain phrases or sentences within a large document.
 // This is where string matching algorithms like the Z-Algorithm come into play.
+
+fun zAlgorithm(s: String): IntArray {
+    val n = s.length
+    val z = intArray(n)
+    var l = 0
+    var r = 0
+    for (i in 1 until n) {
+        if (i <= r) {
+            z[i] = minOf(z[i - l], r - i + 1)
+        }
+
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++
+        }
+        if (i + z[i] - 1 > r) {
+            l = i
+            r = i + z[i] - 1
+        }
+    }
+    return z
+}
