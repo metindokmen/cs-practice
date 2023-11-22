d0ae7ec7eb9859ddd52eadd26cca06be09f6b634
metindokmen <kindmemento@gmail.com>
2023-11-23 00:21:47 +0300

    explain cocktail shaker sort algorithm in comparison to bubble sort in comments

---
+++ algorithms/cocktail_shaker_sort.kt
@@ -0,0 +1,6 @@
+// Cocktail Shaker Sort is a variation of Bubble Sort.
+// While Bubble Sort only passes through the list in one direction (either up or down),
+// Cocktail Shaker Sort passes alternately frmo bottom to top and then from top to bottom.
+// This bidirectional approach helps in moving items to their positions faster,
+// thus improving performance over the regular Bubble Sort in certain cases,
+// especially where items at the end need to be moved to the beginning.
