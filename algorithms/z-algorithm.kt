b01bd94374ce4b1cdf529b73d1c8f37e40e71e27
metindokmen <kindmemento@gmail.com>
2023-11-22 14:10:42 +0300

    define and explain z-algorithm in comments

---
+++ algorithms/z-algorithm.kt
@@ -0,0 +1,3 @@
+// The Z-Algorithm is a linear-time string matching algorithm that identifies occurences of a pattern within a text.
+// It constructs an array, Z, where each entry Z[i] represents the length of the longest substring starting from i that is also a prefix of the text.
+// This array is then used to locate occurrences of the pattern within the text.
