d8a43014967d948cfd0b51e5385a06d2d01e4640
metindokmen <kindmemento@gmail.com>
2023-11-22 14:11:38 +0300

    give a real life example for z-algorithm

--- algorithms/z-algorithm.kt
+++ algorithms/z-algorithm.kt
@@ -1,3 +1,7 @@
 // The Z-Algorithm is a linear-time string matching algorithm that identifies occurences of a pattern within a text.
 // It constructs an array, Z, where each entry Z[i] represents the length of the longest substring starting from i that is also a prefix of the text.
 // This array is then used to locate occurrences of the pattern within the text.
+
+// Imagine you are developing a plagiarism detection tool for academic papers.
+// Your tool needs to quickly find occurrences of certain phrases or sentences within a large document.
+// This is where string matching algorithms like the Z-Algorithm come into play.
