2b71c0cf7d9360f839523bb4fb8ca06afeed4302
metindokmen <kindmemento@gmail.com>
2023-11-24 00:58:42 +0300

    explain ford-fulkerson algorithm in comments

---
+++ algorithms/ford-fulkerson.kt
@@ -0,0 +1,4 @@
+// The Ford-Fulkerson algorithm computes the maximum flow in a flow network.
+// It identifies possible paths from the source to the sink while incrementally increasing the flow along these paths.
+// The algorithm continues until no more augmenting paths from the source to the sink are are available.
+// The "augmenting path" is a path where the flow can be increased.
