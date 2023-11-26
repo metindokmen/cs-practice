121ac79c770781d31981bc3e98894a1ee723b389
metindokmen <kindmemento@gmail.com>
2023-11-26 19:24:44 +0300

    give a real life example use case for heaps algorithm

--- algorithms/heap_algorithm.kt
+++ algorithms/heap_algorithm.kt
@@ -1,3 +1,7 @@
 // Heap's Algorithm is used for generating all possible permutations of n objects.
 // It was first proposed by B.R. Heap in 1963.
 // The algorithm is efficient and requires only O(n!) time to generate all permutations.
+
+// Imagine you're organizing a dinner party and want to decide the seating arrangment for your guests.
+// You have a specific number of guests and want to consider every possible seating order to ensure the possible conversation dynamics.
+// Heap's Algorithm can be used to efficiently generate all possible permutations of the guests' seating order.
