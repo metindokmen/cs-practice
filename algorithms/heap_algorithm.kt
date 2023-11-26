17b2159db546088c1fcfe8df3e98e63fd5d3fc3c
metindokmen <kindmemento@gmail.com>
2023-11-26 19:26:10 +0300

    implement generic function permute

--- algorithms/heap_algorithm.kt
+++ algorithms/heap_algorithm.kt
@@ -5,3 +5,19 @@
 // Imagine you're organizing a dinner party and want to decide the seating arrangment for your guests.
 // You have a specific number of guests and want to consider every possible seating order to ensure the possible conversation dynamics.
 // Heap's Algorithm can be used to efficiently generate all possible permutations of the guests' seating order.
+
+fun <T> permute(elements: MutableList<T>, n: Int = elements.size) {
+    if (n == 1) {
+        println(elements)
+    } else {
+        for (i in 0 until n - 1) {
+            permute(elements, n - 1)
+            if (n % 2 == 0) {
+                elements.swap(i, n - 1)
+            } else {
+                elements.swap(0, n - 1)
+            }
+        }
+        permute(elements, n - 1)
+    }
+}
