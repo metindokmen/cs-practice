bf22111ab6431992f4684a53003eba003977db9d
metindokmen <kindmemento@gmail.com>
2023-11-26 19:23:37 +0300

    explain heap algorithm in comments

---
+++ algorithms/heap_algorithm.kt
@@ -0,0 +1,3 @@
+// Heap's Algorithm is used for generating all possible permutations of n objects.
+// It was first proposed by B.R. Heap in 1963.
+// The algorithm is efficient and requires only O(n!) time to generate all permutations.
