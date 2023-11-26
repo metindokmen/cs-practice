fbf87bc0610c2529ed60af8320e6257c316a5edd
metindokmen <kindmemento@gmail.com>
2023-11-26 19:27:19 +0300

    implement generic extension function swap to extend mutable list of type t

--- algorithms/heap_algorithm.kt
+++ algorithms/heap_algorithm.kt
@@ -21,3 +21,9 @@ fun <T> permute(elements: MutableList<T>, n: Int = elements.size) {
         permute(elements, n - 1)
     }
 }
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
