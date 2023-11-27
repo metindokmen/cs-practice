6a64bbef3e830a79a6835ecf14a8757295f7ccd9
metindokmen <kindmemento@gmail.com>
2023-11-28 00:02:40 +0300

    add a real life example use case for luhn algorithm in comments

--- algorithms/luhn.kt
+++ algorithms/luhn.kt
@@ -1,2 +1,6 @@
 // The Luhn Algorithm works by performing a sum of digits with some digits being doubled and transformed according to specific rules.
 // The validity of an identification number can be determined if the total modulo 10 is equal to 0.
+
+// Suppose you're developing an e-commerce application and need to validate credit card numbers entered by users.
+// The Luhn Algorithm is a simple checksum formula used to validate various identification numbers, such as credit card numbers.
+// It's designed to protect against accidental errors, like a digit mistyped.
