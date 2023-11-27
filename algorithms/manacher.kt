7309d3384160888365dad1570ea6044e662bff34
metindokmen <kindmemento@gmail.com>
2023-11-27 13:42:55 +0300

    explain manacher algorithm in comments

---
+++ algorithms/manacher.kt
@@ -0,0 +1,2 @@
+// Manacher's Algorithm is an efficient method to find the longest palindromic substring in linear time.
+// Traditional approaches might have quadratic or even cubic time complexity, but Manacher's Algorithm accomplishes this in O(n) time, making it much more suitable for long strings.
