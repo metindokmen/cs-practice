4e9c49d3120a60ddb73823dfa0712a0e3c1e36c7
metindokmen <kindmemento@gmail.com>
2023-11-28 00:01:33 +0300

    explain luhn algorithm in comments

---
+++ algorithms/luhn.kt
@@ -0,0 +1,2 @@
+// The Luhn Algorithm works by performing a sum of digits with some digits being doubled and transformed according to specific rules.
+// The validity of an identification number can be determined if the total modulo 10 is equal to 0.
