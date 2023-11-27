3e66fc6a382a3a2b6ed9145f0d65d810eaa50a0a
metindokmen <kindmemento@gmail.com>
2023-11-27 13:48:06 +0300

    implement longestPalindromicSubstring function

--- algorithms/manacher.kt
+++ algorithms/manacher.kt
@@ -4,3 +4,34 @@
 // Imagine you're developing a word game where players need to find the longest palindromic substring within a given string.
 // A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
 // For example, in the string "abcbadad", the longest palindromic substring is "bcacb".
+
+fun longestPalindromicSubstring(s: String): String {
+    val transformed = "#${s.toCharArray().joinToString("#")}{#}"
+    val p = IntArray(transformed.length)
+    var center = 0
+    var right = 0
+
+    for (i in transformed.indices) {
+        val mirror = 2 * center - 1
+        if (i < right) {
+            p[i] = minOf(right - i, p[mirror])
+        }
+
+        var a = i + p[i] + 1
+        var b = i - p[i] - 1
+        while (a < transformed.length && b >= 0 && transformed[a] == transformed[b]) {
+            p[i]++
+            a++
+            b--
+        }
+
+        if (i + p[i] > right) {
+            center = i
+            right = i + p[i]
+        }
+    }
+
+    val (maxLen, centerIndex) = p.withIndex().maxByOrNull { it.value } ?: return ""
+    val start = (centerIndex - maxLen) / 2
+    return s.substring(start, start + maxLen)
+}
