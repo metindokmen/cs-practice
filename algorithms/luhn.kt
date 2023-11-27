8677274af0986fac2530c6a99b2472ac2c357386
metindokmen <kindmemento@gmail.com>
2023-11-28 00:04:16 +0300

    implement a validator to check if argument passed is a valid luhn number

--- algorithms/luhn.kt
+++ algorithms/luhn.kt
@@ -4,3 +4,20 @@
 // Suppose you're developing an e-commerce application and need to validate credit card numbers entered by users.
 // The Luhn Algorithm is a simple checksum formula used to validate various identification numbers, such as credit card numbers.
 // It's designed to protect against accidental errors, like a digit mistyped.
+
+fun isValidLuhnNumber(number: String): Boolean {
+    var sum = 0
+    var alternate = false
+    for (i in number.length - 1 downTo 0) {
+        var n = number[i].digitToInt()
+        if (alternate) {
+            n *= 2
+            if (n > 9) {
+                n = (n % 10) + 1
+            }
+        }
+        sum += n
+        alternate = !alternate
+    }
+    return sum % 10 == 0
+}
