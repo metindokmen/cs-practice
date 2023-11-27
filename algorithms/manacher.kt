a4d51a523529838a6b3f9c6c145a5d17492731b0
metindokmen <kindmemento@gmail.com>
2023-11-27 13:44:25 +0300

    give a real life example for manachers algorithm

--- algorithms/manacher.kt
+++ algorithms/manacher.kt
@@ -1,2 +1,6 @@
 // Manacher's Algorithm is an efficient method to find the longest palindromic substring in linear time.
 // Traditional approaches might have quadratic or even cubic time complexity, but Manacher's Algorithm accomplishes this in O(n) time, making it much more suitable for long strings.
+
+// Imagine you're developing a word game where players need to find the longest palindromic substring within a given string.
+// A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+// For example, in the string "abcbadad", the longest palindromic substring is "bcacb".
