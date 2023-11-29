6b1a8bbb804e6f248007440db0dc28c05da3bc37
metindokmen <kindmemento@gmail.com>
2023-11-29 08:25:13 +0300

    give a real life example use case for fisher-yates shuffle algorithm

--- algorithms/fisher-yates_shuffle.kt
+++ algorithms/fisher-yates_shuffle.kt
@@ -1,2 +1,6 @@
 // The Fisher-Yates Shuffle is an algorithm for generating a random permutation of a finite sequence - in plain terms, for randomly shuffling the sequence.
 // The algorithm runs in O(n) time and is unbiased, meaning every permutation is equally likely.
+
+// Imagine you are creating a digital card game and need to shuffle the deck of cards randomly.
+// The Fisher-Yates Shuffle Algorithm is an efficient way to randomly shuffle the elements in an array, ensuring each possible permutation is equally likely.
+// This algorithm is also commonly used in algorithms that need random sampling, like in generating a random playlist from a set of songs.
