0ed8a85c02c3d840f1cac0774f83da29334222ce
metindokmen <kindmemento@gmail.com>
2023-11-29 06:40:00 +0300

    add real life example use case for prim's algorithm

--- algorithms/prim.kt
+++ algorithms/prim.kt
@@ -1,2 +1,6 @@
 // Prim's Algorithm is a greedy algorithm that finds a minimum spanning tree for a weighted undirected graph.
 // It starts with a single vertex and continuously adds the lowest-weighted edge that connects a vertex in the tree to a vertex outside of the tree until all vertices are included.
+
+// Imagine you're planning the layout of a network of roads to connect several towns.
+// You want to minimize the total length of the roads while ensuring that all towns are connected.
+// This is a case where Prim's Algorithm is useful: it finds a subset of the edges that forms a tree including every vertex, where the total weight of all the edges in the tree is minimized.
