28abd725be27a6206bcb301efa93e758f640e30c
metindokmen <kindmemento@gmail.com>
2023-11-29 08:23:53 +0300

    explain fisher-yates shuffle algorithm in comments

---
+++ algorithms/fisher-yates_shuffle.kt
@@ -0,0 +1,2 @@
+// The Fisher-Yates Shuffle is an algorithm for generating a random permutation of a finite sequence - in plain terms, for randomly shuffling the sequence.
+// The algorithm runs in O(n) time and is unbiased, meaning every permutation is equally likely.
