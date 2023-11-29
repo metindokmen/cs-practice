e22dde81478fa2afb9799dd2ef53115da0badbf6
metindokmen <kindmemento@gmail.com>
2023-11-29 06:38:41 +0300

    add definition for prim's algorithm

---
+++ algorithms/prim.kt
@@ -0,0 +1,2 @@
+// Prim's Algorithm is a greedy algorithm that finds a minimum spanning tree for a weighted undirected graph.
+// It starts with a single vertex and continuously adds the lowest-weighted edge that connects a vertex in the tree to a vertex outside of the tree until all vertices are included.
