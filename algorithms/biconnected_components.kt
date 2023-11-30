1155121ed9b18e098350587a7d7adbf53db51ecb
metindokmen <kindmemento@gmail.com>
2023-11-30 11:51:04 +0300

    explain bcc in comments

---
+++ algorithms/biconnected_components.kt
@@ -0,0 +1,3 @@
+// In graph theory, a biconnected component (BCC) of a graph is a maximal set of edges such that any two edges are connected to each other by a path of edges which does not pass through the same vertex more than once.
+// If a single vertex (called an articulation point or cut vertex) is removed, the BCC will not be disconnected.
+// The algorithm typically uses depth-first search (DFS) to find these components.
