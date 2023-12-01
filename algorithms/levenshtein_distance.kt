cdbcb687116d7a511d08c9a321be05391510ba9f
metindokmen <kindmemento@gmail.com>
2023-12-01 22:48:07 +0300

    give a real life example use case for levenshtein distance algorithm

--- algorithms/levenshtein_distance.kt
+++ algorithms/levenshtein_distance.kt
@@ -1,2 +1,6 @@
 // The Levenshtein Distance Algorithm calculates the minimum number of single-character edits required to change one string into another.
 // This metric is useful in applications where text strings need to be compared in a flexible manner, accounting for possible typos.
+
+// Consider a spell checker that suggests corrections for a misspelled word.
+// The algorithm calculates how many single-character edits (insertions, deletions, or substitutions) are necessary to change one word into another,
+// helping the spell checker determine the most similar correct words to suggest.
