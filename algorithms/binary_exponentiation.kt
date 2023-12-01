c80302893cd765586f810e8fdb71abfb5287b684
metindokmen <kindmemento@gmail.com>
2023-12-02 01:00:45 +0300

    define binary exponentiation algorithm in comments

---
+++ algorithms/binary_exponentiation.kt
@@ -0,0 +1,3 @@
+// Binary Exponentiation (also known as exponentiation by squaring) is an algorithm to calculate the power of a number in O(log n) time.
+// It is significantly faster than the naive approach of multiplying the number n times which would take O(n) time.
+
