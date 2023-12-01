0e34b6e11140db2d3c1f8b4f4fc2eaced6c1c8aa
metindokmen <kindmemento@gmail.com>
2023-12-01 22:46:58 +0300

    explain levenshtein distance algorithm in comments

---
+++ algorithms/levenshtein_distance.kt
@@ -0,0 +1,2 @@
+// The Levenshtein Distance Algorithm calculates the minimum number of single-character edits required to change one string into another.
+// This metric is useful in applications where text strings need to be compared in a flexible manner, accounting for possible typos.
