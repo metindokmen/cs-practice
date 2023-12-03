c90e9b46606233da346ba71e301a52839e42a3f4
metindokmen <kindmemento@gmail.com>
2023-12-03 17:03:05 +0300

    give real life example use case for exponential search algorithm

--- algorithms/exponential_search.kt
+++ algorithms/exponential_search.kt
@@ -6,3 +6,8 @@
 // First, it checks at indices 1, 2, 4, 8, ... and so on until it find an interval where the element must lie.
 // Once the range is found, it performs a binary search between these bounds.
 // This algorithm is efficient for unbounded searches where the size of the array is unknown.
+
+// Imagine you're looking for a specific book in a large library with all books sorted by a numbering system.
+// If you know the range where the book number might lie, it would be inefficient to start from the beginning.
+// Instead, you could use exponential search to quickly narrow down the range, and then use binary search within that range.
+// This is particularly efficient when the element to be searched is closer to the beginning of the array.
