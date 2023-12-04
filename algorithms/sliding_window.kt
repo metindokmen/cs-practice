02aa6e0e3879b8494e2451c6f371d1ea862ea493
metindokmen <kindmemento@gmail.com>
2023-12-04 15:13:01 +0300

    explain sliding window algoritm in comments

---
+++ algorithms/sliding_window.kt
@@ -0,0 +1,3 @@
+// The Sliding Window algorithm is a method for keeping track of a subset of data in a larger dataset.
+// It involves creating a 'window' which can either be a fixed size or flexible, that moves through the data to examine different subsets of elements.
+// This technique is useful for solving array/string problems efficiently, such as finding the longest substring without repeating characters, maximum sum subarray of size 'k', or smallest subarray with a given sum.
