7561e1a0a61b059328f5041d0142d65d543db0bb
metindokmen <kindmemento@gmail.com>
2023-12-04 15:14:00 +0300

    give real life example use case for sliding window algorithm

--- algorithms/sliding_window.kt
+++ algorithms/sliding_window.kt
@@ -1,3 +1,6 @@
 // The Sliding Window algorithm is a method for keeping track of a subset of data in a larger dataset.
 // It involves creating a 'window' which can either be a fixed size or flexible, that moves through the data to examine different subsets of elements.
 // This technique is useful for solving array/string problems efficiently, such as finding the longest substring without repeating characters, maximum sum subarray of size 'k', or smallest subarray with a given sum.
+
+// Suppose you're analyzing stock prices, and you want to find the maximum profit you can by buying and selling once within a given number of days.
+// Using the Sliding Window technique, you can keep track of the minimum price seen so far and the maximum profit that can be made each day as you move through the price list.
