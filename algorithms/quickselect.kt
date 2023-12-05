fa050d9fbbbd7fde3e700103d0fe16dbbe4ffe90
metindokmen <kindmemento@gmail.com>
2023-12-05 22:55:27 +0300

    give real life example for quickselect algorithm

--- algorithms/quickselect.kt
+++ algorithms/quickselect.kt
@@ -1,3 +1,7 @@
 // Quickselect is a selection algorithm to find the k'th smallest element in an unordered list.
 // It is related to the Quicksort sorting algorithm and is typically efficient for this task because it works in expected O(n) time complexity.
 // It uses a partioning method similar to Quicksort to recursively divide the list.
+
+// Suppose you're participating in a race, and after the race, you want to quickly find the runner who finished in the 5th place.
+// Going through the entire list of participants and ordering them from fastest to slowest would be inefficient.
+// Quickselect is an algorithm that can find the k'th smallest (or largest) element in an unordered list without sorting the entire list.
