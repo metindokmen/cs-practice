aa813ecaed62cf198687fb1c5921cf8ce2242d01
metindokmen <kindmemento@gmail.com>
2023-12-05 22:54:08 +0300

    explain quickselect algorithm in comments

---
+++ algorithms/quickselect.kt
@@ -0,0 +1,3 @@
+// Quickselect is a selection algorithm to find the k'th smallest element in an unordered list.
+// It is related to the Quicksort sorting algorithm and is typically efficient for this task because it works in expected O(n) time complexity.
+// It uses a partioning method similar to Quicksort to recursively divide the list.
