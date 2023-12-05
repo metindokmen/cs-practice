bf4788b4a93078a098620f9c2a725c36819bb7df
metindokmen <kindmemento@gmail.com>
2023-12-05 13:35:49 +0300

    explain dutch national flag algorithm in comments

---
+++ algorithms/dutch_national_flag.kt
@@ -0,0 +1,9 @@
+// The Dutch National Flag Algorithm, proposed by Edsger Dijkstra, is used to sort an array of three distinct elements (the classic version uses 0, 1, 2) in linear time and constant space.
+// The idea is to maintain four regions in the array:
+
+// 1. Red objects (e.g., 0's)
+// 2. White objects (e.g., 1's)
+// 3. Blue objects (e.g., 2's)
+// Unknown objects
+
+// The algorithm iterates through the array, expanding the regions as it encounters each color.
