a3a30495a52625b58b32cf05bcea69b8f4170fa5
metindokmen <kindmemento@gmail.com>
2023-12-05 13:39:32 +0300

    implement dutch national flag function

--- algorithms/dutch_national_flag.kt
+++ algorithms/dutch_national_flag.kt
@@ -10,3 +10,31 @@
 
 // Imagine you have balls of three different colors (red, white, and blue) and you want to sort them so that the same colors are grouped together.
 // This problem is analogous to the Dutch National Flag problem, where you're given an array with three different numbers and you need to sort them in place so that the same numbers are grouped together.
+
+fun dutchNationalFlag(arr: IntArray) {
+    var low = 0
+    var high = arr.size - 1
+    var mid = 0
+
+    while (mid <= high) {
+        when (arr[mid]) {
+            0 -> {
+                // Swap low and mid elements
+                arr[mid] = arr[low].also { arr[low] = arr[mid] }
+                // Increment low ad mid pointers
+                low++
+                mid++
+            }
+            1 -> {
+                // If it's 1, just move the mid pointer
+                mid++
+            }
+            2 -> {
+                // Swap mid and high elements
+                arr[mid] = arr[high].also { arr[high] = arr[mid] }
+                // Decrement high pointer
+                high--
+            }
+        }
+    }
+}
