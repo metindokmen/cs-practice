5ba03e5b956ac2497198661bab4f4b4e4a514881
metindokmen <kindmemento@gmail.com>
2023-12-06 17:15:36 +0300

    implement lengthOfLIS function

--- algorithms/longest_increasing_subsequence.kt
+++ algorithms/longest_increasing_subsequence.kt
@@ -3,3 +3,18 @@
 
 // Imagine you're a teacher who has recorded the grades of a student throughout the year, and you want to analyze the longest period of consistent improvement in the student's performance.
 // This can be modeled as finding the longest increasing subsequence within a sequence of grades.
+
+fun lenghtOfLIS(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    val dp = IntArray(nums.size) { 1 }
+
+    for (i in nums.indices) {
+        for (j in 0 until i) {
+            if (nums[j] < nums[i]) {
+                dp[i] = maxOf(dp[i], dp[j] + 1)
+            }
+        }
+    }
+
+    return dp.maxOrNull() ?: 1
+}
