0f5058b91d5f6505a662a44692f0904494f877c3
metindokmen <kindmemento@gmail.com>
2023-12-06 12:08:07 +0300

    explain gale-shapley algorithm

---
+++ algorithms/gale-shapley.kt
@@ -0,0 +1,3 @@
+// The Gale-Shapley algorithm, also known as the Stable Marriage Algorithm, solves the Stable Marriage Problem by iteratively building a stable match.
+// The algorithm involves a series of "proposals" where each unengaged man proposes to the most-preferred woman whom he has not yer proposed.
+// Each women then considers the proposals she has received (if any) and tentatively agrees to marry the one she most prefers at that point, rejecting the rest.
