3978e768845337dfbca24ce5a584bfcc89925311
metindokmen <kindmemento@gmail.com>
2023-12-06 17:14:21 +0300

    give real life example use case for lis algorithm

--- algorithms/longest_increasing_subsequence.kt
+++ algorithms/longest_increasing_subsequence.kt
@@ -1,2 +1,5 @@
 // The Longest Increasing Subsequence is a classic problem in computer science that seeks to find the length of the longest subsequence of a given sequence in which the elements are in sorted, increasing order.
 // This problem has applications in numerous fields, including bioinformatics and stock market analysis.
+
+// Imagine you're a teacher who has recorded the grades of a student throughout the year, and you want to analyze the longest period of consistent improvement in the student's performance.
+// This can be modeled as finding the longest increasing subsequence within a sequence of grades.
