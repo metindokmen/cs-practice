419b6080b81efad1eda8780f78a77e8fd28847e8
metindokmen <kindmemento@gmail.com>
2023-12-06 17:13:27 +0300

    define and explain longest increasing subsequence problem and algorithm

---
+++ algorithms/longest_increasing_subsequence.kt
@@ -0,0 +1,2 @@
+// The Longest Increasing Subsequence is a classic problem in computer science that seeks to find the length of the longest subsequence of a given sequence in which the elements are in sorted, increasing order.
+// This problem has applications in numerous fields, including bioinformatics and stock market analysis.
