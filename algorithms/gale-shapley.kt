5ff06a85bb3e8da0225097828079ac9b893e9a45
metindokmen <kindmemento@gmail.com>
2023-12-06 12:09:33 +0300

    give real life example for gale-shaply algorithm

--- algorithms/gale-shapley.kt
+++ algorithms/gale-shapley.kt
@@ -1,3 +1,8 @@
 // The Gale-Shapley algorithm, also known as the Stable Marriage Algorithm, solves the Stable Marriage Problem by iteratively building a stable match.
 // The algorithm involves a series of "proposals" where each unengaged man proposes to the most-preferred woman whom he has not yer proposed.
 // Each women then considers the proposals she has received (if any) and tentatively agrees to marry the one she most prefers at that point, rejecting the rest.
+
+// Imagine a dating app that aims to pair up partners based on preferences.
+// Each person ranks all members of the opposite sex in order of preference.
+// The goal is to find a stable matching, where no two people would prefer to be with each other over their current partners.
+// The Gale-Shapley algorithm can solve this problem by finding a stable matching.
