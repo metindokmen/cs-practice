cb2c895b684c252852fecb654928ded71ae3abf4
metindokmen <kindmemento@gmail.com>
2023-12-08 00:26:55 +0300

    give a real life example for linear search

--- algorithms/linear_search.kt
+++ algorithms/linear_search.kt
@@ -2,3 +2,7 @@
 // It checks each element of a list in turn to find a specific value.
 // It's best used on small or unsorted data sets
 // because its performance is directly proportional to the size of the data set; the average performance is O(n).
+
+// Imagine you're looking for a specific book in a small library.
+// You start at one end of the shelf and examine each book in turn until you find the one you're looking for.
+// This method, where you check every item sequentially until the desired one is found, is the essence of the Linear Search algorithm.
