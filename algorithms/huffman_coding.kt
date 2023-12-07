fa2c562f02c994ecd23f13710e7498a00b08ae47
metindokmen <kindmemento@gmail.com>
2023-12-07 12:12:52 +0300

    give real life example use case for huffman coding algorithm

--- algorithms/huffman_coding.kt
+++ algorithms/huffman_coding.kt
@@ -2,3 +2,8 @@
 // It assigns variable=length codes to input characters, with shorter codes assigned to more frequent characters.
 // This is achieved by building a binary tree (Huffman Tree), where each leaf node represents a character,
 // and the path from the root to the leaf gives the character's binary code.
+
+// Imagine you're developing a file compression tool.
+// You need to encode text in a way that reduces its size for storage or transmission.
+// Huffman Coding provides an efficient way of encoding characters based on their frequency in the text.
+// Characters that occur more frequently are encoded with shorter codes, and less frequent characters with longer codes, reducing the overall size of the text.
