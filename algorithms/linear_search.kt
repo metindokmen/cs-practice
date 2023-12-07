b1a1b82ba36ade6cab15df038dce0635315ec614
metindokmen <kindmemento@gmail.com>
2023-12-08 00:25:51 +0300

    explain linear search algorithm in comments

---
+++ algorithms/linear_search.kt
@@ -0,0 +1,4 @@
+// Linear Search is one of the simplest searching algorithms.
+// It checks each element of a list in turn to find a specific value.
+// It's best used on small or unsorted data sets
+// because its performance is directly proportional to the size of the data set; the average performance is O(n).
