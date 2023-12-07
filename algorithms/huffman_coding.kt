032fd19deccee00cfde021e37599f632fa7daeca
metindokmen <kindmemento@gmail.com>
2023-12-07 12:11:27 +0300

    explain huffman coding algorithm in comments

---
+++ algorithms/huffman_coding.kt
@@ -0,0 +1,4 @@
+// Huffman Coding is a widely used method of lossless data compression.
+// It assigns variable=length codes to input characters, with shorter codes assigned to more frequent characters.
+// This is achieved by building a binary tree (Huffman Tree), where each leaf node represents a character,
+// and the path from the root to the leaf gives the character's binary code.
