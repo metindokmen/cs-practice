ec80d308fb6bf5a9ecefc8785e75ce5cfebae73c
metindokmen <kindmemento@gmail.com>
2023-12-09 01:54:05 +0300

    explain tower of hanoi algorithm in comments

---
+++ algorithms/tower_of_hanoi.kt
@@ -0,0 +1,3 @@
+// The Tower of Hanoi algorithm is a recursive algorithm to solve the puzzle.
+// The solution can be achieved by recursively moving a smallar stack of disks (n-1) to a buffer post,
+// then moving the largest disk, and finally moving the smaller stack on top of the largest disk.
