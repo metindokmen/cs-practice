d877c5497721209071fe8329570613505e7f2321
metindokmen <kindmemento@gmail.com>
2023-12-09 01:49:18 +0300

    give a real life example use case for euclidean algorithm

--- algorithms/euclidean_algorithm.kt
+++ algorithms/euclidean_algorithm.kt
@@ -1,3 +1,8 @@
 // The Euclidean Algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers.
 // It is based on the principle that the GCD of two numbers also divides their difference.
 // By repeatedly subtracting the smaller number from the larger one, the algorithm eventually reduces this to the case where one of the two numbers is zero, at which point the other number is the GCD.
+
+// Imagine you're a craftsman who needs to cut tiles for a floor.
+// You want to find the largest square tile size that can be used to tile the floor withput cutting any tiles, given the length and width of the floor.
+// The GCD of the floor's length and width will give you the size of the largest square tile that can be used.
+// This is where the Euclidean Algorithm is useful.
