a43ee908fc3a6317d2ee45ab53f3daab0b62a43e
metindokmen <kindmemento@gmail.com>
2023-12-09 01:48:08 +0300

    define and explain the euclidean algorithm in comments

---
+++ algorithms/euclidean_algorithm.kt
@@ -0,0 +1,3 @@
+// The Euclidean Algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers.
+// It is based on the principle that the GCD of two numbers also divides their difference.
+// By repeatedly subtracting the smaller number from the larger one, the algorithm eventually reduces this to the case where one of the two numbers is zero, at which point the other number is the GCD.
