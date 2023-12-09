4bd34ff879fc131c8e1020cbf976a800c74bc021
metindokmen <kindmemento@gmail.com>
2023-12-10 01:35:35 +0300

    explain aho-corasick algorithm in comments

---
+++ algorithms/aho-corasick.kt
@@ -0,0 +1,3 @@
+// The Aho-Corasick algorithm is a string-searching algorithm that constructs a finite-state matching from a set of strings (like a trie with additional links between the various internal nodes).
+// It can match all strings simultaneously in a text, making it efficient for searching multiple patterns.
+// This is particularly useful in applications like search engines, text analysis, and digital content filtering.
