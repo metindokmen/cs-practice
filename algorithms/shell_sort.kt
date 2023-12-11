e32fdf6febc7b7937b68fa7cda64a4a0c208758c
metindokmen <kindmemento@gmail.com>
2023-12-12 00:10:26 +0300

    explain shell sort algorithm in comments

---
+++ algorithms/shell_sort.kt
@@ -0,0 +1,3 @@
+// Shell Sort is a generalization of insertion sort that allows the exchange of items that are far apart.
+// The idea is to arrange the list of elements so that, starting anywhere, taking every hth element produces a sorted list.
+// Such a list is said to be h-sorted. It start by sorting pairs of elements far apart from each other, then progressively reducing the gap between elements to be compared.
