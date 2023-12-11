0bbd4f0d60e45cbb342c9bf79c00c6aba39922ad
metindokmen <kindmemento@gmail.com>
2023-12-11 12:36:23 +0300

    give real life example

--- algorithms/sieve_of_atkin.kt
+++ algorithms/sieve_of_atkin.kt
@@ -1,3 +1,7 @@
 // The Sieve of Atkin is a modern algorithm used to find all prime numbers up to a specified integer.
 // It is an optimized version of the ancient Sieve of Eratosthenes and tends to perform better for larger number ranges.
 // The algorithm uses mathematical properties of quadratic residues to identify potential primes more efficiently.
+
+// Imagine you're working on a software application that requires the generation of a large list of prime numbers,
+// for instance, in cryptography for key generation. While the Sieve of Eratosthenes is a comment method
+// for finding all prime numbers up to a given limit, the Sieve of Atkin is a modern variant that can be more efficient for larger limits.
