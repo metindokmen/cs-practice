5a089d5d1a4e22b284c9590fba8f54db5cfef21a
metindokmen <kindmemento@gmail.com>
2023-12-12 00:03:25 +0300

    give real life example for booths algorithm

--- algorithms/booths.kt
+++ algorithms/booths.kt
@@ -1,2 +1,6 @@
 // Booth's Algorithm is used for finding the lexicogrphical smallest rotation of a string.
 // It's an efficient way to solve problems related to circular strings, where the end of the string is conceptually connected to the beginning.
+
+// Suppose you are developing a feature in a music app to identify a song from a snippet of its lyrics.
+// The lyrics are circular in nature, meaning the end of the string is connected to the beginning (like in a loop).
+// Booth's Algorithm allows you to efficiently find the lexicographically smallest string rotation, which can be used for circular string matching.
