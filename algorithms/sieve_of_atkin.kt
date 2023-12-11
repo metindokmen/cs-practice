14396c8f4e22c4c8d126baa210d19d8473897c0f
metindokmen <kindmemento@gmail.com>
2023-12-11 12:35:03 +0300

    explain sieve of atkin in comments

---
+++ algorithms/sieve_of_atkin.kt
@@ -0,0 +1,3 @@
+// The Sieve of Atkin is a modern algorithm used to find all prime numbers up to a specified integer.
+// It is an optimized version of the ancient Sieve of Eratosthenes and tends to perform better for larger number ranges.
+// The algorithm uses mathematical properties of quadratic residues to identify potential primes more efficiently.
