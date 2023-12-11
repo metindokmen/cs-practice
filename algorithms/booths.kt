d29c32bdaad4866f7f20ba104f9ba300b757389d
metindokmen <kindmemento@gmail.com>
2023-12-12 00:02:09 +0300

    explain booths algorithm in comments

---
+++ algorithms/booths.kt
@@ -0,0 +1,2 @@
+// Booth's Algorithm is used for finding the lexicogrphical smallest rotation of a string.
+// It's an efficient way to solve problems related to circular strings, where the end of the string is conceptually connected to the beginning.
