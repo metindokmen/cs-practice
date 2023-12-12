ee61b1766f3d3496eba263183ebf3344bc513e18
metindokmen <kindmemento@gmail.com>
2023-12-12 20:30:03 +0300

    give a real life example use case for cycle sort algorithm

--- algorithms/cycle_sort.kt
+++ algorithms/cycle_sort.kt
@@ -1,3 +1,8 @@
 // Cycle Sort is an in-place and comparison-based sorting algorithm.
 // It is theoretically optimal in the sense that it minimizes the number of writes to the original array.
 // It works by considering each array element and then putting it at its correct position by rotating the cycle of elements involved.
+
+// Imagine you're organizing a deck of cards where each card has a unique number and a specific order.
+// Your task is to sort the deck such that each card is placed in its correct position with the minimum number of writes (or movements).
+// Cycle Sort is an in-place sorting algorithm that is optimal in terms of the number of memory writes.
+// It's especially useful when memory write is a costly operation.
