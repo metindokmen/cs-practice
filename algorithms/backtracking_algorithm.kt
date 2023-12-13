6a01f75f78b2fc6047cb09209a72db9422635d4c
metindokmen <kindmemento@gmail.com>
2023-12-14 00:04:49 +0300

    implement isSafe function

--- algorithms/backtracking_algorithm.kt
+++ algorithms/backtracking_algorithm.kt
@@ -5,3 +5,27 @@
 // Consider a puzzle where you need to place N queens on an NxN chessboard so that no two queens threaten each other.
 // This means no two queens can be in the same row, column, or diagonal.
 // The N-Queens Problem is a classic example where the backtracking algorithm is used to systematically explore the possible arrangements of queens on the board.
+
+fun isSafe(board: Array<IntArray>, row: Int, col: Int, n: Int): Boolean {
+    for (i in 0 until col) {
+        if (board[row][i] == 1) return false
+    }
+
+    var i = row
+    var j = col
+    while (i >= 0 && j >= 0) {
+        if (board[i][j] == 1) return false
+        i--
+        j--
+    }
+
+    i = row
+    j = col
+    while (j >= 0 && i < n) {
+        if (board[i][j] == 1) return false
+        i++
+        j--
+    }
+
+    return true
+}
