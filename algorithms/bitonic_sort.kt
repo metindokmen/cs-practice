f792aecbee1a88e7fe717d05cd8da9a32b29f3c4
metindokmen <kindmemento@gmail.com>
2023-12-13 13:12:25 +0300

    explain bitonic sorting in comments

---
+++ algorithms/bitonic_sort.kt
@@ -0,0 +1,3 @@
+// Bitonic Sort is a parallel algorithm for sorting. It is also a comparison-based sorting algorithm and is used mainly in parallel computing.
+// A sequence is called bitonic if it is a sequence of "x0​≤x1​≤...≤xk​≥xk+1​≥...≥xn−1" for some k (0 ≤ � k < � n), or a circular shift of such a sequence.
+// Bitonic Sort first produces a bitonic sequence frmo the unsorted input, and then it sorts the bitonic sequence.
