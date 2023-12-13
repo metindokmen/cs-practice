539fababc2b5dbc4f1dfa8502645bcc8c252b578
metindokmen <kindmemento@gmail.com>
2023-12-13 13:16:01 +0300

    give real life example for bitonic sorting

--- algorithms/bitonic_sort.kt
+++ algorithms/bitonic_sort.kt
@@ -1,3 +1,8 @@
 // Bitonic Sort is a parallel algorithm for sorting. It is also a comparison-based sorting algorithm and is used mainly in parallel computing.
 // A sequence is called bitonic if it is a sequence of "x0​≤x1​≤...≤xk​≥xk+1​≥...≥xn−1" for some k (0 ≤ � k < � n), or a circular shift of such a sequence.
 // Bitonic Sort first produces a bitonic sequence frmo the unsorted input, and then it sorts the bitonic sequence.
+
+// Imagine you're managaing a large-scale, parallel processing system where you need to sort vast amounts of data quickly.
+// In such a scenario, traditional sorting algorithms might not be efficient due to their sequential nature.
+// Bitonic Sort, however, is designed for parallel processing, where multiple comparisons and swaps can be executed simultaneously,
+// significantly reducing the sorting time.
