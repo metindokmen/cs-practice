7e7576f1373e5e3c59ce3f120d853f512d7a4b78
metindokmen <kindmemento@gmail.com>
2023-12-14 00:03:11 +0300

    explain N-Queens Problems as a use case example for backtracking algorithm

--- algorithms/backtracking_algorithm.kt
+++ algorithms/backtracking_algorithm.kt
@@ -1,3 +1,7 @@
 // Backtracking is an algorithm paradigm that tries different solutions until it finds a solution that works.
 // It is used when you need to find all (or some) solutions to a problem that satisfies specific constraints.
 // Backtracking incrementally builds candidates for the solutions and abandons a candidate ("backtracks") as soon as it determines that this candidate cannot possible be completed to a valid solution.
+
+// Consider a puzzle where you need to place N queens on an NxN chessboard so that no two queens threaten each other.
+// This means no two queens can be in the same row, column, or diagonal.
+// The N-Queens Problem is a classic example where the backtracking algorithm is used to systematically explore the possible arrangements of queens on the board.
