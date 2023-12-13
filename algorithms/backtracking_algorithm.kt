e35a58bfafcd620a62b5e5e2b4905febf29c8f04
metindokmen <kindmemento@gmail.com>
2023-12-14 00:01:48 +0300

    explain backtracking algorithm in comments

---
+++ algorithms/backtracking_algorithm.kt
@@ -0,0 +1,3 @@
+// Backtracking is an algorithm paradigm that tries different solutions until it finds a solution that works.
+// It is used when you need to find all (or some) solutions to a problem that satisfies specific constraints.
+// Backtracking incrementally builds candidates for the solutions and abandons a candidate ("backtracks") as soon as it determines that this candidate cannot possible be completed to a valid solution.
