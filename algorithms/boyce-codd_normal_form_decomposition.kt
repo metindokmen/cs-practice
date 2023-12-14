50eb751a0c113650dcbb48ec5bc4df46729d5ba6
metindokmen <kindmemento@gmail.com>
2023-12-15 00:55:43 +0300

    add relation and functionalDependency data classes

--- algorithms/boyce-codd_normal_form_decomposition.kt
+++ algorithms/boyce-codd_normal_form_decomposition.kt
@@ -6,3 +6,9 @@
 // Over time, you notice that there are redundancies and anomalies due to the way this data is structured.
 // To optimize the database, reduce redundancy, and improve data integrity, you can use the BCNF Decomposition Algorithm.
 // It helps restructure the database into tables that better represent the relationship between different types of data.
+
+// Represents a table or relation in a database
+data class Relation(val attributes: Set<String>, val dependencies: Set<FunctionalDependency>)
+
+// Represents a functional dependency, e.g., "A -> B"
+data class FunctionalDependency(val determinant: Set<String>, val dependent: Set<String>)
