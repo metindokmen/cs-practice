a37fd0037244509d87d35357ee14210123a4bbe1
metindokmen <kindmemento@gmail.com>
2023-12-15 00:52:24 +0300

    explain bcnf decomposition algorithm in comments

---
+++ algorithms/boyce-codd_normal_form_decomposition.kt
@@ -0,0 +1,2 @@
+// The BCNF Decomposition Algorithm is used in database normalization to decompose relation that is not in BCNF into a set of relations that are in BCNF.
+// BCNF, a stricter version of the Third Normal Form (3NF), aims to resolve anomalies resulting from functional dependencies in a database.
