bf2320ed691a780162d18eeba7ca722498e18596
metindokmen <kindmemento@gmail.com>
2023-12-15 00:54:22 +0300

    give example use case for bcnf decomposition algorithm

--- algorithms/boyce-codd_normal_form_decomposition.kt
+++ algorithms/boyce-codd_normal_form_decomposition.kt
@@ -1,2 +1,8 @@
 // The BCNF Decomposition Algorithm is used in database normalization to decompose relation that is not in BCNF into a set of relations that are in BCNF.
 // BCNF, a stricter version of the Third Normal Form (3NF), aims to resolve anomalies resulting from functional dependencies in a database.
+
+// Imagine you're designing a database for a university.
+// The database includes information about courses, the departments they belong to, and the professors teaching them.
+// Over time, you notice that there are redundancies and anomalies due to the way this data is structured.
+// To optimize the database, reduce redundancy, and improve data integrity, you can use the BCNF Decomposition Algorithm.
+// It helps restructure the database into tables that better represent the relationship between different types of data.
