0d5f0a8a3fc61a09bfab3b0b95ace961d7f2b798
metindokmen <kindmemento@gmail.com>
2023-12-15 17:18:18 +0300

    explain simulated annealing algorithm in comments

---
+++ algorithms/simulated_annealing.kt
@@ -0,0 +1,3 @@
+// Simulated Annealing is a probabilistic technique used for finding an approximate solution to an optimization problem.
+// The algorithm randomly explores the solution space, accepting not only changes that improve the solution but also, with a certain probability, changes that worsen it.
+// This probability decreases with time, mimicking the cooling of metal, thus reducing the changes of making bad moves as the algorithm progresses.
