3789bccb7ccc493b5f121888895dfed9d6395774
metindokmen <kindmemento@gmail.com>
2023-12-15 17:19:53 +0300

    give real life use case for simulated annealing algorihtm

--- algorithms/simulated_annealing.kt
+++ algorithms/simulated_annealing.kt
@@ -1,3 +1,7 @@
 // Simulated Annealing is a probabilistic technique used for finding an approximate solution to an optimization problem.
 // The algorithm randomly explores the solution space, accepting not only changes that improve the solution but also, with a certain probability, changes that worsen it.
 // This probability decreases with time, mimicking the cooling of metal, thus reducing the changes of making bad moves as the algorithm progresses.
+
+// Imagine you're trying to solve a complex optimization problem, like finding the most efficient route for a delivery truck that needs to visit multiple locations.
+// Traditional algorithms might get stuck in a local minimum, where they find a good solution, but not the best one.
+// Simulated Annealing, insipred by the process of annealing in metallurgy, offers a way to escape these local minima and search for a global minimum (or maximum) by allowing "bad" moves with decreasing probablility as the "temprerature" decreases.
