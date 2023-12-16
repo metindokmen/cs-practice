98da2a18c5105f3e9839f57a36c96e38283f46ea
metindokmen <kindmemento@gmail.com>
2023-12-16 12:49:46 +0300

    explain sleep sort algorithm in comments

---
+++ algorithms/sleep_sort.kt
@@ -0,0 +1,2 @@
+// Sleep Sort is more of a conceptual and humorous sorting algorithm and is typically not used in practical applications due to its unreliablity and inefficiency.
+// It's based on the idea of using sleep or delay in threads, where each element in the array causes the program to sleep for an interval proportional to the element's value before outputting the element.
