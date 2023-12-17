eee30a25b2a57045f6708721125528b661f4088a
metindokmen <kindmemento@gmail.com>
2023-12-17 15:50:36 +0300

    explain bucket sort algorithm in comments

---
+++ algorithms/bucket_sort.kt
@@ -0,0 +1,3 @@
+// Bucket Sort is an efficient sorting algorithm for sorting elements that are uniformly distributed across a range.
+// It works by distributing elements into several "buckets" and then sorting these buckets individually.
+// Each bucket is sorted individually using a different sorting algorithm, typically insertion sort, due to its efficiency with small data sets.
