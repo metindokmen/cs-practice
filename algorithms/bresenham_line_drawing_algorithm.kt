662b37b34a192ad266dacb210257566a28af01a9
metindokmen <kindmemento@gmail.com>
2023-12-18 00:13:07 +0300

    give real life example use case for bresenham line drawing algorithm

--- algorithms/bresenham_line_drawing_algorithm.kt
+++ algorithms/bresenham_line_drawing_algorithm.kt
@@ -2,3 +2,7 @@
 // It determines which pixels should be plotted in order to form a close approximation of a straight line between two points.
 // The algorithm works by iterating over the dominant axis (either x or y) and calculating the minor axis pixel based on a decision variable.
 // It requires only integer addition, subtraction, and bit shifting, all of which are very fast operations.
+
+// Imagine you're developing a graphics drawing application where you need to draw straight lines between points on a raster grid (like pixels on a computer screen).
+// While you could use a simple equation of a line for this, it would require floating-point arithmetic, which is computationally expensive.
+// Bresenham's Line Drawing Algorithm provides an efficient way to draw lines using only integer arithmetic, making it well-suited for computer graphics where performance is key.
