2890b6c810d143dbde7d6fc0c38840bd395b2b58
metindokmen <kindmemento@gmail.com>
2023-12-18 00:18:27 +0300

    explain gift wrapping algorithm in comments

---
+++ algorithms/gift_wrapping_algorithm.kt
@@ -0,0 +1,3 @@
+// The Gift Wrapping algorithm is a method of computing the convex hull of a finite set of points in the plane.
+// The algorithm works by starting from the leftmost point (or the point with the lowest x-coordinate) and wrapping the hull around the set of points, much like wrapping a gift.
+// At each step, it selects the point that is the most counterclockwise relative to the current point, continuing until it wraps back around to the starting point.
