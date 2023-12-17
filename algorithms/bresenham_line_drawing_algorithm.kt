551ed61a35c39c4c0a0fe19e0658a0f738bd5f33
metindokmen <kindmemento@gmail.com>
2023-12-18 00:15:27 +0300

    implement drawLine function

--- algorithms/bresenham_line_drawing_algorithm.kt
+++ algorithms/bresenham_line_drawing_algorithm.kt
@@ -6,3 +6,35 @@
 // Imagine you're developing a graphics drawing application where you need to draw straight lines between points on a raster grid (like pixels on a computer screen).
 // While you could use a simple equation of a line for this, it would require floating-point arithmetic, which is computationally expensive.
 // Bresenham's Line Drawing Algorithm provides an efficient way to draw lines using only integer arithmetic, making it well-suited for computer graphics where performance is key.
+
+fun drawLine(x0: Int, y0: Int, x1: Int, y1: Int): List<Pair<Int, Int>> {
+    val pixels = mutableListOf<Pair<Int, Int>>()
+
+    var dx = Math.abs(x1 - x0)
+    var dy = Math.abs(y1 - y0)
+
+    var sx = if (x0 < x1) 1 else -1
+    var sy = if (y0 < y1) 1 else -1
+
+    var err = dx - dy
+    var e2: Int
+
+    var x = x0
+    var y = y0
+
+    while (true) {
+        pixels.add(Pair(x, y))
+        if (x == x1 && y == y1) break
+        e2 = 2 * err
+        if (e2 > -dy) {
+            err -= dy
+            x += sx
+        }
+        if (e2 < dx) {
+            err += dx
+            y += sy
+        }
+    }
+
+    return pixels
+}
