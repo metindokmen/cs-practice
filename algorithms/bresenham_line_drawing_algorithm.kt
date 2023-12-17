2f6845fbe1dc5987d7f672cbd836fe9ec6dbad85
metindokmen <kindmemento@gmail.com>
2023-12-18 00:11:34 +0300

    explain bresenham algorithm in comments

---
+++ algorithms/bresenham_line_drawing_algorithm.kt
@@ -0,0 +1,4 @@
+// Bresenham's Algorithm is an efficient way to draw a straight line between two points in a computer graphics context.
+// It determines which pixels should be plotted in order to form a close approximation of a straight line between two points.
+// The algorithm works by iterating over the dominant axis (either x or y) and calculating the minor axis pixel based on a decision variable.
+// It requires only integer addition, subtraction, and bit shifting, all of which are very fast operations.
