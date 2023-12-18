665260cba9e420f8851d1a0139d48a500dd51ca6
metindokmen <kindmemento@gmail.com>
2023-12-18 20:52:46 +0300

    give real life example for rainbow table algorithm

--- algorithms/rainbow_table_algorithm.kt
+++ algorithms/rainbow_table_algorithm.kt
@@ -3,3 +3,9 @@
 // by stroing precomputed hashes of possible plaintexts,
 // you can quickly find the matching plaintext for a given hash,
 // as long as the plaintext exists in your table.
+
+// Imagine you're working in cybersecurity, and you encounter a database breach where passwords are stored as hash values.
+// To find out the actual passwords, you might consider a brute-force attack, but it's highly time-consuming.
+// A Rainbow Table offers a more efficient method.
+// It's precomputed table for reversing cryptographic hash functions, primarily used for cracking password hashes.
+// By using a Rainbow Table, you can quickly look up the corresponding plaintext for a given hash.
