e5bae50fd3e595f17ab291ab168c82130a3bb00d
metindokmen <kindmemento@gmail.com>
2023-12-19 15:06:02 +0300

    give real life example use case for strassens algorithm

--- algorithms/strassen_matrix_multiplication.kt
+++ algorithms/strassen_matrix_multiplication.kt
@@ -3,3 +3,7 @@
 // The key idea behind Strassen's algorithm is that it reduces the number of recursive calls for multiplying submatrices from 8 (in the standard algorithm) to 7,
 // using clever additions and subtractions of those products.
 // This reduction provides a better run-time complexity.
+
+// Imagine you're working with 3D graphics in a video game, where you need to perform numerous matrix transformations - scaling, rotating, and translationg objects in the game world.
+// Efficient matrix multiplication becomes crucial for rendering these objects quickly, especially as the complexity of the scene increases.
+// Strassen's algorithm can be used here to optimize these matrix multiplication operations.
