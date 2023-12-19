5996bec7f5858d08f12870cad01ad41a951eb0e0
metindokmen <kindmemento@gmail.com>
2023-12-19 15:09:04 +0300

    set typealias Matrix for a type of array of arrays of integers

--- algorithms/strassen_matrix_multiplication.kt
+++ algorithms/strassen_matrix_multiplication.kt
@@ -7,3 +7,17 @@
 // Imagine you're working with 3D graphics in a video game, where you need to perform numerous matrix transformations - scaling, rotating, and translationg objects in the game world.
 // Efficient matrix multiplication becomes crucial for rendering these objects quickly, especially as the complexity of the scene increases.
 // Strassen's algorithm can be used here to optimize these matrix multiplication operations.
+
+typealias Matrix = Array<Array<Int>>
+
+fun addMatrix(A: Matrix, B: Matrix): Matrix {
+    val n = A.size
+    val sum = Array(n) { Array(n) { 0 } }
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            sum[i][j] = A[i][j] + B[i][j]
+        }
+    }
+
+    return sum
+}
