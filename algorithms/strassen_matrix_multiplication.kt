60b12330bfd3fda98bac0bb020bffe66bb55c0c1
metindokmen <kindmemento@gmail.com>
2023-12-19 15:04:03 +0300

    explain strassens matrix multiplication algorithm in comments

---
+++ algorithms/strassen_matrix_multiplication.kt
@@ -0,0 +1,5 @@
+// Strassen's Algorithm is an efficient algorithm used for matrix multiplication.
+// It is particularly faster than the standard algorithm for large matrices.
+// The key idea behind Strassen's algorithm is that it reduces the number of recursive calls for multiplying submatrices from 8 (in the standard algorithm) to 7,
+// using clever additions and subtractions of those products.
+// This reduction provides a better run-time complexity.
