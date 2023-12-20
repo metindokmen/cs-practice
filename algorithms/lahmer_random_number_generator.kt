a03c7a147b58ded3801242c842d7222d76f4994b
metindokmen <kindmemento@gmail.com>
2023-12-20 23:09:45 +0300

    implement lehmerRNG class and add main fun

--- algorithms/lahmer_random_number_generator.kt
+++ algorithms/lahmer_random_number_generator.kt
@@ -5,3 +5,24 @@
 // Suppose you're developing a mobile game, and you need a reliable way to generate random numbers for various game mechanics, like determining loot drops or enemy behavior.
 // A random number generator like Lehmer's can provide a sequence of pseudorandom numbers based on an initial seed.
 // Such generators are crucial in games and simulations where controlled randomness is required.
+
+class LehmerRNG(seed: Long) {
+    private var current: Long = seed
+    private val a = 48271L    // Multiplier
+    private val m = 2147483647L  // Modulus (2^31 - 1, a prime number)
+
+    fun nextInt(): Int {
+        current = (a * current) % m
+        return current.toInt()
+    }
+}
+
+fun main() {
+    val seed = 12345L  // Example seed
+    val rng = LehmerRNG(seed)
+
+    // Generate and print 10 random numbers
+    for (i in 1..10) {
+        println(rng.nextInt())
+    }
+}
