3d0438cefa90b463127f09fea347904ac2e31cb5
metindokmen <kindmemento@gmail.com>
2023-12-20 23:09:02 +0300

    give real life example use case

--- algorithms/lahmer_random_number_generator.kt
+++ algorithms/lahmer_random_number_generator.kt
@@ -1,3 +1,7 @@
 // The Lehmer Random Number Generator is a type of linear congruential generator,
 // one of the oldest and simplest methods for generating sequences of pseudorandom numbers.
 // The generator works on the principle of taking the remainder of a linear equation to produce the next number in the sequence.
+
+// Suppose you're developing a mobile game, and you need a reliable way to generate random numbers for various game mechanics, like determining loot drops or enemy behavior.
+// A random number generator like Lehmer's can provide a sequence of pseudorandom numbers based on an initial seed.
+// Such generators are crucial in games and simulations where controlled randomness is required.
