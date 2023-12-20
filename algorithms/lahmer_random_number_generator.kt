6d9dc88cf2c100cb69e6257ac7ec7f9bbdb62460
metindokmen <kindmemento@gmail.com>
2023-12-20 23:08:06 +0300

    explain algo on comments

---
+++ algorithms/lahmer_random_number_generator.kt
@@ -0,0 +1,3 @@
+// The Lehmer Random Number Generator is a type of linear congruential generator,
+// one of the oldest and simplest methods for generating sequences of pseudorandom numbers.
+// The generator works on the principle of taking the remainder of a linear equation to produce the next number in the sequence.
