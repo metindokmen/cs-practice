7ab284dd1c7cf30911ac0037662525bd794cea0e
metindokmen <kindmemento@gmail.com>
2023-12-21 14:24:09 +0300

    implement lcg class

---
+++ algorithms/linear_congruential_generator.kt
@@ -0,0 +1,14 @@
+// The Linear Congruential Generator is one of the oldest and simplest algorithms for generating pseudorandom numbers.
+// The algorithm works by following a linear formula: Xn+1​ = (a×Xn​+c) mod m
+
+class LinearCongruentialGenerator(seed: Long) {
+    private var current: Long = seed
+    private val a = 1664525L
+    private val c = 1013904223L
+    private val m = (2L shl 31)
+
+    fun nextInt(): Int {
+        current = (a * current + c) % m
+        return current.toInt()
+    }
+}
