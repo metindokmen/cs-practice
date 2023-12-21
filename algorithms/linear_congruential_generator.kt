a9e6cf6863d8b0a0646856cdbda06cc9f8d70c6d
metindokmen <kindmemento@gmail.com>
2023-12-21 14:27:12 +0300

    add missing real life example for lcg

--- algorithms/linear_congruential_generator.kt
+++ algorithms/linear_congruential_generator.kt
@@ -1,6 +1,10 @@
 // The Linear Congruential Generator is one of the oldest and simplest algorithms for generating pseudorandom numbers.
 // The algorithm works by following a linear formula: Xn+1​ = (a×Xn​+c) mod m
 
+// Suppose you are creating a virtual dice-rolling application for a board game.
+// You need a simple and efficient way to generate random numbers that simulate the roll of a dice.
+// The Linear Congruential Generator is a method used to produce a sequence of pseduorandomized numbers based on a linear formula, which is perfect for such a use case.
+
 class LinearCongruentialGenerator(seed: Long) {
     private var current: Long = seed
     private val a = 1664525L
