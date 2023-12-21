ba2aac97599dd8e8be0338075e84b21c13258f5f
metindokmen <kindmemento@gmail.com>
2023-12-21 14:25:15 +0300

    add main fun, get current time for randomness, pass it as seed to lcg, generate 10 random numbers and print them to standard output

--- algorithms/linear_congruential_generator.kt
+++ algorithms/linear_congruential_generator.kt
@@ -12,3 +12,13 @@ class LinearCongruentialGenerator(seed: Long) {
         return current.toInt()
     }
 }
+
+fun main() {
+    val seed = System.currentTimeMillis() // Using current time as seed for randomness
+    val lcg = linearCongruentialGenerator(seed)
+
+    // Generate 10 random numbers
+    repeat(10) {
+        println(lcg.nextInt())
+    }
+}
