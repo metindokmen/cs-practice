c327f60b07ce9e4f1b88e1946e7374cce558ff50
metindokmen <kindmemento@gmail.com>
2023-12-21 14:29:54 +0300

    give real life example for vigenere cipher algorithm

--- algorithms/vigenere_cipher.kt
+++ algorithms/vigenere_cipher.kt
@@ -1,3 +1,7 @@
 // The Vigenere Cipher is a method of encrypting alphabetic text by using a simple form of polyalphabetic substitution.
 // A keyword is used to determine the shift for each letter in the plaintext.
 // For each letter in the plaintext, you shift it by the number of positions indicated by the corresponding letter in the keyword, repeating the keyword as necessary.
+
+// Imagine you're sending confidential messages in a scenario where basic encryption is needed.
+// While simple ciphers like Caesar's cipher are easy to break due to their predictability, the Vigenere Cipher provides a more secure alternative.
+// It uses a keyword to vary the shift of each letter in the plaintext, making it less susceptible to traditional frequency analysis attacks.
