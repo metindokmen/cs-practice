799a926d27e69326d19023a5ff98419877e6d699
metindokmen <kindmemento@gmail.com>
2023-12-21 14:28:43 +0300

    explain vigenere cipher algorithm in comments

---
+++ algorithms/vigenere_cipher.kt
@@ -0,0 +1,3 @@
+// The Vigenere Cipher is a method of encrypting alphabetic text by using a simple form of polyalphabetic substitution.
+// A keyword is used to determine the shift for each letter in the plaintext.
+// For each letter in the plaintext, you shift it by the number of positions indicated by the corresponding letter in the keyword, repeating the keyword as necessary.
