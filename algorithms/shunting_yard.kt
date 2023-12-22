e258ce15ed977a31f278ddaa7d333bf5387d41dd
metindokmen <kindmemento@gmail.com>
2023-12-23 00:42:56 +0300

    give real life example use case for shunting yard algorithm

--- algorithms/shunting_yard.kt
+++ algorithms/shunting_yard.kt
@@ -1,2 +1,6 @@
 // The Shunting Yard Algorithm, devised by Edsger Dijkstra, is used to parse mathematical expressions specified in infix notation (e.g., "3 + 4") and produce an equivalent postfix notation expression (e.g., "3 4 +").
 // This postfix notation is easier to evaluate algorithmically without the need for precedence rules or parantheses.
+
+// Imagine you're developing a calculator app that needs to handle complex mathematical expressions input by the user, such as "3 + 4 * 2 / (1 - 5) ^ 2 ^ 3".
+// The challenge is to correctly interpret the order of operations, including paranthesis.
+// The Shunting Yard Algorithm is used to convert expressions from infix notation (which is human-readable) to postfix notation (which is easier for a computer to evaluate).
