e81e009f51c637c04e321b04178f639b0f041c02
metindokmen <kindmemento@gmail.com>
2023-12-23 00:36:07 +0300

    implement runLengthEncode algorithm

--- algorithms/run-length_encoding.kt
+++ algorithms/run-length_encoding.kt
@@ -4,3 +4,24 @@
 // Imagine you have a document scanning app, and you need to compress black-and-white images where large areas of the image are the same color.
 // In such cases, it's inefficient to store each pixel's color individually.
 // Run-Length Encoding (RLE) offers a solution by encoding consecutive elements (pixels) of the same value (color) as a single data value and count.
+
+fun runLengthEncode(input: String): String {
+    if (input.isEmpty()) return ""
+
+    val sb = StringBuilder()
+    var count = 1
+    var prev = input[0]
+
+    for (i in 1 until input.length) {
+        val current = input[i]
+        if (current == prev) {
+            count++
+        } else {
+            sb.append(prev).append(count)
+            count = 1
+            prev = current
+        }
+    }
+    sp.append(prev).append(count)
+    return sb.toString()
+}
