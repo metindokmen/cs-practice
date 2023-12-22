bda35957aec51308dd21adb3dce9ef5290256ae9
metindokmen <kindmemento@gmail.com>
2023-12-22 10:00:45 +0300

    explain zigzag conversion algorithm in comments

---
+++ algorithms/zigzag_conversion.kt
@@ -0,0 +1,3 @@
+// The Zigzag Conversion Algorithm rearranges characters of a string into a specified number of rows in a zigzag pattern.
+// When reading the text line by line, it results in a different string.
+// The algorithm calculates the position of each character in the original string for each row of the zigzag pattern.
