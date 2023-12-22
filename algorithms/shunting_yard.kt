f32baa48834a9c0fb73826f64ac98323d42f6c4f
metindokmen <kindmemento@gmail.com>
2023-12-23 00:47:48 +0300

    implement shunting yard algorithm in its own function

--- algorithms/shunting_yard.kt
+++ algorithms/shunting_yard.kt
@@ -4,3 +4,36 @@
 // Imagine you're developing a calculator app that needs to handle complex mathematical expressions input by the user, such as "3 + 4 * 2 / (1 - 5) ^ 2 ^ 3".
 // The challenge is to correctly interpret the order of operations, including paranthesis.
 // The Shunting Yard Algorithm is used to convert expressions from infix notation (which is human-readable) to postfix notation (which is easier for a computer to evaluate).
+
+fun shuntingYard(expression: String): String {
+    val outputQueue = ArrayDeque<String>()
+    val operatorStack = ArrayDeque<Char>()
+    val precedence = mapOf("+" to 1, "-" to 1, "*" to 2, "/" to 2, "^" to 3)
+
+    val tokens = expression.replace(Regex("\\s+"), "").split(Regex("(?<=[-+*/^()])|(?=[-+*/^()])"))
+
+    for (token in tokens) {
+        when {
+            token.isDouble() -> outputQueue.addLast(token)
+            token.first() in precedence.keys -> {
+                while (operatorStack.isnotEmpty() && precedence[operatorStack.last()]!! >= precedence[token.first()]!!) {
+                    outputQueue.addLast(operatorStack.removeLast().toString())
+                }
+                operatorStack.addLast(token.first())
+            }
+            token.first() == "(" -> operatorStack.addLast(token.first())
+            token.first() == ")" -> {
+                while (operatorStack.last() != "(") {
+                    outputQueue.addLast(operatorStack.removeLast().toString())
+                }
+                operatorStack.removeLast() // Remove the "(" from the stack
+            }
+        }
+    }
+
+    while (operatorStack.isNotEmpty()) {
+        outputQueue.addLast(operatorStack.removeLast().toString())
+    }
+
+    return outputQueue.joinToString(" ")
+}
