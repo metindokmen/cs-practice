4b0ca04325b3bd1eb6aa96209c13380c625e7824
metindokmen <kindmemento@gmail.com>
2023-12-22 10:02:31 +0300

    give real life example use case for zigzag conversion algorithm

--- algorithms/zigzag_conversion.kt
+++ algorithms/zigzag_conversion.kt
@@ -1,3 +1,11 @@
 // The Zigzag Conversion Algorithm rearranges characters of a string into a specified number of rows in a zigzag pattern.
 // When reading the text line by line, it results in a different string.
 // The algorithm calculates the position of each character in the original string for each row of the zigzag pattern.
+
+// Imagine you're developing a text formatting tool that needs to display text in a zigzag pattern for stylistic purposes.
+// For instance, the word "KOTLIN" could be displayed in a two-row zigzag like this:
+
+//K   I
+//O T L N
+
+// This kind of pattern is common in certain types of artistic text rendering or playful user interfaces.
