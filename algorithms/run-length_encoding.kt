6849b2b5867e72540d0a9cb9b67d8514ca34ab81
metindokmen <kindmemento@gmail.com>
2023-12-23 00:33:17 +0300

    explain run-length encoding algorithm in comments

---
+++ algorithms/run-length_encoding.kt
@@ -0,0 +1,2 @@
+// Run Length Encoding is a data compression technique in which consecutive data elements (pixels, characters, etc.) are replaced by just one data value and a count.
+// It's most efficient on data that contains many such runs, makint it well-suited for compressing images, text documents with many spaces, or any data with repeated elements.
