0c7fb1cc9eca0c1b3699f357c82a77d81fa3ee22
metindokmen <kindmemento@gmail.com>
2023-12-22 09:50:45 +0300

    explain interpolation search in comments

---
+++ algorithms/interpolation_search.kt
@@ -0,0 +1,3 @@
+// Interpolation Search is an algorithm for searching for a key in an array that has been ordered numerical values assigned to the keys (key values).
+// It works on the principle of estimating the position of the key based on the lowest and highest elements in the array and the value of the key.
+// It's particularly efficient for uniformly distributed data, where it can achieve O(log log n) complexity, as opposed to Binary Search's O(log n).
