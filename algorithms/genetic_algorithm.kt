d7b235ec36ae64bc032d44a82c1600c3b3c12aa2
metindokmen <kindmemento@gmail.com>
2023-12-25 01:16:18 +0300

    explain genetic algorithms in comments

---
+++ algorithms/genetic_algorithm.kt
@@ -0,0 +1,3 @@
+// Genetic Algorithms (GAs) are adaptive heuristic search algorithms based on the evolutionary ideas of natural selection and genetics.
+// They represent an intelligent exploitation of a random search used to solve optimization problems.
+// Essentially, you start with a population of random solutions (chromosomes), evolve the population over several generations, and select the best ones.
