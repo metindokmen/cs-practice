3691484ee812514223ffbe668c90ac1b7cec0767
metindokmen <kindmemento@gmail.com>
2023-12-24 18:48:48 +0300

    give real life example use case for hungarian algorithm

--- algorithms/hungarian_algorithm.kt
+++ algorithms/hungarian_algorithm.kt
@@ -1,2 +1,7 @@
 // The Hungarian Algorithm, also known as the Kuhn-Munkres Algorithm, is a combinatorial optimization algorithm that solves the assignment problem in polynomial time.
 // It's used to find the optimal assignment that either maximizes the sum of weights (in the case of a maximization problem) or minimizes the sum of costs (in a minimization problem) across all pairs.
+
+// Imagine you're organizing a sports tournament and need to assign teams to different time slots on various fields.
+// Each team has a preference for certain times and fields, and each field-time slot combination can only accomodate one team.
+// The goal is to find the assignment that maximizes overall satisfaction or minimizes total cost.
+// The Hungarian Algorithm is perfect for this kind of optimization problem, commonly known as the assignment problem.
