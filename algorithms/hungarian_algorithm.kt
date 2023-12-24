be6d17ceb01fba7b7843210682cd8df9ab4b89f9
metindokmen <kindmemento@gmail.com>
2023-12-24 18:47:24 +0300

    explain hungarian algorithm in comments

---
+++ algorithms/hungarian_algorithm.kt
@@ -0,0 +1,2 @@
+// The Hungarian Algorithm, also known as the Kuhn-Munkres Algorithm, is a combinatorial optimization algorithm that solves the assignment problem in polynomial time.
+// It's used to find the optimal assignment that either maximizes the sum of weights (in the case of a maximization problem) or minimizes the sum of costs (in a minimization problem) across all pairs.
