8e1037d4fcd4fe3091875e8b85e9cc24f7c3e1b8
metindokmen <kindmemento@gmail.com>
2023-12-24 18:59:32 +0300

    explain rod cutting problem in comments

---
+++ algorithms/rod_cutting_problem.kt
@@ -0,0 +1,2 @@
+// The Rod Cutting Problem is a classic example of an optimization problem solved using Dynamic Programming.
+// The idea is to make cuts at different positions and compare the total revenue obtained from those cuts, storing the results of sub-problems to avoid re-computation.
