3987ca4c97df9ec38254ab6df022e189484351de
metindokmen <kindmemento@gmail.com>
2023-12-25 01:07:46 +0300

    explain miller-rabin primality test algorithm in comments

---
+++ algorithms/miller-rabin_primality_test.kt
@@ -0,0 +1,4 @@
+// The Miller-Rabin Primality Test is a probabilistic algorithm that determines whether a given number is prime.
+// It's an enhancement over simpler deterministic tests due to its efficiency with large numbers.
+// While it doesn't conclusively prove a number is prime, it can tell with high probability whether a number is composite.
+// For cryptographic applications, this level of certainty isusually sufficient.
