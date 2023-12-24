a3cd5b089e84daf18d4d0a49f474645954f27a32
metindokmen <kindmemento@gmail.com>
2023-12-25 01:17:36 +0300

    give real life example use case for genetic algorithms

--- algorithms/genetic_algorithm.kt
+++ algorithms/genetic_algorithm.kt
@@ -1,3 +1,7 @@
 // Genetic Algorithms (GAs) are adaptive heuristic search algorithms based on the evolutionary ideas of natural selection and genetics.
 // They represent an intelligent exploitation of a random search used to solve optimization problems.
 // Essentially, you start with a population of random solutions (chromosomes), evolve the population over several generations, and select the best ones.
+
+// Imagine you're designing an AI for a racing game, where the goal is to find the optimal path and speed for completing a track in the shortest time.
+// There are countless combinations of turns, accelerations, and braking points.
+// A genetic algorithm can evolve solutions over generations, much like natural selection, to find an optimal or near-optimal solution that represents the best racing strategy.
