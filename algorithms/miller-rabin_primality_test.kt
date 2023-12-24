8918a4aa490b516ddaba51f9892dccb6d8fd7a77
metindokmen <kindmemento@gmail.com>
2023-12-25 01:09:24 +0300

    give real life example use case for miller-rabin primality test algorithm

--- algorithms/miller-rabin_primality_test.kt
+++ algorithms/miller-rabin_primality_test.kt
@@ -2,3 +2,7 @@
 // It's an enhancement over simpler deterministic tests due to its efficiency with large numbers.
 // While it doesn't conclusively prove a number is prime, it can tell with high probability whether a number is composite.
 // For cryptographic applications, this level of certainty isusually sufficient.
+
+// Imagine you're developing a security application where you need to generate large prime numbers for encryption keys, like in RSA encryption.
+// Checking if large numbers are prime using simple methods (like trial division) is computationally expensive.
+// The Miller-Rabin Primality Test provides a probabilistic method to quickly check if a number is prime, which is particularly useful for large numbers.
