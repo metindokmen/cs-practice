89bf5e90bffe4131a40fc18a4450f047691c1109
metindokmen <kindmemento@gmail.com>
2023-12-26 00:15:06 +0300

    give real life example for minimax algorithm

--- algorithms/minimax.kt
+++ algorithms/minimax.kt
@@ -1,2 +1,7 @@
 // Minimax is a backtracking algorithm used in decision-making and game theory to find the optimal movel for a player, assuming the opponent also plays optimally.
 // Alpha-Beta Pruning is an optimization technique to the Minimax Algorithm that significantly reduces the number of nodes evaluated.
+
+// Imagine you're developing an AI for a game like chess or tic-tac-toe.
+// Each player can make a series of moves that lead to either winning, losing, or drawing the game.
+// The AI needs to evaluate the best possible move, assuming the opponent is also playing optimally.
+// The Minimax Algorithm is used here, exploring all possible moves, their consequences, and ultimately choosing the move that maximizes the AI's chance of winning, while minimizing the opponent's.
