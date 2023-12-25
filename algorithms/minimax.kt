5982c5f6d467eb153451339362153973e2532731
metindokmen <kindmemento@gmail.com>
2023-12-26 00:13:35 +0300

    explain minimax algorithm in comments

---
+++ algorithms/minimax.kt
@@ -0,0 +1,2 @@
+// Minimax is a backtracking algorithm used in decision-making and game theory to find the optimal movel for a player, assuming the opponent also plays optimally.
+// Alpha-Beta Pruning is an optimization technique to the Minimax Algorithm that significantly reduces the number of nodes evaluated.
