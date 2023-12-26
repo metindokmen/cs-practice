2b96d0173f800a2337a4142756d0f09acb2fc430
metindokmen <kindmemento@gmail.com>
2023-12-27 00:36:38 +0300

    add data class Point, and implement fun quadraticBezier

--- algorithms/bezier_curve.kt
+++ algorithms/bezier_curve.kt
@@ -6,3 +6,11 @@
 // Imagine you're developing a graphic design tool and need to allow users to draw smooth, curved lines that can be easily adjusted.
 // Bézier curves are perfect for this purpose. They are defined by a set of control points, and the curve is drawn by linearly interpolating these points.
 // This makes them very useful in vector graphic design software, computer animation, and user interface design.
+
+data class Point(val x: Double, val y: Double)
+
+fun quadraticBezier(p0: Point, p1: Point, p2: Point, t: Double): Point {
+    val x = (1 - t).pow(2) * p0.x + 2 * (1 - t) * t * p1.x + t.pow(2) * p2.x
+    val y = (1 - t).pow(2) * p0.y + 2 * (1 - t) * t * p1.y + t.pow(2) * p2.y
+    return Point(x, y)
+}
