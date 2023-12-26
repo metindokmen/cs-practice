fd14369b20490890a2f8111bc8745b24291ff735
metindokmen <kindmemento@gmail.com>
2023-12-27 00:34:48 +0300

    give real life example for bezier curves

--- algorithms/bezier_curve.kt
+++ algorithms/bezier_curve.kt
@@ -2,3 +2,7 @@
 // The simplest form is the linear Bézier curve, which is a straight line.
 // The quadratic and cubic Bézier curvers are more commonly used, defined by three and four points, respectively.
 // The algorithm uses a recursive approach to divide the curve into smaller segments, each approximating the curve.
+
+// Imagine you're developing a graphic design tool and need to allow users to draw smooth, curved lines that can be easily adjusted.
+// Bézier curves are perfect for this purpose. They are defined by a set of control points, and the curve is drawn by linearly interpolating these points.
+// This makes them very useful in vector graphic design software, computer animation, and user interface design.
