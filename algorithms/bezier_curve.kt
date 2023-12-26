f16d8e894c6e9569a2ee833fdcf2e1374f67e15b
metindokmen <kindmemento@gmail.com>
2023-12-27 00:33:35 +0300

    explain bezier curve algorithm in comments

---
+++ algorithms/bezier_curve.kt
@@ -0,0 +1,4 @@
+// A Bézier Curve is a parametric curve frequently used in computer graphics and related fields.
+// The simplest form is the linear Bézier curve, which is a straight line.
+// The quadratic and cubic Bézier curvers are more commonly used, defined by three and four points, respectively.
+// The algorithm uses a recursive approach to divide the curve into smaller segments, each approximating the curve.
