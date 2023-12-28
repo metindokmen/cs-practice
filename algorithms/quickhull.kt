86b784e2e670f69894b5758ccf9855857bd3fd81
metindokmen <kindmemento@gmail.com>
2023-12-29 00:15:37 +0300

    explain quickhull algorithm in comments

---
+++ algorithms/quickhull.kt
@@ -0,0 +1,3 @@
+// Quickhull is a method of computing the convex hull of a finite set of points in the plane.
+// It is similar to the divide and conquer approach of QuickSort, hence the name.
+// The algorithm works by finding extreme points and recursively finding other extreme points in subsets of the input set.
