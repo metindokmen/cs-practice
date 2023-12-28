30dab9d044805be375f56cdd95ea08ec0116f9dd
metindokmen <kindmemento@gmail.com>
2023-12-28 05:40:15 +0300

    explain monte carlo simulation algorithm in comments

---
+++ algorithms/monte_carlo_simulation.kt
@@ -0,0 +1,3 @@
+// Monte Carlo Simulation is a computational algorithm that relis on repeated random sampling to obtain numerical results.
+// The underlying concept is to use randomness to solve problems that might be deterministic in principle.
+// It's often used when it's infeasible or impossible to compute an exact result with a deterministic algorithm.
