adc454fcf37f7359c028aeecb15cc619490e4e65
metindokmen <kindmemento@gmail.com>
2023-12-29 00:16:59 +0300

    give real life example use case for quickhull algorithm

--- algorithms/quickhull.kt
+++ algorithms/quickhull.kt
@@ -1,3 +1,7 @@
 // Quickhull is a method of computing the convex hull of a finite set of points in the plane.
 // It is similar to the divide and conquer approach of QuickSort, hence the name.
 // The algorithm works by finding extreme points and recursively finding other extreme points in subsets of the input set.
+
+// Imagine you are developing a geographic mapping application and need to determine the boundary that encompasses a set of points (like cities on a map).
+// The convex hull is the smallest convex shape that contains all the points.
+// Quickhull is an algorithm that efficiently finds this convex hull, similar to how an elastic band would snap around the outermost points.
