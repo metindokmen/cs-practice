8ab60236cce340122fa7679f42c4527dd1e27889
metindokmen <kindmemento@gmail.com>
2023-12-30 00:59:11 +0300

    fix typos in abbreviations

--- algorithms/baum-welch.kt
+++ algorithms/baum-welch.kt
@@ -1,6 +1,6 @@
-// The baum-Welch Algorithm is an iterative algorithm used to train Hidden Markov Models (HHMs).
+// The baum-Welch Algorithm is an iterative algorithm used to train Hidden Markov Models (HMMs).
 // It's a specific case of the more general Expectation-Maximization algorithm.
-// The algorithm finds the unknown parameters (transition and observation probabilities) of an HHM that maximize the likelihood of a given set of observed sequences.
+// The algorithm finds the unknown parameters (transition and observation probabilities) of an HMM that maximize the likelihood of a given set of observed sequences.
 
 // Imagine you're developing a speech recognition system.
 // The system needs to understand spoken language, which involves recognizing sequences of sounds and interpreting them as words.
@@ -9,11 +9,11 @@
 // The Baum-Welch Algorithm helps by adjusting the model's parameters to best fit a set of observed training data (speech samples).
 
 class HiddenMarkovModel {
-    // Placeholder for HHM parameters: states, observations, transition probabilities, etc.
+    // Placeholder for HMM parameters: states, observations, transition probabilities, etc.
 }
 
-fun baumWelchAlgorithm(hhm: HiddenMarkovModel, observedSequences: List<List<String>>) {
-    // Initialize HHM parameters if not already initialized
+fun baumWelchAlgorithm(hmm: HiddenMarkovModel, observedSequences: List<List<String>>) {
+    // Initialize HMM parameters if not already initialized
 
     do {
         // E-step: Estimate probabilities using current parameters
