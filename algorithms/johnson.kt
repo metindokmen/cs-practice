f8bb4a009ac2d2a865868e2266240539735b4f3b
metindokmen <kindmemento@gmail.com>
2023-12-30 00:46:51 +0300

    implement johnsons algorithm

--- algorithms/johnson.kt
+++ algorithms/johnson.kt
@@ -7,3 +7,14 @@
 // Some streets have time-based restrictions (negative weights), like faster movement during non-peak hours.
 // However, you want to avoid routes that have a total negative weight to prevent infinite loops (negative weight cycles).
 // Johnson's Algorithm can efficiently find the shortest paths between all pairs of vertices in such a weighted graph, even with some negative weights.
+
+fun johnsonsAlgorithm(graph: Graph): Map<Pair<Int, Int>, Int> {
+    // Add a new vertex to the graph and connect it to all other vertices with zero-weight edges
+    // Use Bellman-Ford algorithm to calculate the shortest path from this new vertex to all others
+    // Reweight the graph using the shortest path distances obtained
+    // Remove the additional vertex
+    // Run dijkstra's algorithm for each vertex
+
+    / Placeholder for implementation
+    return mapOf() // Return a map of shortest paths
+}
