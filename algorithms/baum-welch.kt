710ef62678e0c99ad2b4fcfeb2f4d667993a86f0
metindokmen <kindmemento@gmail.com>
2023-12-30 00:51:26 +0300

    explain baum-welch algorithm in comments

---
+++ algorithms/baum-welch.kt
@@ -0,0 +1,3 @@
+// The baum-Welch Algorithm is an iterative algorithm used to train Hidden Markov Models (HHMs).
+// It's a specific case of the more general Expectation-Maximization algorithm.
+// The algorithm finds the unknown parameters (transition and observation probabilities) of an HHM that maximize the likelihood of a given set of observed sequences.
