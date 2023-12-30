a86c6208c032e0485e851889e0a8031acb37b488
metindokmen <kindmemento@gmail.com>
2023-12-31 01:38:15 +0300

    explain brents algorithm in comments

---
+++ algorithms/brent.kt
@@ -0,0 +1,3 @@
+// Brent's Algorithm is an algorithm for cycle detection in sequences.
+// It is similar to Floyd's cycle-finding algorithm but more efficient, as it requires fewer function evaluations.
+// Bren't Algorithm determines the length of the cycle and the position where the cycle starts in a sequence.
