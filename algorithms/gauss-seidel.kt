6557de68456a5904320d09d15c91832def59492b
metindokmen <kindmemento@gmail.com>
2024-01-01 01:52:34 +0300

    give real life example for gauss-seidel method

--- algorithms/gauss-seidel.kt
+++ algorithms/gauss-seidel.kt
@@ -2,3 +2,8 @@
 // It is particularly useful for large systems where direct methods are not feasible.
 // The method assumes the system is in the form Ax=b and iteratively approximates the solution x.
 // It is an improvement over the Jacobi method in terms of convergence speed.
+
+// Imagine you're an engineer working on a simulation of physical forces in a building structure.
+// Your task involves solving a large system of linear equations that represent varius physical forces and constratins.
+// Direct methods like Gaussian elimination can be computationally expensive for large systems.
+// The Gauss-Seidel Method provides an iterative approach to approximate the solution more efficiently.
