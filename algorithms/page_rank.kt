a51bf418d13587c475401fbc55ed5d79add5f960
metindokmen <kindmemento@gmail.com>
2024-01-01 01:57:53 +0300

    explain page rank algorithm in comments

---
+++ algorithms/page_rank.kt
@@ -0,0 +1,3 @@
+// The PageRank algorithm is a way of measuring the importance of website pages.
+// Developed by Larry Page and Sergey Brin while at Stanford University, it assigns a numerical weighting to each element of a hyperlinked set of documents,
+// such as the world Wide Web, with the purpose of measuring its relative importance within the set.
