ba588f8e6646cd858cf9cba45d7a0a902d548a2e
metindokmen <kindmemento@gmail.com>
2024-01-01 01:58:57 +0300

    give real life example use case for page rank algorithm

--- algorithms/page_rank.kt
+++ algorithms/page_rank.kt
@@ -1,3 +1,7 @@
 // The PageRank algorithm is a way of measuring the importance of website pages.
 // Developed by Larry Page and Sergey Brin while at Stanford University, it assigns a numerical weighting to each element of a hyperlinked set of documents,
 // such as the world Wide Web, with the purpose of measuring its relative importance within the set.
+
+// Imagine you're creating a search engine, and you need to rank websites based on their importance and relevance.
+// The PageRank algorithm can help with this.
+// It ranks each website based on the number and quality of links to it, under the assumption that more important websites are likely to receive more links from other websites.
