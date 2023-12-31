ff2481c41417592e3b2bb04894854a208b0d9026
metindokmen <kindmemento@gmail.com>
2024-01-01 01:50:57 +0300

    explain gauss-seidel method in comments

---
+++ algorithms/gauss-seidel.kt
@@ -0,0 +1,4 @@
+// The Gauss-Seidel Method is an iterative technique used for solving a system of linear equations.
+// It is particularly useful for large systems where direct methods are not feasible.
+// The method assumes the system is in the form Ax=b and iteratively approximates the solution x.
+// It is an improvement over the Jacobi method in terms of convergence speed.
