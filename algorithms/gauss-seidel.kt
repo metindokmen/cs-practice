6ef89ed185814ab4d41cef8c108d8a97d98db208
metindokmen <kindmemento@gmail.com>
2024-01-01 01:54:15 +0300

    implement gaussSeidel function

--- algorithms/gauss-seidel.kt
+++ algorithms/gauss-seidel.kt
@@ -7,3 +7,21 @@
 // Your task involves solving a large system of linear equations that represent varius physical forces and constratins.
 // Direct methods like Gaussian elimination can be computationally expensive for large systems.
 // The Gauss-Seidel Method provides an iterative approach to approximate the solution more efficiently.
+
+fun gaussSeidel(a: Array<DoubleArray>, b: DoubleArray, numIterations: Int): DoubleArray {
+    val n = a.size
+    val x = DoubleArray(n)
+
+    for (iteration in 0 until numIterations) {
+        for (i in 0 until n) {
+            var sum = b[i]
+            for (j in 0 until n) {
+                if (i != j) {
+                    sum -= a[i][j] * x[j]
+                }
+            }
+            x[i] = sum / a[i][i]
+        }
+    }
+    return x
+}
