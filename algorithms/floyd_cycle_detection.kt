6010342b804abe82ec56523b768e6a9e6080a663
metindokmen <kindmemento@gmail.com>
2024-01-02 00:43:00 +0300

    explain floyds cycle detection algorithm in comments

---
+++ algorithms/floyd_cycle_detection.kt
@@ -0,0 +1,4 @@
+// Floyd's Cycle Detection Algorithm is an algorithm used to detect cycles in a sequence, such as a linked list.
+// It uses two pointers, usually called the "tortoise" and the "hare", which move at different speeds.
+// The hare moves twice as fast as the tortoise.
+// If there is a cycle, the hare will eventually meet the tortoise again somewhere inside the cycle.
