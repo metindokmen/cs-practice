fc0a25886d4f651bad64a5183af8ed4c39cc712f
metindokmen <kindmemento@gmail.com>
2024-01-02 00:26:21 +0300

    give real life example use case for decision trees

--- algorithms/decision_tree.kt
+++ algorithms/decision_tree.kt
@@ -3,3 +3,6 @@
 // The topmost node in a decision tree is known as the root node.
 // The algorithm splits the data into subsets based on the value of the input features,
 // and this process is repeated recursively, resulting in a tree-lioke model of decisions.
+
+// Imagine you are developing a system to predict whether a customer will buy a particular product based on various attributes like age, income, browsing history, and so on.
+// A decision tree can help by creating a model that makes predictions based on the decision rules inferred from the data.
