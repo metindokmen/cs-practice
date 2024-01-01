bc5b5efde1d33c7cf56656b60d72a56b744fa114
metindokmen <kindmemento@gmail.com>
2024-01-02 00:44:21 +0300

    give real life example use case for floyd cycle detection algorithm

--- algorithms/floyd_cycle_detection.kt
+++ algorithms/floyd_cycle_detection.kt
@@ -2,3 +2,7 @@
 // It uses two pointers, usually called the "tortoise" and the "hare", which move at different speeds.
 // The hare moves twice as fast as the tortoise.
 // If there is a cycle, the hare will eventually meet the tortoise again somewhere inside the cycle.
+
+// Imagine you're creating a fitness tracking application that includes a feature for detecting repetitive patterns or loops in a user's running route.
+// Suppose the GPS data forms a linked list where each point is a node; you want to determine if the runner is moving in a loop (circular route) or not.
+// Floyd's Cycle Detection Algorithm can efficiently identify if the linked list of GPS data points has a cycle.
