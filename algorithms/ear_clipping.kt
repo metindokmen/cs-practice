585f242c15c6654b11bf2d9faf79e2ae5aaea322
metindokmen <kindmemento@gmail.com>
2024-01-03 14:07:51 +0300

    explain ear clipping algorithm in comments

---
+++ algorithms/ear_clipping.kt
@@ -0,0 +1,3 @@
+// The Ear Clipping Algorithm is a method for triangulating a polygon by dividing it into triangles.
+// It works by finding 'ears' in the polygon, which are triangles that can be cut off (or 'clipped') without cutting into the rest of the polygon.
+// An 'ear' is a set of three consecutive vertices A, B, C where the triangle ABC is entirely inside the polygon, and no other vertices of the polygon are inside ABC.
