4a3aa89d6bcc02120f5356fb21c964284b805bb3
metindokmen <kindmemento@gmail.com>
2024-01-03 14:08:58 +0300

    give real life example use case for ear clipping algorithm

--- algorithms/ear_clipping.kt
+++ algorithms/ear_clipping.kt
@@ -1,3 +1,7 @@
 // The Ear Clipping Algorithm is a method for triangulating a polygon by dividing it into triangles.
 // It works by finding 'ears' in the polygon, which are triangles that can be cut off (or 'clipped') without cutting into the rest of the polygon.
 // An 'ear' is a set of three consecutive vertices A, B, C where the triangle ABC is entirely inside the polygon, and no other vertices of the polygon are inside ABC.
+
+// Imagine you're developing a 3D modeling software where users can create complex shapes.
+// To render these shapes efficiently, especially for non-convex polygons, you need to break them down into simpler components.
+// The Ear Clipping Algorithm can triangulate a polygon, dividing it into triangles, which are the simplest shapes for rendering in computer graphics.
