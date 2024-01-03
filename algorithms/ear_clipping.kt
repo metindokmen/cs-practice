ee95fd7b6d64d15edc1fc12594f7ed205a8eb8e4
metindokmen <kindmemento@gmail.com>
2024-01-03 14:10:22 +0300

    define point data class, implement isConvex checker function

--- algorithms/ear_clipping.kt
+++ algorithms/ear_clipping.kt
@@ -5,3 +5,10 @@
 // Imagine you're developing a 3D modeling software where users can create complex shapes.
 // To render these shapes efficiently, especially for non-convex polygons, you need to break them down into simpler components.
 // The Ear Clipping Algorithm can triangulate a polygon, dividing it into triangles, which are the simplest shapes for rendering in computer graphics.
+
+data class Point(val x: Double, val y: Double)
+
+fun isConvex(p: Point, q: Point, r: Point): Boolean {
+    val crossProduct = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+    return crossProduct < 0
+}
