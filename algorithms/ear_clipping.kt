91104274bbf237d505f4e9ace9e61e32ac501635
metindokmen <kindmemento@gmail.com>
2024-01-03 14:16:46 +0300

    implement earClipping algorithm

--- algorithms/ear_clipping.kt
+++ algorithms/ear_clipping.kt
@@ -24,6 +24,38 @@ fun isEar(p: Point, q: Point, r: Point, remainingVertices: List<Point>): Boolean
     return true
 }
 
+fun earClipping(vertices: MutableList<Point>): List<Triple<Point, Point, Point>> {
+    val triangles = mutableListOf<Triple<Point, Point, Point>>()
+    val remainingVertices = vertices.toMutableList()
+
+    while (remainingVertices.size > 3) {
+        var earFound = false
+
+        for (i in remainingVertices.indices) {
+            val prevIndex = if (i == 0) remainingVertices.size - 1 else i - 1
+            val nextIndex = if (i == remainingVertices.size - 1) 0 else i + 1
+
+            val p = remainingVertices[prevIndex]
+            val q = remainingVertices[i]
+            val r = remainingVertices[nextIndex]
+
+            if (isEar(p, q, r, remainingVertices)) {
+                triangles.add(Triple(p, q, r))
+                remainingVertices.removeAt(i)
+                earFound = true
+                break
+            }
+        }
+
+        if (!earFound) {
+            throw Exception("No ear found. The polygon might be self-intersecting.")
+        }
+    }
+
+    triangles.add(Triple(remainingVertices[0], remainingVertices[1], remainingVertices[2]))
+    return triangles
+}
+
 fun isInsideTriangle(p: Point, q: Point, r: Point, s: Point): Boolean {
     // Implement point-in-triangle test (e.q., using barycentric coordinates)
     return true // Placeholder
