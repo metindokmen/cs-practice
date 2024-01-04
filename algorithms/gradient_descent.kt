7c83403d59bed9bf4850ef399a2a90ab8542ab3e
metindokmen <kindmemento@gmail.com>
2024-01-04 14:49:52 +0300

    implement linear regression class with methods fit and predict

--- algorithms/gradient_descent.kt
+++ algorithms/gradient_descent.kt
@@ -5,3 +5,29 @@
 // Imagine you are developing a machine learning model to predict housing prices based on features like size, location, and number of bedrooms.
 // The challenge is to find the best model parameters (weights) that minimze the prediction error.
 // Gradient Descent is an iterative optimization algorithm used to find the minimum of a function (in this case, the loss function that measures prediction errors).
+
+class LinearRegression(val learningRate: Double, val iterations: Int) {
+    var weights: DoubleArray = doubleArrayOf()
+
+    fun fit(X: Array<DoubleArray>, y: DoubleArray) {
+        val nFeatures = X[0].size
+        weights = DoubleArray(nFeatures) { 0.0 }
+
+        for (i in 0 until iterations) {
+            val gradients = DoubleArray(nFeatures) { 0.0 }
+            for (j in X.indices) {
+                val prediction = predict(X[j])
+                for (k in gradients.indices) {
+                    gradients[k] += (prediction - y[j]) * X[j][k]
+                }
+            }
+            for (k in weights.indices) {
+                weights[k] -= learningRate * gradients[k] / X.size
+            }
+        }
+    }
+
+    fun predict(x: DoubleArray): Double {
+        return weights.zip(x).sumOf { (weight, feature) -> weight * feature }
+    }
+}
