3d61d1f11282f78f67d9fd6ab025cdc214d9332e
metindokmen <kindmemento@gmail.com>
2024-01-04 14:45:36 +0300

    explain gradient descent algorithm in comments

---
+++ algorithms/gradient_descent.kt
@@ -0,0 +1,3 @@
+// Gradient Descent is a first-order iterative optimization algorithm used to find the minimum of a function.
+// Specifically, in machine learning, it's used to minimize a loss function by iteratively moving towards the steepest descent as defined by the negative of the gradient.
+// It's fundamental in training many types of machine learning models.
