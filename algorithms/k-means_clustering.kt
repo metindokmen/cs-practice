46226a033019b95e0b7b83c863e30561e0c573a0
metindokmen <kindmemento@gmail.com>
2024-01-06 00:15:48 +0300

    give real life example for k-means clustering algorithm

--- algorithms/k-means_clustering.kt
+++ algorithms/k-means_clustering.kt
@@ -1,2 +1,6 @@
 // K-Means Clustering is an unsupervised machine learning algorithm that groups data into 'k' numbers of clusters.
 // The algorithm initializes 'k' centroids randomly, then iteratively assigns each data point to the nearest centroid and recomputes the centroid of each cluster until convergence or for a fixed number of iterations.
+
+// Imagine you're analyzing cutomer data for a marketing campaign.
+// You want to segment customers into distinct groups based on similarities in their purchasing behavior, demographics, or other relevant characteristics.
+// The K-Means algorithm can cluster these customers into groups (or 'clusters') that exhibit similar traits, helping tailor marketing strategies to different customer segments.
