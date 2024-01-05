c061e0644b67efcc0d680b4449734bcf4d5f3974
metindokmen <kindmemento@gmail.com>
2024-01-05 14:21:15 +0300

    give real life example use case for apriori algorithm

--- algorithms/apriori.kt
+++ algorithms/apriori.kt
@@ -1,3 +1,7 @@
 // The Apriori Algorithm is used for mining frequent itemsets and learning association rules in large databses.
 // It's based on the principle that a subset of a frequent itemset must also be a frequent itemset.
 // The algorithm iteratively identifies the frequent individual items in the database and extends them to larger itemsets as long as those itemsets appear sufficiently often in the database.
+
+// Imagine you are a data analyst in a retail company, and you want to understand the purchasing behavior of customers to enhance cross-selling strategies.
+// For instance, you might want to find out which products are often bought together, like break and butter.
+// The Apriori Algorithm can identify these associations or frequent itemsets in transaction databases, enabling effective recommendation systems or targeted marketing.
