ffa4956feebb173cbe20835aea0b067e313ea17c
metindokmen <kindmemento@gmail.com>
2024-01-06 00:16:46 +0300

    implement point data class

--- algorithms/k-means_clustering.kt
+++ algorithms/k-means_clustering.kt
@@ -4,3 +4,12 @@
 // Imagine you're analyzing cutomer data for a marketing campaign.
 // You want to segment customers into distinct groups based on similarities in their purchasing behavior, demographics, or other relevant characteristics.
 // The K-Means algorithm can cluster these customers into groups (or 'clusters') that exhibit similar traits, helping tailor marketing strategies to different customer segments.
+
+import kotlin.math.sqrt
+import kotlin.random.Random
+
+data class Point(val x: Double, val y: Double) {
+    fun distance(other: Point): Double {
+        return sqrt((x - other.x).pow(2) + (y - other.y).pow(2))
+    }
+}
