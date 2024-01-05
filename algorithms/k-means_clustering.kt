7d16c8c0d3a9a0b2c0c2c5ab17f92bfb948c5c31
metindokmen <kindmemento@gmail.com>
2024-01-06 00:14:38 +0300

    explain k-means clustering algorithm in comments

---
+++ algorithms/k-means_clustering.kt
@@ -0,0 +1,2 @@
+// K-Means Clustering is an unsupervised machine learning algorithm that groups data into 'k' numbers of clusters.
+// The algorithm initializes 'k' centroids randomly, then iteratively assigns each data point to the nearest centroid and recomputes the centroid of each cluster until convergence or for a fixed number of iterations.
