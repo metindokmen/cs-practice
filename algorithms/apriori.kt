ed1d5c9a2f55ea1ddc0025d50018122937e5d755
metindokmen <kindmemento@gmail.com>
2024-01-05 14:19:56 +0300

    explain apriori algorithm in comments

---
+++ algorithms/apriori.kt
@@ -0,0 +1,3 @@
+// The Apriori Algorithm is used for mining frequent itemsets and learning association rules in large databses.
+// It's based on the principle that a subset of a frequent itemset must also be a frequent itemset.
+// The algorithm iteratively identifies the frequent individual items in the database and extends them to larger itemsets as long as those itemsets appear sufficiently often in the database.
