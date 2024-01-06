99cbde020b78b2f93b0d59a6f31ca3d4ba785df2
metindokmen <kindmemento@gmail.com>
2024-01-07 00:18:41 +0300

    give real life example for bayes naive classifier algorithm

--- algorithms/naive_bayes_classifier.kt
+++ algorithms/naive_bayes_classifier.kt
@@ -1,2 +1,7 @@
 // The Naive bayes classifier is a probabilistic machine learning model based on Bayes' Theorem with the "naive" assumption of conditional independence between every pair of features given the value of the class variable.
 // Despite its simplicity, Naive Bayes can outperform more complex models, especially in text classification.
+
+// Imagine you're developing a spam filter for an email application.
+// You need a way to classify incoming emails into "spam" or "not spam" based on their content.
+// The Naive Bayes Classifier is ideal for this task.
+// It uses the probabilities of words appearing in spam and non-spam emails to calculate the likelihood of a new email being in one of these categories.
