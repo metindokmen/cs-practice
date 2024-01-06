45fa1a7eb35033c233b464187a037f8fa3eef55a
metindokmen <kindmemento@gmail.com>
2024-01-07 00:18:28 +0300

    explain bayes naive classifier in comments

---
+++ algorithms/naive_bayes_classifier.kt
@@ -0,0 +1,2 @@
+// The Naive bayes classifier is a probabilistic machine learning model based on Bayes' Theorem with the "naive" assumption of conditional independence between every pair of features given the value of the class variable.
+// Despite its simplicity, Naive Bayes can outperform more complex models, especially in text classification.
