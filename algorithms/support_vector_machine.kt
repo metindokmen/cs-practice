78046b5b16f987e909d45f8fdf79d59a56201e8b
metindokmen <kindmemento@gmail.com>
2024-01-09 00:38:02 +0300

    explain support vector machine algorithm in comments

---
+++ algorithms/support_vector_machine.kt
@@ -0,0 +1,2 @@
+// SVM is a supervised learning algorithm that finds the hyperplane in an N-dimensional space (N is the number of features) that distinctly classifies the data points.
+// It works by maximizing the margin between different classes and can handle both linear and non-linear classification tasks through the use of different kernel functions.
