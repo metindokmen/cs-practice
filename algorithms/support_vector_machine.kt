fd332dfe610662bf1b55cea9ec9c4d7f4bc0c91f
metindokmen <kindmemento@gmail.com>
2024-01-09 00:41:09 +0300

    include the necessary dependency in comments, import from said dependency, add data class email

--- algorithms/support_vector_machine.kt
+++ algorithms/support_vector_machine.kt
@@ -5,3 +5,12 @@
 // The SVM algorithm can be applied to create a decision boundary that effectively separates spam and non-spam emails
 // in a high-dimensional space based on features such as the frequency of certain words or patterns.
 // SVM is widely used in text and image classification tasks.
+
+// The following dependency should be added to the build.gradle file of the project:
+// implementation 'com.github.austinv11:SVM:1.4.0'
+
+import com.austinv11.ssvm.SSVM
+import com.austinv11.ssvm.libsvm.svm_node
+import com.austinv11.ssvm.libsvm.svm_problem
+
+data class Email(val wordFrequency1: Double, val wordFrequency2: Double, val label: Int)
