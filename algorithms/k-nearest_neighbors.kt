556007b218c80c1cfc07af9010f9a81def805ba9
metindokmen <kindmemento@gmail.com>
2024-01-08 04:04:39 +0300

    give real life example for k-nearest neighbors algorihtm

--- algorithms/k-nearest_neighbors.kt
+++ algorithms/k-nearest_neighbors.kt
@@ -1,2 +1,6 @@
 // KNN is a simple and effective algorithm based on the principle that similar data points are close to each other in the feature space.
 // It classifies a new data point or predicts its value by considering the majority class or average value of its k-nearest neighbors.
+
+// Imagine you are working on a movie recommendation system.
+// Users who liked a certain movie are potential candidates for similar movie recommendations.
+// The K-Nearest Neighbors algorithm can identify movies liked by users with similar preferences, helping to recommend movies based on the preferences of their "neighbors".
