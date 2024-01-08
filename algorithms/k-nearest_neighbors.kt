72ab34f4126f3f9143fec05650f9dd987b51a762
metindokmen <kindmemento@gmail.com>
2024-01-08 04:10:21 +0300

    add main fun, set list of movies, set new movie, init classifier, predict genre and print result to standard output

--- algorithms/k-nearest_neighbors.kt
+++ algorithms/k-nearest_neighbors.kt
@@ -21,3 +21,21 @@ class KNNClassifier(private val k: Int, private val movies: List<Movie>) {
             .take(k)
     }
 }
+
+fun main() {
+    val movies = listOf(
+        Movie("Movie1", "Action", 4.5),
+        Movie("Movie2", "Drama", 3.8),
+        Movie("Movie3", "Action", 4.2),
+        Movie("Movie4", "Comedy", 3.9),
+        Movie("Movie5", "Drama", 4.1),
+        Movie("Movie6", "Comedy", 3.7),
+    )
+
+    val newMovie = Movie("NewMovie", "Unknown", 4.0)
+
+    val knnClassifier = KNNClassifier(k = 3, movies = movies)
+    val predictedGenre = knnClassifier.classify(newMovie)
+
+    println("Predicted Genre for ${newMovie.genre}: $predictedGenre")
+}
