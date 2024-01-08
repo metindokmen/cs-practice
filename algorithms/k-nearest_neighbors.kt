ff03aabb5755d78957ee8c141442627553ec3dd8
metindokmen <kindmemento@gmail.com>
2024-01-08 04:08:00 +0300

    add KNNClassifier class with methods classify and findKNearestNeighbors

--- algorithms/k-nearest_neighbors.kt
+++ algorithms/k-nearest_neighbors.kt
@@ -6,3 +6,18 @@
 // The K-Nearest Neighbors algorithm can identify movies liked by users with similar preferences, helping to recommend movies based on the preferences of their "neighbors".
 
 data class Movie(val name: String, val genre: String, val rating: Double)
+
+class KNNClassifier(private val k: Int, private val movies: List<Movie>) {
+    fun classify(newMovie: Movie): String {
+        val neighbors = findKNearestNeighbors(newMovie)
+        val majorityGenre = neighbors.groupingBy { it.genre }.eachCount().maxByOrNull { it.value }?.key
+        return majorityGenre ?: "Unknown"
+    }
+
+    private fun findKNearestNeighbors(newMovie: Movie): List<Movie> {
+        return movies
+            .filter { it != newMovie } // Exclude the new movie itself
+            .sortedBy { it.rating } // Assume lower rating indicates closer similarity
+            .take(k)
+    }
+}
