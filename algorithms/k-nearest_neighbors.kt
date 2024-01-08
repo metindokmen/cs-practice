eda0ceb7634d0830f8e02bc89b16d8253c2e177c
metindokmen <kindmemento@gmail.com>
2024-01-08 04:03:34 +0300

    explain k-nearest neighbors algorithm in comments

---
+++ algorithms/k-nearest_neighbors.kt
@@ -0,0 +1,2 @@
+// KNN is a simple and effective algorithm based on the principle that similar data points are close to each other in the feature space.
+// It classifies a new data point or predicts its value by considering the majority class or average value of its k-nearest neighbors.
