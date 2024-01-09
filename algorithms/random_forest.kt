65165becfc7eca5201c77e10aef06aa3bbab1644
metindokmen <kindmemento@gmail.com>
2024-01-09 16:13:34 +0300

    explain random forest algorithm in comemnts

---
+++ algorithms/random_forest.kt
@@ -0,0 +1,4 @@
+// Random Forest builds multiple decision trees and merges their predictions to obtain a more stable and accurate result.
+// It introduces randomness during the training process by using bootstrap sampling (randomly selecting subsets of the training data)
+// and geature bagging (randomly selecting subsets of features for each tree).
+// This helps prevent overfitting and enhances the model's generalization capabilities.
