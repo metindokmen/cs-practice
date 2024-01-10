422fc891aad2ebf5cd52005b17613dbeee6fb0d2
metindokmen <kindmemento@gmail.com>
2024-01-10 15:09:32 +0300

    explain principal component analysis algorithm in comments

---
+++ algorithms/principal_component_analysis.kt
@@ -0,0 +1,2 @@
+// PCA aims to transform the original features into a new set of uncorrelated features, known as principal components.
+// These components are ordered by the amount of varianca they capture in the data, allowing you to retain the most relevant information with fewer features.
