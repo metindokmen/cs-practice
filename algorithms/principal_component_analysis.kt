371605f34bbb147bcbe8d4a803cdc352fae6b5d4
metindokmen <kindmemento@gmail.com>
2024-01-10 15:12:14 +0300

    include necessary dependency in comments, import it, implement data class data point

--- algorithms/principal_component_analysis.kt
+++ algorithms/principal_component_analysis.kt
@@ -4,3 +4,10 @@
 // Imagine you have a dataset with numerous features, and you want to reduce its dimensionality while retaining as much relevant information as possible.
 // PCA can be applied to identify the principal components, which are new features that capture the most significant variations in the data.
 // This can be beneficial in various applications, such as image compression, facial recognition, and signal processing.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifeng1:smile-core:2.5.0'
+
+import smile.projection.PCA
+
+data class DataPoint(val feature1: Double, val feature2: Double, val feature3: Double)
