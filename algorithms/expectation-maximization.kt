6c94dd473f0f82db2ae122f0ccd5f15f16ca2901
metindokmen <kindmemento@gmail.com>
2024-01-12 00:33:28 +0300

    give real life example use case for em algorithm

--- algorithms/expectation-maximization.kt
+++ algorithms/expectation-maximization.kt
@@ -1,3 +1,6 @@
 // EM is an iterative algorithm that aims to find the maximum likelihood estimates of parameters in statistical models with missing or incomplete data.
 // It consists of two main steps: the E-step (Expectation) and the M-step (Maximization).
 // In each iteration, the algorithm refines its estimates until convergence.
+
+// Imagine you have a dataset of customer purchases, but some of the purchase amounts are missing.
+// The EM algorithm can be applied to estimate the missing values and identify clusters of customers with similar purchasing patterns.
