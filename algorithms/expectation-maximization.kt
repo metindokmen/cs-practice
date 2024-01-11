a1953600936cdd390fb2efb82994b558b006143b
metindokmen <kindmemento@gmail.com>
2024-01-12 00:32:30 +0300

    explain em algorithm in comments

---
+++ algorithms/expectation-maximization.kt
@@ -0,0 +1,3 @@
+// EM is an iterative algorithm that aims to find the maximum likelihood estimates of parameters in statistical models with missing or incomplete data.
+// It consists of two main steps: the E-step (Expectation) and the M-step (Maximization).
+// In each iteration, the algorithm refines its estimates until convergence.
