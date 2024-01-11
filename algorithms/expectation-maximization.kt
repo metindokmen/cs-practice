e50cc436ff9479208aaecfd33948ef608475d2d0
metindokmen <kindmemento@gmail.com>
2024-01-12 00:35:11 +0300

    add dependency as comment, make imports

--- algorithms/expectation-maximization.kt
+++ algorithms/expectation-maximization.kt
@@ -4,3 +4,13 @@
 
 // Imagine you have a dataset of customer purchases, but some of the purchase amounts are missing.
 // The EM algorithm can be applied to estimate the missing values and identify clusters of customers with similar purchasing patterns.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.GaussianMixture
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
