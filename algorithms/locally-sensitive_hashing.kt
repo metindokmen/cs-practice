88621d7bcc4eb0b47e6bdbedbf75baf4393812b5
metindokmen <kindmemento@gmail.com>
2024-01-14 02:19:57 +0300

    give real life example use case for lsh algorithm

--- algorithms/locally-sensitive_hashing.kt
+++ algorithms/locally-sensitive_hashing.kt
@@ -1,3 +1,6 @@
 // LSH is a method for finding similar items with high probability in high-dimensional data.
 // It hashes input items in a way that similar items map to the same "buckets" with high probability, while dissimilar items map to different buckets.
 // This enables faster search for approximate nearest neighbors.
+
+// Consider a scenario where you have a large dataset of images, and you want to efficiently find similar images.
+// LSH can be applied to hash the images in a way that similar images are likely to have the same hash, enabling quick approximate nearest neighbor searches.
