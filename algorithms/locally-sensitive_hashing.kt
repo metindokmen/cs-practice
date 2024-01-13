239331b20a263513f7fddd1cf463f39865b23079
metindokmen <kindmemento@gmail.com>
2024-01-14 02:19:04 +0300

    explain lsh in comments

---
+++ algorithms/locally-sensitive_hashing.kt
@@ -0,0 +1,3 @@
+// LSH is a method for finding similar items with high probability in high-dimensional data.
+// It hashes input items in a way that similar items map to the same "buckets" with high probability, while dissimilar items map to different buckets.
+// This enables faster search for approximate nearest neighbors.
