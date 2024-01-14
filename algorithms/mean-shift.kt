afbf896ac82706e159dde359c4388e1420b546e2
metindokmen <kindmemento@gmail.com>
2024-01-15 01:07:41 +0300

    explain mean-shift algorithm in comments

---
+++ algorithms/mean-shift.kt
@@ -0,0 +1,3 @@
+// Mean-Shift is a mode-seeking algorithm that seeks modes or peaks in the data density.
+// It iteratively shifts each data point toward the mean of the data points within a local neighborhood until convergence.
+// The resulting clusters are centered around high-density regions.
