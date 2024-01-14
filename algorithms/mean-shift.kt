fa59c0d93b89c7ad8c0a3e4bfb5e520d9b0ab775
metindokmen <kindmemento@gmail.com>
2024-01-15 01:09:05 +0300

    give real life example use case for mean-shift algorithm

--- algorithms/mean-shift.kt
+++ algorithms/mean-shift.kt
@@ -1,3 +1,6 @@
 // Mean-Shift is a mode-seeking algorithm that seeks modes or peaks in the data density.
 // It iteratively shifts each data point toward the mean of the data points within a local neighborhood until convergence.
 // The resulting clusters are centered around high-density regions.
+
+// Imagine you have a dataset representing customer locations, and you want to identify clusters of high-density regions to target specific marketing strategies.
+// Mean-Shift can be applied to discover these clusters based on the density of data points.
