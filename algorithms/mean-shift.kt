6769fc4b6a08e31ad0f18ed33af205e1315e009c
metindokmen <kindmemento@gmail.com>
2024-01-15 01:10:14 +0300

    add necessary dependency in comments and make imports

--- algorithms/mean-shift.kt
+++ algorithms/mean-shift.kt
@@ -4,3 +4,13 @@
 
 // Imagine you have a dataset representing customer locations, and you want to identify clusters of high-density regions to target specific marketing strategies.
 // Mean-Shift can be applied to discover these clusters based on the density of data points.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.MeanShift
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
