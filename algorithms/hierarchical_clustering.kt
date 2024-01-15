ba04f6641917657b69062fe117ae576aae4550ba
metindokmen <kindmemento@gmail.com>
2024-01-16 01:21:47 +0300

    add necessary dependency as comment, make imports

--- algorithms/hierarchical_clustering.kt
+++ algorithms/hierarchical_clustering.kt
@@ -5,3 +5,15 @@
 
 // Imagine you have a dataset of customer purchase history, and you want to group customers based on their purchasing behavior.
 // Hierarchical clustering can help identify different levels of similarity among customers, forming a hierarchical structure of clusters.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.linkage.CompleteLinkage
+import smile.clustering.linkage.Linkage
+import smile.clustering.linkage.WardLinkage
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
