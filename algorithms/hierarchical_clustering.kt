3124ea7b78c4d682b906c327f03ca062213ba41b
metindokmen <kindmemento@gmail.com>
2024-01-16 01:19:45 +0300

    explain hierarchical clustering in comments

---
+++ algorithms/hierarchical_clustering.kt
@@ -0,0 +1,4 @@
+// Hierarchical Clustering builds a tree of clusters.
+// It can be agglomerative (bottom-up) or divisive (top-down).
+// In the agglomerative approach, each data point starts as a separate cluster, and pairs of clusters are successively merged based on their similarity.
+// In the divisive approach, all data points begin in one cluster, and the algorithm recursively splits clusters.
