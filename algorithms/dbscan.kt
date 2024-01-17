5db86d1f1ab222a99206e867809b5cc380af85f4
metindokmen <kindmemento@gmail.com>
2024-01-18 01:44:03 +0300

    explain dbscan algorithm in comments

---
+++ algorithms/dbscan.kt
@@ -0,0 +1,3 @@
+// DBSCAN (Density-Based Spatial Clustering of Applications with Noise) Algorithm groups together data points that are close to each other
+// and have a sufficient number of neighboring points, forming dense regions.
+// It classifies points as core, border, or noise based on their density.
