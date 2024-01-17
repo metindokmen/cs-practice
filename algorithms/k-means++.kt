9e44279e3c39c59a92ea28c51a87ff0807fe7e4c
metindokmen <kindmemento@gmail.com>
2024-01-17 14:52:52 +0300

    add necessary dependency as comment, make imports

--- algorithms/k-means++.kt
+++ algorithms/k-means++.kt
@@ -4,3 +4,13 @@
 
 // Imagine you have a dataset of customer transactions, and you want to identify natural groupings of customers based on their purchase behavior.
 // K-Means++ can be applied to efficiently initialize the centroids, improving the convergence speed and quality of the clusters.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.KMeans
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
