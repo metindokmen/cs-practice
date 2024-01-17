074042bc7759e7f61d7b0315b5c131ce6ff73149
metindokmen <kindmemento@gmail.com>
2024-01-17 14:49:38 +0300

    explain k-means++ algorithm in comments

---
+++ algorithms/k-means++.kt
@@ -0,0 +1,3 @@
+// K-Means++ is an enhancement to the K-Means algorithm for centroid initialization.
+// Instead of randomly selecting the initial centroids, K-Means++ uses a probabilistic approach to choose centroids that are likely to be far from each other.
+// This helps in achieving better convergence and avoiding poor local optima.
