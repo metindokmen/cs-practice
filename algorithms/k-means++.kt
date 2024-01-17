6af1eba15b9a7c7f75106509acdea833a58ae386
metindokmen <kindmemento@gmail.com>
2024-01-17 14:53:06 +0300

    implement kMeansPlusPlus class

--- algorithms/k-means++.kt
+++ algorithms/k-means++.kt
@@ -14,3 +14,17 @@ import smile.data.AttributeDataset
 import smile.data.NominalAttribute
 import smile.data.parser.DelimitedTextParser
 import java.io.File
+
+class KMeansPlusPlusExample(private val datasetFile: String, private val numClusters: Int) {
+    fun performKMeansPlusPlus(): KMeans {
+        val parser = DelimitedTextParser().apply {
+            delimiter = ","
+            // Assuming the dataset has appropriate column names and attributes
+        }
+
+        val data: AttributeDataset = parser.parse(File(datasetFile))
+        val x = data.toArray()
+
+        return KMeans.fit(x, numClusters, true) // Use KMeans.fit(x, numClusters, true) for K-Means++
+    }
+}
