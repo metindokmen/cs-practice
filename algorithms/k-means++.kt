04f2e9076b8c77db3104553dd7f510e0d06dddf1
metindokmen <kindmemento@gmail.com>
2024-01-17 14:51:23 +0300

    give real life example use case for k-means++ algorithm

--- algorithms/k-means++.kt
+++ algorithms/k-means++.kt
@@ -1,3 +1,6 @@
 // K-Means++ is an enhancement to the K-Means algorithm for centroid initialization.
 // Instead of randomly selecting the initial centroids, K-Means++ uses a probabilistic approach to choose centroids that are likely to be far from each other.
 // This helps in achieving better convergence and avoiding poor local optima.
+
+// Imagine you have a dataset of customer transactions, and you want to identify natural groupings of customers based on their purchase behavior.
+// K-Means++ can be applied to efficiently initialize the centroids, improving the convergence speed and quality of the clusters.
