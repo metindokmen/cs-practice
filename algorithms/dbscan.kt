0e617160124cb122a3724226c58f34eb12bf175c
metindokmen <kindmemento@gmail.com>
2024-01-18 01:45:21 +0300

    add necessary dependency as comment, make imports

--- algorithms/dbscan.kt
+++ algorithms/dbscan.kt
@@ -4,3 +4,13 @@
 
 // Consider a geographical dataset of mobile users, and you want to identify clusters of users who frequently visit certain locations.
 // DBSCAN can help identify dense regions representing significant user activity.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.DBSCAN
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
