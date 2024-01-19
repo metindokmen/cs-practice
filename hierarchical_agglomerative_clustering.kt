bfd592966d59643c4e74f0006c55afd26dcb7a5a
metindokmen <kindmemento@gmail.com>
2024-01-19 16:46:57 +0300

    explain hac in comments

---
+++ hierarchical_agglomerative_clustering.kt
@@ -0,0 +1,2 @@
+// HAC starts with each data point as a separate cluster and iteratively merges the closest clusters until only one cluster remains.
+// The result is a tree-like structure called a dendrogram, representing the hierarchy of cluster relationships.
