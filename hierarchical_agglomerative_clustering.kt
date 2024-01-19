ac0994266155c2840c25f066b80fa338ddf3d537
metindokmen <kindmemento@gmail.com>
2024-01-19 16:47:17 +0300

    give real life example use case for hac

--- hierarchical_agglomerative_clustering.kt
+++ hierarchical_agglomerative_clustering.kt
@@ -1,2 +1,5 @@
 // HAC starts with each data point as a separate cluster and iteratively merges the closest clusters until only one cluster remains.
 // The result is a tree-like structure called a dendrogram, representing the hierarchy of cluster relationships.
+
+// Imagine you have a dataset of customer purchase histories, and you want to identify groups of products that are frequently bought together.
+// HAC can be applied to create a hierarchy of product clusters, capturing relationships at different levels of granularity.
