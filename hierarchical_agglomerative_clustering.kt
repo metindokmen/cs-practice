5502ef475d4a428bd8a24d3b11f08e01834fec5b
metindokmen <kindmemento@gmail.com>
2024-01-19 16:47:51 +0300

    add dependency, make imports

--- hierarchical_agglomerative_clustering.kt
+++ hierarchical_agglomerative_clustering.kt
@@ -3,3 +3,14 @@
 
 // Imagine you have a dataset of customer purchase histories, and you want to identify groups of products that are frequently bought together.
 // HAC can be applied to create a hierarchy of product clusters, capturing relationships at different levels of granularity.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
+
+import smile.clustering.HierarchicalClustering
+import smile.data.Attribute
+import smile.data.AttributeDataset
+import smile.data.NominalAttribute
+import smile.data.parser.DelimitedTextParser
+import java.io.File
+
