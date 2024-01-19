0d3c097db710472b5d7efade4b5521455f3f9975
metindokmen <kindmemento@gmail.com>
2024-01-20 00:58:56 +0300

    explain optics algorithm in comments

---
+++ optics.kt
@@ -0,0 +1,2 @@
+// OPTICS is a density-based clustering algorithm that extends DBSCAN by introducing an ordering of points based on their reachability distance.
+// It produces a reachability plot, allowing users to explore different cluster structures and densities.
