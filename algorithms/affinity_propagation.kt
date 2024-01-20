ddded3cfba7341a7dc9401118c6ac647f13a736c
metindokmen <kindmemento@gmail.com>
2024-01-21 00:45:07 +0300

    give real life example use case for affinity propagation algorithm

--- algorithms/affinity_propagation.kt
+++ algorithms/affinity_propagation.kt
@@ -1,2 +1,5 @@
 // Affinity Propagation is a clustering algorithm that does not require specifying the number of clusters beforehand.
 // It considers each data point as a potential exemplar and iteratively updates messages between data points to determine the most suitable exemplars.
+
+// Consider a social network where individuals interact with each other.
+// Affinity Propagation can be applied to identify key influencers or individuals who serve as exemplars, representing different social groups.
