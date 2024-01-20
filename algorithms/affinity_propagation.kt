417282f5a7230d359b52df8e05b1d22ec0945b3c
metindokmen <kindmemento@gmail.com>
2024-01-21 00:45:48 +0300

    add necessary dependency as comment

--- algorithms/affinity_propagation.kt
+++ algorithms/affinity_propagation.kt
@@ -3,3 +3,6 @@
 
 // Consider a social network where individuals interact with each other.
 // Affinity Propagation can be applied to identify key influencers or individuals who serve as exemplars, representing different social groups.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'com.github.haifengl:smile-core:2.5.0'
