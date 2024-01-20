d6457a2eeea1be3d5e6bf58576864171cf599dc4
metindokmen <kindmemento@gmail.com>
2024-01-21 00:44:22 +0300

    explain affinity propagation algorithm in comments

---
+++ algorithms/affinity_propagation.kt
@@ -0,0 +1,2 @@
+// Affinity Propagation is a clustering algorithm that does not require specifying the number of clusters beforehand.
+// It considers each data point as a potential exemplar and iteratively updates messages between data points to determine the most suitable exemplars.
