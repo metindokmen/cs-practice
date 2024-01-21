22a2919230becd830f0bb58a2b9f4824690ee85e
metindokmen <kindmemento@gmail.com>
2024-01-22 01:46:05 +0300

    explain birch algorihtm in comment

---
+++ algorithms/birch.kt
@@ -0,0 +1,2 @@
+// bIRCH (Balanced Iterative Reducing and Clustering using Hierarchies) Algorithm is a hierarchical clustering algorithm that builds a tree-like structure to represent clusters.
+// It efficiently handles large datasets by incrementally updating the structure as new data points arrive.
