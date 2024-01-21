788e1291041be343138a23a883604ae3b94816f5
metindokmen <kindmemento@gmail.com>
2024-01-22 01:47:25 +0300

    add necesary dependency as comment

--- algorithms/birch.kt
+++ algorithms/birch.kt
@@ -3,3 +3,6 @@
 
 // Imagine you have a massive dataset of customer transactions in an e-commerce platform.
 // BIRCH can be applied to group customers based on their purchasing behavior, enabling targeted marketing strategies.
+
+// The folllowing dependency is to be added to the build.gradle file:
+// implementation 'org.apache.commons:commons-math3:3.6.1'
