93704a2bb1a7fcf56829cc1e0a09117177344e74
metindokmen <kindmemento@gmail.com>
2024-01-23 00:19:51 +0300

    add necessary dependency as comment

--- algorithms/agglomerative_hierarchical_clustering.kt
+++ algorithms/agglomerative_hierarchical_clustering.kt
@@ -3,3 +3,6 @@
 
 // Imagine you have a dataset of customer purchase histories.
 // Agglomerative Hierarchical Clustering can be applied to group similar customers into clusters based on their buying patterns, creating a hierarchy of customer segments.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'org.apache.commons:commons-math3:3.6.1'
