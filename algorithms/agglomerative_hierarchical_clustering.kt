e362c510b7546e7ef80a4f3d42e3a4cc6b82a313
metindokmen <kindmemento@gmail.com>
2024-01-23 00:19:13 +0300

    give real life example use case

--- algorithms/agglomerative_hierarchical_clustering.kt
+++ algorithms/agglomerative_hierarchical_clustering.kt
@@ -1,2 +1,5 @@
 // Agglomerative Hierarchical Clustering starts with each data point as a separate cluster and merges the closest clusters iteratively until only one cluster remains.
 // The result is a dendrogram representing the hierarchy of clusters.
+
+// Imagine you have a dataset of customer purchase histories.
+// Agglomerative Hierarchical Clustering can be applied to group similar customers into clusters based on their buying patterns, creating a hierarchy of customer segments.
