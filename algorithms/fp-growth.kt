54123578ec3212ee4d9a6dd67ac36d80569cfd63
metindokmen <kindmemento@gmail.com>
2024-01-24 01:54:46 +0300

    explain fp growth algorithm in comments

---
+++ algorithms/fp-growth.kt
@@ -0,0 +1,2 @@
+// FP-Growth (Frequent Pattern growth) is an algorithm for mining frequent itemsets without generating candidate sets explicitly.
+// It uses a data structure called an FP-tree (Frequent Pattern tree) to efficiently mine frequent itemsets.
