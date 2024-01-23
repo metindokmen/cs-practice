253c013bc1e8c888eae6bd0958b5ce7df0867517
metindokmen <kindmemento@gmail.com>
2024-01-23 12:56:26 +0300

    give real life example use case for k-medoids algorihtm

--- algorithms/k-medoids.kt
+++ algorithms/k-medoids.kt
@@ -1,2 +1,6 @@
 // K-Medoids is an extension of K-Means that uses actual data points as medoids (cluster representatives).
 // It minimizes the sum of dissimilarities (e.g. distance or dissimilarity measure) between data points and their respective medoids.
+
+// Consider a delivery service managing a fleet of vehicles.
+// K-Medoids can be applied to cluster vehicle locations, selecting the most centrally located vehicle within each cluster as a representative,
+// facilitating efficient route planning.
