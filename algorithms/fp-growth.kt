819123aa88ec0f024854f95d0242959db367a5f9
metindokmen <kindmemento@gmail.com>
2024-01-24 01:55:34 +0300

    give real life example use case for fp-growth algorithm

--- algorithms/fp-growth.kt
+++ algorithms/fp-growth.kt
@@ -1,2 +1,5 @@
 // FP-Growth (Frequent Pattern growth) is an algorithm for mining frequent itemsets without generating candidate sets explicitly.
 // It uses a data structure called an FP-tree (Frequent Pattern tree) to efficiently mine frequent itemsets.
+
+// Consider a retail business analyzing customer purchase data.
+// The FP-growth algorithm can identify frequent itemsets, helping the business understand which products are often purchased together.
