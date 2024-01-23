59030ce112c52da83b39a40cb3f365c7d438f760
metindokmen <kindmemento@gmail.com>
2024-01-23 12:51:58 +0300

    explain k-medoids algorithm in comments

---
+++ algorithms/k-medoids.kt
@@ -0,0 +1,2 @@
+// K-Medoids is an extension of K-Means that uses actual data points as medoids (cluster representatives).
+// It minimizes the sum of dissimilarities (e.g. distance or dissimilarity measure) between data points and their respective medoids.
