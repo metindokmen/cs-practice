c0d23894ec39e610d651977e97c6d11384bb6daa
metindokmen <kindmemento@gmail.com>
2024-01-25 15:18:15 +0300

    explain lowess algorithm in comments

---
+++ algorithms/lowess.kt
@@ -0,0 +1,2 @@
+// LOWESS fits multiple local weighted regressions to the data, assigning different weights to data points based on their proximity to the point being smoothed.
+// It is particularly useful for capturing trends in noisy datasets.
