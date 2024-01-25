d9e90489a92ed6c606f7f0c1d3ae2717e453c26b
metindokmen <kindmemento@gmail.com>
2024-01-26 02:26:08 +0300

    give real life example use case for earth movers distance algorithm

--- algorithms/earth_movers_distance.kt
+++ algorithms/earth_movers_distance.kt
@@ -1,1 +1,3 @@
 // EMD measures the minimum amount of work needed to transform one distribution into another, considering the "cost" of moving material from one point to another.
+
+// Consider comparing the color distribution of two images. EMD can be applied to measure how much effort (distance) it would take to transform the color distribution of one image into another.
