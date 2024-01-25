daabbd4504f2dd3c7e491f5461988a320d27d958
metindokmen <kindmemento@gmail.com>
2024-01-25 15:22:23 +0300

    implement lowess example class with method performLowess

--- algorithms/lowess.kt
+++ algorithms/lowess.kt
@@ -3,3 +3,18 @@
 
 // Consider monitoring the daily temperature changes.
 // LOWESS can be applied to smooth the temperature data, providing a clearer trend by reducing noise caused by daily fluctuations.
+
+class LowessExample(private val xValues: DoubleArray, private val yValues: DoubleArray, private val bandwith: Double) {
+
+    fun performLowess(): DoubleArray {
+        val smoothedValues = DoubleArray(xValues.size)
+
+        for (i in xValues.indices) {
+            val x = xValues[i]
+            smoothedValues[i] = locallyWeightedRegression(x, xValues, yValues, bandwith)
+        }
+
+        return smoothedValues
+    }
+
+}
