6ba82cc5545bfed176f60095d2ac9a2654841053
metindokmen <kindmemento@gmail.com>
2024-01-26 02:26:38 +0300

    add dependency as comment

--- algorithms/earth_movers_distance.kt
+++ algorithms/earth_movers_distance.kt
@@ -1,3 +1,6 @@
 // EMD measures the minimum amount of work needed to transform one distribution into another, considering the "cost" of moving material from one point to another.
 
 // Consider comparing the color distribution of two images. EMD can be applied to measure how much effort (distance) it would take to transform the color distribution of one image into another.
+
+// The following dependency is to be added to the build.gradle file:
+// implementation 'org.apache.commons:commons-math3:3.6.1'
