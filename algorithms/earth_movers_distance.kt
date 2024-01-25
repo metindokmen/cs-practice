b9755f772f524a53d17fa228cc01180991e72f29
metindokmen <kindmemento@gmail.com>
2024-01-26 02:26:46 +0300

    make imports

--- algorithms/earth_movers_distance.kt
+++ algorithms/earth_movers_distance.kt
@@ -4,3 +4,21 @@
 
 // The following dependency is to be added to the build.gradle file:
 // implementation 'org.apache.commons:commons-math3:3.6.1'
+
+import org.apache.commons.math3.analysis.function.Abs
+import org.apache.commons.math3.analysis.function.Multiply
+import org.apache.commons.math3.analysis.function.Pow
+import org.apache.commons.math3.analysis.integration.SimpsonIntegrator
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction
+import org.apache.commons.math3.linear.RealMatrix
+import org.apache.commons.math3.linear.SparseRealMatrix
+import org.apache.commons.math3.optim.MaxIter
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType
+import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer.Simplex
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer.SimplexFactory
+import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizer.SimplexOptimizerBuilder
+import org.apache.commons.math3.util.FastMath
+import kotlin.math.abs
+import kotlin.math.min
