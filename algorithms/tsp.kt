119040c0509be72a9b808291654cb6974dc04588
metindokmen <kindmemento@gmail.com>
2024-01-27 01:22:20 +0300

    explain tsp in comments

---
+++ algorithms/tsp.kt
@@ -0,0 +1,2 @@
+// The TSP involves finding the shortest possible route that visits a given set of cities and returns to the original city.
+// Genetic Algorithms mimic the process of natural selection to evolve a population of potential solutions toward an optimal or near-optimal solution.
