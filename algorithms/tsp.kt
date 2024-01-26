8ec59c21d58f266568a351b1b97c1e7a6391fcd9
metindokmen <kindmemento@gmail.com>
2024-01-27 01:23:35 +0300

    give real life example use case for tsp

--- algorithms/tsp.kt
+++ algorithms/tsp.kt
@@ -1,2 +1,5 @@
 // The TSP involves finding the shortest possible route that visits a given set of cities and returns to the original city.
 // Genetic Algorithms mimic the process of natural selection to evolve a population of potential solutions toward an optimal or near-optimal solution.
+
+// Consider a delivery person aiming to minimize travel distance while visiting a set of locations.
+// The TSP with Genetic Algorithms can provide a near-optimal route for efficient deliveries.
