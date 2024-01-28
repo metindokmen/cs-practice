ab23ef7bf287cf4e8a16556b879f69f363d33253
metindokmen <kindmemento@gmail.com>
2024-01-28 16:36:18 +0300

    explain kargers algorithm in comments

---
+++ algorithms/kargers_min_cut.kt
@@ -0,0 +1,3 @@
+// Karger's algorithm repeatedly contracts random edges in the graph until only two vertices remain.
+// The remaining vertices represent the two parts of the cut.
+// The algorithm has a high probability of finding the correct minimum cut.
