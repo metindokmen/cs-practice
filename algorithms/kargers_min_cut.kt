b21f7532eb87c003a29dd258a563d59aae5ddd44
metindokmen <kindmemento@gmail.com>
2024-01-28 16:37:12 +0300

    give real life example use case for kargers algorithm

--- algorithms/kargers_min_cut.kt
+++ algorithms/kargers_min_cut.kt
@@ -1,3 +1,6 @@
 // Karger's algorithm repeatedly contracts random edges in the graph until only two vertices remain.
 // The remaining vertices represent the two parts of the cut.
 // The algorithm has a high probability of finding the correct minimum cut.
+
+// Consider a telecommunication network where you want to find the minimum number of links that need to be removed to disconnect the network into two parts.
+// Karger's algorithm can help identify this minimum cut efficiently.
