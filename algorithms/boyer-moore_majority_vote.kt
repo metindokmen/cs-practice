83b107028b1ae58b62d727aae675354e4d6d76d4
metindokmen <kindmemento@gmail.com>
2024-01-29 17:26:01 +0300

    implement algorithm

--- algorithms/boyer-moore_majority_vote.kt
+++ algorithms/boyer-moore_majority_vote.kt
@@ -4,3 +4,30 @@
 
 // Imagine you are organizing a voting process where people are voting for different candidates.
 // Boyer-Moore Majority Vote Algorithm can help identify the candidate who received more than half of the total votes, simplifying the process of determining the winner.
+
+fun majorityElement(nums: IntArray): Int {
+    var candidate = 0 // Potential majority element
+    var count = 0 // Counter for the potential majority element
+
+    // Find potential candidate for the majority element
+    for (num in nums) {
+        if (count == 0) {
+            candidate = num
+            count++
+        } else if (candidate == num) {
+            count++
+        } else {
+            count--
+        }
+    }
+
+    // Verify if the candidate is the majority element
+    count = 0
+    for (num in nums) {
+        if (num == candidate) {
+            count++
+        }
+    }
+
+    return if (count > nums.size / 2) candidate else -1 // Return majority element or -1 if not found
+}
