c25db40ccccf253508028f4a64b9d132b8d5d0ba
metindokmen <kindmemento@gmail.com>
2024-02-06 00:27:24 +0300

    explain ternary search tree algorithm in comments

---
+++ algorithms/ternary_search_tree.kt
@@ -0,0 +1,5 @@
+// A Ternary Search Tree is a special type of trie data structure where each node has three children:
+// one for characters less than the current node's character,
+// one for characters equal to the current node's character,
+// and one for characters greater than the current node's character.
+// This structure allows for efficient storage and retrieval of strings.
