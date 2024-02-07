d1b67eb4281de56a3c760a8b0bea655d7c9bd1bc
metindokmen <kindmemento@gmail.com>
2024-02-07 23:47:07 +0300

    give real life example use case for r-tree

--- algorithms/r-tree.kt
+++ algorithms/r-tree.kt
@@ -2,3 +2,7 @@
 // Each node in an R-tree represents a bounding box that encompasses a group of spatial objects.
 // The root node represents the entire space, and as you traverse down the tree, the bounding boxes become more specific.
 // Each leaf node contains references to actual spatial objects.
+
+// Consider a mapping application where you want to efficiently find all restaurants within a certain area on the map.
+// The application needs to handle queries like "find all restaurants within this bounding box."
+// An R-tree can efficiently index the spatial data of restaurants' locations and quickly answer such queries.
