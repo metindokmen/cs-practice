060a552121ba7e2621185fe2dc58a53c0fd6dbd5
metindokmen <kindmemento@gmail.com>
2024-02-08 00:20:49 +0300

    explain hopcroft-karp algorithm in comments

---
+++ algorithms/hopcroft-karp.kt
@@ -0,0 +1,2 @@
+// The Hopcroft-Karp Algorithm is an efficient algorithm for finding the maximum cardinality matching in a bipartite graph.
+// It's an improvement over the Ford-Fulkerson Algorithm for this specific problem.
