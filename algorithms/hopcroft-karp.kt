246da37cd4d05415c98a17a0e88cc047c07c877f
metindokmen <kindmemento@gmail.com>
2024-02-08 00:21:21 +0300

    give real life example use case for hopcroft-karp algorithm

--- algorithms/hopcroft-karp.kt
+++ algorithms/hopcroft-karp.kt
@@ -1,2 +1,7 @@
 // The Hopcroft-Karp Algorithm is an efficient algorithm for finding the maximum cardinality matching in a bipartite graph.
 // It's an improvement over the Ford-Fulkerson Algorithm for this specific problem.
+
+// Imagine a scenario where you have a set of applicants and a set of job positions,
+// and you want to match applicants to job positions such that each job position is filled with a suitable applicant.
+// The Hopcroft-Karp Algorithm can efficiently find the maximum number of such matches in a bipartite graph,
+// ensuring that every job position is assigned to exactly one applicant.
