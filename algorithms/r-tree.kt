90c90041514a30e3870d327dfb132a8521ea1d51
metindokmen <kindmemento@gmail.com>
2024-02-07 23:46:48 +0300

    explain r-tree in comments

---
+++ algorithms/r-tree.kt
@@ -0,0 +1,4 @@
+// An R-tree is a tree data structure optimized for indexing spatial information in multi-dimensional space.
+// Each node in an R-tree represents a bounding box that encompasses a group of spatial objects.
+// The root node represents the entire space, and as you traverse down the tree, the bounding boxes become more specific.
+// Each leaf node contains references to actual spatial objects.
