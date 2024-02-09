e8e1c3aa1745f9869f1ce123e9f62184dd7f3c05
metindokmen <kindmemento@gmail.com>
2024-02-09 18:16:33 +0300

    explain b-tree in comments

---
+++ algorithms/b-tree.kt
@@ -0,0 +1,2 @@
+// A B-tree is a self-balancing tree data structure that maintains sorted data and allows for efficient insertion, deletion, and searching operations.
+// It is optimized for systems that read and write large blocks of data, makint it ideal for use in databases and file systems.
