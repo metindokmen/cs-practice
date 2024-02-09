3a2108379ceda7dfa538ae8a5ef02603ba448ead
metindokmen <kindmemento@gmail.com>
2024-02-09 18:18:20 +0300

    implement btreenode class with insertNonFull method

--- algorithms/b-tree.kt
+++ algorithms/b-tree.kt
@@ -4,3 +4,30 @@
 // Imagine you're designing a database management system for a large-scale e-commerce platform.
 // Each product entry needs to be stored efficiently to enable fast searches, insertions, and deletions.
 // The B-tree algorithm can efficiently organize and manage the product data, ensuring quick access to information even with millions of products.
+
+class BTreeNode(private val t: Int, var leaf: Boolean = true) {
+    val keys = mutableListOf<Int>()
+    val children = mutableListOf<BTreeNode>()
+
+    fun insertNonFull(key: Int) {
+        var i = keys.size - 1
+        if (leaf) {
+            while (i >= 0 && keys[i] > key) {
+                keys[i + 1] = keys[i]
+                i--
+            }
+            keys[i + 1] = key
+        } else {
+            while (i >= 0 && keys[i] > key) {
+                i--
+            }
+            if (children[i + 1].keys.size == 2 * t - 1) {
+                splitChild(i + 1, children[i + 1])
+                if (keys[i + 1] < key) {
+                    i++
+                }
+            }
+            children[i + 1].insertNonFull(key)
+        }
+    }
+}
