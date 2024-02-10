9f43bd84fc31f4989360b1edd74c384a58e46468
metindokmen <kindmemento@gmail.com>
2024-02-10 14:56:06 +0300

    explain aes in comments

---
+++ algorithms/rijndael_encryption.kt
@@ -0,0 +1,3 @@
+// Rijndael Encryption Algorithm, commonly known as Advanced Encryption Standard (AES), operates on blocks of data, with key sizes of 128, 192, or 256 bits.
+// It consists of several rounds of substitution, permutation, and linear transformation operations.
+// These operations are applied in a specific sequence to transform plaintext into ciphertext, and vice versa, using the same key.
