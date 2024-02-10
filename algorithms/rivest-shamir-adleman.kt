e1bd60d23e752fe0e9b6162164fde226c051fa99
metindokmen <kindmemento@gmail.com>
2024-02-11 01:16:37 +0300

    explain rsa in comments

---
+++ algorithms/rivest-shamir-adleman.kt
@@ -0,0 +1,3 @@
+// RSA relies on the mathematical properties of large prime numbers and their factorization.
+// It involves generating a public-private key pair, where the public key is used for encryption and the private key for decryption.
+// The security of RSA is based on the difficulty of factoring the product of two large prime numbers.
