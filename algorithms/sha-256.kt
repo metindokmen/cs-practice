d3b44335a1b5a79eb996c66f07c7c15a1737a907
kindmemento <kindmemento@gmail.com>
2024-02-12 23:41:47 +0300

    implement sha256 algorithm

--- algorithms/sha-256.kt
+++ algorithms/sha-256.kt
@@ -5,3 +5,11 @@
 // Consider a scenario where you want to securely store user passwords in a database.
 // Instead of storing passwords in plain text, you can use SHA-256 to generate hash values of passwords before storing them.
 // This way, even if the database is compromised, attackers cannot retrieve the original passwords from the hash values.
+
+import java.security.MessageDigest
+
+fun sha256(input: String): String {
+    val digest = MessageDigest.getInstance("SHA-256")
+    val hashBytes = digest.digest(input.toByteArray())
+    return hashBytes.joinToString("") { "%02x".format(it) }
+}
