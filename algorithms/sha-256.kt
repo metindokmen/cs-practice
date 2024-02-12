c72f327a3486cefb5334db4c7793e304bbfd350a
kindmemento <kindmemento@gmail.com>
2024-02-12 23:41:07 +0300

    explain sha-256

---
+++ algorithms/sha-256.kt
@@ -0,0 +1,3 @@
+// SHA-256 operates by taking an input message of arbitrary length and producing a fixed-size (256-bit) hash value.
+// It uses a series of modular arithmetic and bitwise operations to process blocks of the input message and generate the hash value.
+// SHA-256 is designed to be collision-resistant, meaning it's computationally infeasible to find two different inputs that produce the same hash output.
