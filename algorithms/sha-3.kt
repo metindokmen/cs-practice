965ebe2e0bb530c7fb5b47222c552cc9a7f02735
kindmemento <kindmemento@gmail.com>
2024-02-13 15:56:19 +0300

    explain sha-3 algorithm in comments

---
+++ algorithms/sha-3.kt
@@ -0,0 +1,3 @@
+// SHA-3 is based on the Keccak algorithm, which was selected through a public competition organized by NIST (National Institute of Standards and Technology).
+// It operates on input data of arbitrary length and produces a fixed-size hash value.
+// SHA-3 uses a sponge construction and permutation-based compression function to process input data.
