6e6e7320537b9b0863cf6822b13c16f7e6a6783b
kindmemento <kindmemento@gmail.com>
2024-02-15 14:26:22 +0300

    explain svd algorithm in comments

---
+++ algorithms/singular_value_decomposition.kt
@@ -0,0 +1,7 @@
+// SVD is a matrix factorization technique that decomposes a matrix A into three matrices U, Σ, and V^T, such that:
+
+// A=UΣV^T
+
+// U is an m x m orthogonal matrix containing the left singular vectors.
+// Σ is an m x n diagonal matrix containing the singular values in descending order.
+// V^T is an n×n orthogonal matrix containing the right singular vectors.
