b99bda1e8a39949f28b105f0cf028912bfa417bf
kindmemento <kindmemento@gmail.com>
2024-02-16 17:47:52 +0300

    give real life example use case for smith-waterman algorithm

--- algorithms/smith-waterman.kt
+++ algorithms/smith-waterman.kt
@@ -1,2 +1,5 @@
 // The Smith-Waterman algorithm is an extension of the Needleman-Wunsch algorithm for global sequence alignment.
 // It finds the optimal local alignment between two sequences by maximizing a similarity score and allowing gaps (insertions and deletions) within the sequences.
+
+// Consider you have two DNA sequences and you want to find the optimal alignment between them, considering mutations, insertions, and deletions.
+// The Smith-Waterman algorithm can help you find the best local alignment, highlighting the regions of similarity and dissimilarity between the sequences.
