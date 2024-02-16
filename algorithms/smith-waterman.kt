af2b14783f009c9f1a8216b8a77f3df1aa6c8427
kindmemento <kindmemento@gmail.com>
2024-02-16 17:46:58 +0300

    explain smith-waterman in comments

---
+++ algorithms/smith-waterman.kt
@@ -0,0 +1,2 @@
+// The Smith-Waterman algorithm is an extension of the Needleman-Wunsch algorithm for global sequence alignment.
+// It finds the optimal local alignment between two sequences by maximizing a similarity score and allowing gaps (insertions and deletions) within the sequences.
