8530ec3d31d3150b45bfba27d15df28eb4a0b5be
kindmemento <kindmemento@gmail.com>
2024-02-17 15:20:14 +0300

    implement data class fibonacciNode with generic type

--- data_structures/fibonacci_heap.kt
+++ data_structures/fibonacci_heap.kt
@@ -18,3 +18,5 @@
 
 // Consider you have a system where tasks have different priorities, and you need to efficiently manage them.
 // The Fibonacci Heap can be used to implement a priority queue efficiently, allowing for quick insertion, deletion, and modification of tasks with different priorities.
+
+data class FibonacciNode<T>(var key: Int, val value: T, var parent: FibonacciNode<T>? = null, var child: FibonacciNode<T>? = null, var left: FibonacciNode<T>? = null, var right: FibonacciNode<T>? = null, var degree: Int = 0, var marked: Boolean = false)
