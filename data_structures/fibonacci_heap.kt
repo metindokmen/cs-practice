40a68d3c56c8e4079c9e9f0060cd3c494b66d321
kindmemento <kindmemento@gmail.com>
2024-02-17 15:15:41 +0300

    explain fibonacci heap data structure in comments

---
+++ data_structures/fibonacci_heap.kt
@@ -0,0 +1,3 @@
+// A Fibonacci Heap is a collection of rooted trees that satisfy the min-heap property.
+// Each tree in the heap is a minimum heap, meaning the key of a parent node is less than or equal to the keys of its children.
+// Additionally, it maintains a pointer to the minimum node in the heap.
