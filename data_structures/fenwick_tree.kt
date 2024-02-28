4b969077a83363c92a46cc5988b3c7c062ccdb19
kindmemento <kindmemento@gmail.com>
2024-02-28 23:38:30 +0300

    explain operations

--- data_structures/fenwick_tree.kt
+++ data_structures/fenwick_tree.kt
@@ -2,3 +2,10 @@
 
 // 1. Structure: A Binary Indexed Tree is represented as an array of size 'n+1', where 'n' is the number of elements in the input sequence.
 // Each element of the array stores the cumulative sum of a specific range of elements in the input sequence.
+
+// 2. Operations:
+// Construction: The Binary Indexed Tree is built using a bottom-up approach.
+// Initially, all elements are set to 0. Then, updates are performed to construct the tree efficiently.
+// Update: Updates involve modifying the values in the Binary Indexed Tree array to reflect changes in the input sequence.
+// These updates are performed by traversing the tree using bitwise operations.
+// Query: Queries for prefix sums are efficiently answered by traversing the tree using bitwise operations to compute cumulative sums.
