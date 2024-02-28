030cb054964ee77b72d64acc8c6548d6715a7c4c
kindmemento <kindmemento@gmail.com>
2024-02-28 23:36:28 +0300

    explain the structure

--- data_structures/fenwick_tree.kt
+++ data_structures/fenwick_tree.kt
@@ -1,1 +1,4 @@
 // Binary Indexed Tree, also known as Fenwick Tree, is a data structure that efficiently supports prefix sum queries and updates in a sequence of numbers.
+
+// 1. Structure: A Binary Indexed Tree is represented as an array of size 'n+1', where 'n' is the number of elements in the input sequence.
+// Each element of the array stores the cumulative sum of a specific range of elements in the input sequence.
