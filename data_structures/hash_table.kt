cc52acbea2590a6f567a69facb84e60bd7d41a49
kindmemento <kindmemento@gmail.com>
2024-02-29 10:56:40 +0300

    explain hash table structure

---
+++ data_structures/hash_table.kt
@@ -0,0 +1,5 @@
+// Hash Table Data Structure:
+
+// 1. Structure:
+// A hash table is an array-based data structure that stores key-value pairs.
+// It uses a hash function to map keys to indices in the array, enabling quick access to values associated with keys.
