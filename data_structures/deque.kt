a999ad7fb99fa970a6d121b14a8cf0c5b6d04914
kindmemento <kindmemento@gmail.com>
2024-03-04 23:57:58 +0300

    explain deque data structure

---
+++ data_structures/deque.kt
@@ -0,0 +1,6 @@
+// Deque Data Structure
+
+// 1. Structure: Deque is a linear data structure that supports the following operations:
+// Insertion and Deletion: Elements can be added or removed from both the front and the back.
+// Peek: Retrieving the element at the front or the back without removing it.
+// Size: Getting the number of elements in the deque.
