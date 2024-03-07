1a7e0cf5ad0dcee53f074156706b3e5f5ddb05fd
kindmemento <kindmemento@gmail.com>
2024-03-07 10:39:34 +0300

    explain areas of application

--- algorithms/bit_manipulation.kt
+++ algorithms/bit_manipulation.kt
@@ -3,3 +3,9 @@
 
 // 1. Concept: Bit manipulation involves performing logical operations (AND, OR, XOR, NOT)
 // and bitwise shifts (left shift, right shift) on individual bits within binary representations of numbers.
+
+// 2. Applications:
+// - Flag Manipulation: Efficiently manage boolean flags using individual bits within an integer.
+// - Bitwise Operations: Perform operations like setting, clearing, or toggling specific bits.
+// - Bitwise Arithmetic: Perform arithmetic operations (addition, subtraction, multiplication, division) using bitwise operations.
+// - Bitwise Representation: Represent data compactly and efficiently, such as encoding information in network protocols or file formats.
