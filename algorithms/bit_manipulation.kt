fc4641be042ef203b0dfa3fd0fbc0f2afd4be693
kindmemento <kindmemento@gmail.com>
2024-03-07 10:36:35 +0300

    briefly explain bit manipulation in comments

---
+++ algorithms/bit_manipulation.kt
@@ -0,0 +1,2 @@
+// Bit Manipulation is a technique which involves performing operations directly on individual bits within binary representations of numbers.
+// It's commonly used in various fields like computer science, cryptography, and hardware design.
