b99e01a9f08f94eb768e60a1b1a34bb8016e6f90
kindmemento <kindmemento@gmail.com>
2024-03-08 10:17:19 +0300

    explain radix tree in comments

---
+++ data_structures/radix_tree.kt
@@ -0,0 +1,1 @@
+// Radix Tree (also known as radix trie or compact prefix tree) is a data structure used for storing a set of keys in a memory-efficient manner, particularly in scenarios where keys are strings.
