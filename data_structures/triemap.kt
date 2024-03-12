95ffd3c4a6f72ae2d7f5dc12dc57caade6f785dc
kindmemento <kindmemento@gmail.com>
2024-03-12 10:13:36 +0300

    explain the structure of a triemap

--- data_structures/triemap.kt
+++ data_structures/triemap.kt
@@ -5,3 +5,8 @@
 // TrieMap is a data structure that combines the properties of a trie and a hash map.
 // It organizes keys in a trie-like structure, where each node represents a prefix of the stored keys.
 // At the leaf nodes, key-value paris are stored like in a hash map.
+
+// 2. Structure:
+// - TrieMap consists of nodes, where each node contains a map of child nodes (character to node mapping) and an optinal value associated with the node.
+// - Keys are stored by traversing down the trie, with each character in the key corresponding to a level in the trie.
+// - At leaf nodes, key-value pairs are stored.
