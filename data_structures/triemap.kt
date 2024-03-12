b7e351ffed24dbff4dfa68f9e18544fcc613f691
kindmemento <kindmemento@gmail.com>
2024-03-12 10:12:29 +0300

    explain the concept of a triemap

--- data_structures/triemap.kt
+++ data_structures/triemap.kt
@@ -1,2 +1,7 @@
 // TrieMap is a data structure which is a hybrid of a trie and a hash map.
 // It combines the efficient prefix search capability of a trie with the quick key-value lookup of a hash map.
+
+// 1. Concept:
+// TrieMap is a data structure that combines the properties of a trie and a hash map.
+// It organizes keys in a trie-like structure, where each node represents a prefix of the stored keys.
+// At the leaf nodes, key-value paris are stored like in a hash map.
