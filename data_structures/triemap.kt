1024e055a9b68b4bbaf29966cabd61e08c939fbc
kindmemento <kindmemento@gmail.com>
2024-03-12 10:11:36 +0300

    briefly explain triemap in comments

---
+++ data_structures/triemap.kt
@@ -0,0 +1,2 @@
+// TrieMap is a data structure which is a hybrid of a trie and a hash map.
+// It combines the efficient prefix search capability of a trie with the quick key-value lookup of a hash map.
