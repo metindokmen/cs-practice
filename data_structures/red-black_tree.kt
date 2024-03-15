022b0335260aacbd987ca7dbbc46b98273a9c0a2
kindmemento <kindmemento@gmail.com>
2024-03-15 10:06:03 +0300

    explain red-black tree and its concept

---
+++ data_structures/red-black_tree.kt
@@ -0,0 +1,7 @@
+// The Red-Black Tree is a self-balancing binary search tree that maintains balance during insertions and deletions.
+
+// 1. Concept: A red-black tree is a binary search tree with the following properties:
+// - Each node is colored either red or black.
+// - The root is always black.
+// - No two red nodes can appear consecutively along any path.
+// - Every path from a node to its descendant NIL nodes (external nodes) has the same number of black nodes.
