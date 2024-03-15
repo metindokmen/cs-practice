513a9b6d9f055cdbc742a2c2704ddc3259596237
kindmemento <kindmemento@gmail.com>
2024-03-15 10:09:35 +0300

    add color enum and node data class

--- data_structures/red-black_tree.kt
+++ data_structures/red-black_tree.kt
@@ -14,3 +14,7 @@
 // - Insertion: Add a new node while preserving the red-black properties. This may involve rotations and recoloring to maintain balance.
 // - Deletion: Remove a node from the tree while ensuring the red-black properties are maintained. Similar to insertion, rotations and recoloring may necessary.
 // - Search: Find a node with a given key in the tree, similar to binary search tree operations.
+
+enum class Color { RED, BLACK }
+
+data class Node(var key: Int, var color: Color, var left: Node? = null, var right: Node? = null, var parent: Node? = null)
