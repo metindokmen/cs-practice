f376d7b652af6a35aab55987016a858d1f1fe292
kindmemento <kindmemento@gmail.com>
2024-03-17 21:44:51 +0300

    explain count-min sketch data structure and its concept

---
+++ data_structures/count-min_sketch.kt
@@ -0,0 +1,5 @@
+// The Count-Min Skcetch algorithm is a probabilistic data structure used for approximate counting of events in massive data streams.
+
+// 1. Concept:
+// - The Count-Min Sketch is a probabilistic data structure designed to provide approximate frequency counts of items in a stream of data.
+// - It uses multiple hash functions and an array of counters to store counts.
