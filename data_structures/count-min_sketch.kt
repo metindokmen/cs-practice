ae79f9f6f70b95b563183e1e234c586064aea2d6
kindmemento <kindmemento@gmail.com>
2024-03-17 21:48:20 +0300

    explain operations of cms

--- data_structures/count-min_sketch.kt
+++ data_structures/count-min_sketch.kt
@@ -8,3 +8,7 @@
 // - The CMS consists of a 2-dimensional array of counters, where each row corresponds to a hash function and each column represents a counter.
 // - Multiple hash functions are used to hash each item, and the corresponding counters are updated based on the hashed values.
 // - The size of the array and the number of hash functions determine the accuracy of the frequency estimates.
+
+// 3. Operations:
+// - Increment: When an item is encountered in the stream, its frequency count is incremented in each counter location determined by the hash functions.
+// - Estimate: To estimate the frequency of an item, its hashed values are used to retrieve the counts from the corresponding counter locations, and the minimum count among all locations is considered as the estimated frequency.
