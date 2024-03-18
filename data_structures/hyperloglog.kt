e748174c0e615caf4d29d93128589191098ed5f8
kindmemento <kindmemento@gmail.com>
2024-03-18 09:51:31 +0300

    add hll class, implement add method

--- data_structures/hyperloglog.kt
+++ data_structures/hyperloglog.kt
@@ -11,3 +11,21 @@
 // 3. Operations:
 // - Add: When an element is encountered in the multiset, its hash value is computed, and the leading zero count is updated in the corresponding register.
 // - Estimate: To estimate the cardinality, the harmonic mean of the register values is computed, and a correction factor is applied to account for bias introduced by register collisions.
+
+import kotlin.math.ln
+
+class HyperLogLog(private val precision: Int) {
+    private val registers: IntArray = IntArray(1 shl precision)
+
+    init {
+        require(precision in 4..16) { "Precision must be in the range [4, 16]" }
+    }
+
+    fun add(element: String) {
+        val hash = element.hashCode()
+        val index = hash ushr (32 - precision)
+        val leadingZeros = Integer.numberOfLeadingZeros(hash shl precision)
+        registers[index] = maxOf(registers[index], leadingZeros)
+    }
+
+}
