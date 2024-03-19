9a8881bde7f9419c8c71d9a3f0a4e8d56f2e75fa
kindmemento <kindmemento@gmail.com>
2024-03-19 10:06:19 +0300

    explain fibonacci search and its concept in comments

---
+++ algorithms/fibonacci_search.kt
@@ -0,0 +1,5 @@
+// Fibonacci Search is an algorithm which efficiently finds the position of a target value in a sorted array.
+
+// 1. Concept:
+// - Fibonacci Search is an efficient search algorithm similar to binary search, but it divides the array into Fibonacci sequence-sized subarrays instead of halves.
+// - It works best for uniformly distributed arrays.
