08def68047a1b28626b1b68c97766327319aeac5
kindmemento <kindmemento@gmail.com>
2024-03-20 10:29:50 +0300

    explain and give real life example for floyd tortoise and hare algorithm

---
+++ algorithms/floyd_tortoise_and_hare.kt
@@ -0,0 +1,6 @@
+// Floyd's Tortoise and Hare algorithm, also known as cycle detection algorithm,
+// is used to detect cycles in a linked list efficiently without using any additional data structure.
+
+// Suppose you have a circular race track where racers run around the track.
+// Some racers might be faster than others, and eventually, they may meet or overlap if the track is circular.
+// The Floyd's Tortoise and Hare algorithm can be used to detect if any two racers ever meet on the track.
