5c6b36293c41120b00b8f6ef1f5b5ae62af757e4
kindmemento <kindmemento@gmail.com>
2024-03-20 10:52:54 +0300

    implement hashCycle function

--- algorithms/floyd_tortoise_and_hare.kt
+++ algorithms/floyd_tortoise_and_hare.kt
@@ -4,3 +4,22 @@
 // Suppose you have a circular race track where racers run around the track.
 // Some racers might be faster than others, and eventually, they may meet or overlap if the track is circular.
 // The Floyd's Tortoise and Hare algorithm can be used to detect if any two racers ever meet on the track.
+
+data class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun hashCycle(head: ListNode?): Boolean {
+    var slow = head
+    var fast = head
+
+    while (fast?.next != null && fast.next?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+
+        if (slow == fast) {
+            return true // Cycle detected
+        }
+        return false // No cycle found
+    }
+}
