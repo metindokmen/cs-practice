bdb6108a50ea0b351f0f3dd0813fa625ac591de4
kindmemento <kindmemento@gmail.com>
2024-03-22 09:56:07 +0300

    explain longest common substring algorithm and give an example use case

---
+++ algorithms/longest_common_substring.kt
@@ -0,0 +1,5 @@
+// Longest Common Substring algorithm is used to find the longest substring that is present in two given strings.
+
+// Suppose you have two texts, one representing a document and another representing a summary.
+// You want to find the longest section of the document that appears in the summary.
+// This could help identify the most important parts that are highlighted in the summary.
