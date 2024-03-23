d9d64ea8534e40e039921131b3a6c68f0fcfc3f1
kindmemento <kindmemento@gmail.com>
2024-03-23 16:53:58 +0300

    explain bfs in comments

---
+++ algorithms/breadth-first_search.java
@@ -0,0 +1,2 @@
+// The Breadth-First Search (BFS) algorithm is used for traversing or searching tree or graph data structures.
+// BFS starts at a specific node of a graph, visits all of its neighbors at the present depth, and then moves on to the nodes at the next depth level.
