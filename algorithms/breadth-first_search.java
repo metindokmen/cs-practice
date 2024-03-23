b3b78525bbb8f3ee28e4882f5c1a7907fa322678
kindmemento <kindmemento@gmail.com>
2024-03-23 16:54:26 +0300

    give real life example use case for bfs

--- algorithms/breadth-first_search.java
+++ algorithms/breadth-first_search.java
@@ -1,2 +1,4 @@
 // The Breadth-First Search (BFS) algorithm is used for traversing or searching tree or graph data structures.
 // BFS starts at a specific node of a graph, visits all of its neighbors at the present depth, and then moves on to the nodes at the next depth level.
+
+// BFS can be applied to find the shortest path in a network, such as the shortest route to travel from one city to another in a transportation network.
