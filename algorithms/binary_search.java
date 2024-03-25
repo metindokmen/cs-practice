9abe0af7f0a85169ed9281fc563aea7868a28cfc
kindmemento <kindmemento@gmail.com>
2024-03-25 11:00:12 +0300

    explain binary search in comments

---
+++ algorithms/binary_search.java
@@ -0,0 +1,3 @@
+// Binary Search is an efficient algorithm for finding a target value within a sorted array.
+// It works by repeatedly dividing in half the portion of the list that could contain the target value, and comparing the target value to the middle element of the subarray.
+// The algorithm eliminates the half in which the target value cannot lie in each iteration.
