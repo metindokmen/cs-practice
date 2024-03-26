735026b809b2147b32899e102b3592c74646f0fd
kindmemento <kindmemento@gmail.com>
2024-03-26 10:16:27 +0300

    explain insertion sort in comments

---
+++ algorithms/insertion_sort.java
@@ -0,0 +1,2 @@
+// Insertion Sort is a simple sorting algorithm that builds the final sorted array one item at a time.
+// It iterates through an input array, removing one element at a time and then inserting it into the correct position in the sorted array.
