dce5efe9b51a509bf15e37b81ee1114cf70fdcdf
kindmemento <kindmemento@gmail.com>
2024-03-26 10:18:09 +0300

    give real life example use case for insertion sort

--- algorithms/insertion_sort.java
+++ algorithms/insertion_sort.java
@@ -1,2 +1,7 @@
 // Insertion Sort is a simple sorting algorithm that builds the final sorted array one item at a time.
 // It iterates through an input array, removing one element at a time and then inserting it into the correct position in the sorted array.
+
+// Imagine you have a deck of cards that are initially unsorted.
+// To sort the deck using insertion sort, you would start with an empty hand and pick up one card at a time from the unsorted deck.
+// As you pick up each card, you compare it to the cards already in your hand and insert it into the correct position.
+// By the time you've picked up all the cards from the unsorted deck, your hand will contain all the cards sorted from lowest to highest.
