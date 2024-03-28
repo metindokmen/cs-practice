e708e4d59d34e347e6cca5f49db664553a56cf36
kindmemento <kindmemento@gmail.com>
2024-03-28 10:28:58 +0300

    explain dfs in comments

---
+++ algorithms/depth_first_search.java
@@ -0,0 +1,2 @@
+// Depth First Search (DFS) is a graph traversal algorithm that explores as far as possible along each branch before backtracking.
+// It is often used to traverse or search through trees or graphs.
