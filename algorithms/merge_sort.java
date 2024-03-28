f1ffad3a9ee9c73020af196ed08c2a19e10a7685
kindmemento <kindmemento@gmail.com>
2024-03-28 11:32:22 +0300

    add printArray helper method and implement main method

--- algorithms/merge_sort.java
+++ algorithms/merge_sort.java
@@ -55,4 +55,38 @@ public class MergeSort {
             k++;
         }
     }
+
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ")
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int n = arr.length;
+
+        System.out.println("Given array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, n - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
 }
