ef27f742efd3e8c9d9e7010780805486e22bc445
kindmemento <kindmemento@gmail.com>
2024-03-29 10:54:14 +0300

    explain quicksort algorithm in comments

---
+++ algorithms/quick_sort.java
@@ -0,0 +1,2 @@
+// Quicksort is a comparison-based sorting algorithm that divides the input array into smallar sub-arrays, then recursively sorts the sub-arrays.
+// Quicksort is often used in practice due to its efficiency and effectiveness on average.
