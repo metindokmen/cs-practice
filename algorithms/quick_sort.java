8082e6052c451114f01b5f1ff5554cb7fe186ffe
kindmemento <kindmemento@gmail.com>
2024-03-29 10:55:11 +0300

    give real life example use case for quicksort algorithm

--- algorithms/quick_sort.java
+++ algorithms/quick_sort.java
@@ -1,2 +1,6 @@
 // Quicksort is a comparison-based sorting algorithm that divides the input array into smallar sub-arrays, then recursively sorts the sub-arrays.
 // Quicksort is often used in practice due to its efficiency and effectiveness on average.
+
+// Consider arranging a group of students in ascending order of their heights.
+// Quicksort would work by selecting a pivot student, arranging students taller than the pivot to the right and shorter to the left.
+// This process continues recursively for the left and right subgroups until the entire group is sorted by height.
