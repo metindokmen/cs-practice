c1cc1c968810d46e9ccccff42770b9fb45f99051
kindmemento <kindmemento@gmail.com>
2024-04-01 09:54:07 +0300

    give real life example use case for selection sort

--- algorithms/selection_sort.java
+++ algorithms/selection_sort.java
@@ -1,2 +1,6 @@
 // Selection Sort is a simple sorting algorithm that works
 // by repeatedly selecting the minimum element from the unsorted portion of the array and moving it to the beginning.
+
+// Suppose you have a deck of cards that are not in any particular order,
+// and you want to arrange them in ascending order based on their values.
+// You can use the selection sort algorithm to achieve this.
