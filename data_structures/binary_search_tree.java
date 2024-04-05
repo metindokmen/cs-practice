a67839f2557ef70c58772dfaebb762c8e09b9a13
kindmemento <kindmemento@gmail.com>
2024-04-05 11:18:04 +0300

    implement TreeNode calss

--- data_structures/binary_search_tree.java
+++ data_structures/binary_search_tree.java
@@ -6,3 +6,15 @@
 // Think of a phonebook where entires are stored alphabetically by last name.
 // Each entry (node) contains a last name (key) and associated information (value).
 // Searching for a specific last name efficiently is analogous to using a BST.
+
+class TreeNode {
+    int key;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
