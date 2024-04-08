9b43753d4776a213ef31ce73adc0eb7a1f3a0262
kindmemento <kindmemento@gmail.com>
2024-04-08 10:51:17 +0300

    give real life example use case for floyd's cycle detection algorithm

--- algorithms/floyd_cycle_detection.java
+++ algorithms/floyd_cycle_detection.java
@@ -4,3 +4,7 @@
 // Floyd's Cycle Detection Algorithm uses two pointers, referred to as slow and fast pointers, to detect cycles in a linked list.
 // The slow pointer moves one step at a time, while the fast pointer moves two steps at a time.
 // If there's a cycle in the linked list, the fast pointer will eventually meet the slow pointer inside the cycle.
+
+// Imagine a scenario where you have a circular track and multiple runners are jogging on this track.
+// Some runners might be faster than others.
+// The challenge is to determine if any two runners will meet each other at some point on the track.
