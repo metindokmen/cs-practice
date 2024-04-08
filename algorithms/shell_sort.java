2439cd2f911863a32b71325fee9950ffa4636b37
kindmemento <kindmemento@gmail.com>
2024-04-08 11:40:34 +0300

    give real life example use case for shell sort

--- algorithms/shell_sort.java
+++ algorithms/shell_sort.java
@@ -2,3 +2,6 @@
 // It uses a sequence of increments (gaps) to sort elements.
 // It starts by sorting pairs of elements far apart from each other, then gradually reduces the gap between elements to perform a final Insertion Sort.
 // The idea is to move elements faster towards their expected position.
+
+// Imagine you have a list of tasks to do, each with a priority level.
+// You want to sort these tasks based on priority but prefer to tackle similar priority tasks together for efficiency.
