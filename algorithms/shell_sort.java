da3c63f515c9272db0b59903ffa7855ba7f52859
kindmemento <kindmemento@gmail.com>
2024-04-08 11:39:50 +0300

    explain shell sort algorithm in comments

---
+++ algorithms/shell_sort.java
@@ -0,0 +1,4 @@
+// Shell Short algorithm is an extension of and an improvement over the Insertion Sort.
+// It uses a sequence of increments (gaps) to sort elements.
+// It starts by sorting pairs of elements far apart from each other, then gradually reduces the gap between elements to perform a final Insertion Sort.
+// The idea is to move elements faster towards their expected position.
