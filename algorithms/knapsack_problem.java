52687ab75fba0dd0c16fd0ecad3035be4573f407
kindmemento <kindmemento@gmail.com>
2024-04-16 10:14:54 +0300

    explain knapsack problem in comments

---
+++ algorithms/knapsack_problem.java
@@ -0,0 +1,2 @@
+// The Knapsack Problem is a classic optimization problem where the goal is to maximize the total value of items selected, given a weight constraint.
+// There are two main variations of the problem: 0/1 Knapsack (where items cannot be divided) and Fractional Knapsack (where items can be taken in fractions).
