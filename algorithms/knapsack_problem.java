03e9564dbdeb7ff3926a49024b8c4a451f93cc58
kindmemento <kindmemento@gmail.com>
2024-04-16 10:19:59 +0300

    give real life example use case for knapsack

--- algorithms/knapsack_problem.java
+++ algorithms/knapsack_problem.java
@@ -7,3 +7,5 @@
 // - If adding the `i`-th item exceeds the current capacity `j`, then `dp[i][j]` is the same as `dp[i-1][j]`.
 // - Otherwise, consider the maximum value between including and excluding the `i`-th item: `dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i])`
 
+// Imagine you are a thief trying to maximize the value of items you can steal from a store, but you have a limited capacity (weight) that you can carry in your knapsack.
+// Each items has a specific weight and value, and the goal is to select a combination of items that maximizes the total value without exceeding the kanpsack's capacity.
