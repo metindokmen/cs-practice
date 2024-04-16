44a73c82b360aedf4a7cde58d4c13152bf45eb90
kindmemento <kindmemento@gmail.com>
2024-04-16 10:22:35 +0300

    implement knapsack method of knapsack public class

--- algorithms/knapsack_problem.java
+++ algorithms/knapsack_problem.java
@@ -9,3 +9,27 @@
 
 // Imagine you are a thief trying to maximize the value of items you can steal from a store, but you have a limited capacity (weight) that you can carry in your knapsack.
 // Each items has a specific weight and value, and the goal is to select a combination of items that maximizes the total value without exceeding the kanpsack's capacity.
+
+public class Knapsack {
+
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int weight = weights[i - 1];
+            int value = values[i - 1];
+            for (int j = 0; j <= capacity; j++) {
+                if (j < weight) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight] + value);
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+
+}
