da60603a5eb56c266bdcf0260c7b40322c832924
kindmemento <kindmemento@gmail.com>
2024-04-25 11:03:46 +0300

    explain bellman-ford algorithm in comments

---
+++ algorithms/bellman-ford.java
@@ -0,0 +1,3 @@
+// The Bellman-Ford algorithm works by iteratively relaxing edges in the graph.
+// It starts by assuming an infinite distance for all vertices except the source vertex, which has a distance of 0.
+// Then, it relaxes each edge repeatedly, updating the shortest known distance to each vertex until no more updates can be made.
