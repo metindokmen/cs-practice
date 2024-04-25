1993a8c1a4157acfcb02d97360bc61ff4f61903c
kindmemento <kindmemento@gmail.com>
2024-04-25 11:05:04 +0300

    give real life example use case for bellman-ford

--- algorithms/bellman-ford.java
+++ algorithms/bellman-ford.java
@@ -1,3 +1,6 @@
 // The Bellman-Ford algorithm works by iteratively relaxing edges in the graph.
 // It starts by assuming an infinite distance for all vertices except the source vertex, which has a distance of 0.
 // Then, it relaxes each edge repeatedly, updating the shortest known distance to each vertex until no more updates can be made.
+
+// Imagine you are managing a transportation network where cities are connected by roads with different travel times.
+// The Bellman-Ford algorithm can be used to determine the shortest travel times from a starting city to all other cities, considering possible delays or negative weights due to traffic conditions.
