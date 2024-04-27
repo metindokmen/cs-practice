fe371ecd5ba3d71651b77e6b529290eeefb03933
kindmemento <kindmemento@gmail.com>
2024-04-27 20:05:28 +0300

    explain a star search algorithm in comments

---
+++ algorithms/a_star_search.java
@@ -0,0 +1,3 @@
+// The A* algorithm combines elements of Dijkstra's algorithm and greedy best-first search.
+// It uses a heuristic function to estimate the cost of reaching the target from any given node.
+// A priority queue (min-heap) is employed to explore the most promising paths first based on a combined cost function.
