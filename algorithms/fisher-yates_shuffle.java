55b68d76cf2ead089e1275ef81d737dd496e823e
kindmemento <kindmemento@gmail.com>
2024-04-29 22:11:51 +0300

    explain fisher yates shuffle algorithm in comments

---
+++ algorithms/fisher-yates_shuffle.java
@@ -0,0 +1,2 @@
+// The Fisher-Yates Shuffle algorithm, also known as the Knuth Shuffle, works by iteratively selecting a random element from the remaining unshuffled elements, and swapping it with the current element.
+// This process ensures that every element is equally likely to end up in any position of the shuffled array.
