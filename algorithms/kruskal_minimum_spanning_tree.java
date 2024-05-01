a45a395e83cf3c30960a33a47058c83604bc6c46
kindmemento <kindmemento@gmail.com>
2024-05-01 16:16:15 +0300

    explain kruskal's minimum spanning tree algorithm and its steps in comments

---
+++ algorithms/kruskal_minimum_spanning_tree.java
@@ -0,0 +1,11 @@
+// Kurskal's algorithm is a greedy algorithm that constructs a Minimum Spanning Tree (MST) for a connected weighted graph.
+// The MST is a subset of the edges of the graph that connects all the vertices together without any cycles and with the minimum total edge weight.
+
+// Steps:
+// 1. Sort Edges: Sort all edges in non-decreasing order of their weight.
+// 2. Initialize MST: Create an empty graph `MST` that will eventually form the MST.
+// 3. Union-Find Data Structure: Use a Union-Find data structure to keep track of connected components.
+// 4. Iterate Over Edges: For each edge (sorted by weight):
+// - If adding the edge to `MST` doesn't form a cycle (i.e., the edge connects two different components), add it to `MST`
+// 5. Final MST: The `MST` will contain the minimum set of edges that connect all the vertices with the minimum total weight.
+
