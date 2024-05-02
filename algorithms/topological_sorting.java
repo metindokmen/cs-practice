845c3186a40b550959262b73f055ab1040d4abe3
kindmemento <kindmemento@gmail.com>
2024-05-02 14:04:42 +0300

    explain topological sorting algorithm in comments

---
+++ algorithms/topological_sorting.java
@@ -0,0 +1,2 @@
+// Topological Sorting is used to order the vertices in a directed graph such that for every directed edge `u -> v`, vertex `u` comex before vertex `v` in the ordering.
+// This ordering is possible if the graph is a Directed Acyclic Graph (DAG).
