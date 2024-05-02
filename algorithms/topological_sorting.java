50a877c1d2c4123c1d46b112f6c4a5865bae0d8d
kindmemento <kindmemento@gmail.com>
2024-05-02 14:06:00 +0300

    give real life example use case for topological sorting algorithm

--- algorithms/topological_sorting.java
+++ algorithms/topological_sorting.java
@@ -1,2 +1,6 @@
 // Topological Sorting is used to order the vertices in a directed graph such that for every directed edge `u -> v`, vertex `u` comex before vertex `v` in the ordering.
 // This ordering is possible if the graph is a Directed Acyclic Graph (DAG).
+
+// Consider the course prerequisites in a university.
+// Each course has dependencies (prerequisites) that must be completed before taking the course.
+// Topological sorting can be used to determine the order of courses to be taken, ensuring that all prerequisites are fulfilled.
