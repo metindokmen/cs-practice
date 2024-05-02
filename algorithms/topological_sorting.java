48d70bfe3748d12a9ce44f4f991d64705819ea44
kindmemento <kindmemento@gmail.com>
2024-05-02 14:09:49 +0300

    implement graph class

--- algorithms/topological_sorting.java
+++ algorithms/topological_sorting.java
@@ -4,3 +4,50 @@
 // Consider the course prerequisites in a university.
 // Each course has dependencies (prerequisites) that must be completed before taking the course.
 // Topological sorting can be used to determine the order of courses to be taken, ensuring that all prerequisites are fulfilled.
+
+import java.util.*;
+
+class Graph {
+    private int vertices;
+    private List<List<Integer>> adjacencyList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adjacencyList.get(u).add(v);
+    }
+
+    public List<Integer> topologicalSort() {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[vertices];
+
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, stack);
+            }
+        }
+
+        List<Integer> topologicalOrder = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            topologicalOrder.add(stack.pop());
+        }
+
+        return topologicalOrder;
+    }
+
+    private void dfs(int vertex, boolean[] visited, Stack<Integer> stack) {
+        visited[vertex] = true;
+        for (int neighbor : adjacencyList.get(vertex)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, visited, stack);
+            }
+        }
+        stack.push(vertex);
+    }
+}
