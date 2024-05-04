dbefe47d24f55d081c6605ccf717e6553409686f
kindmemento <kindmemento@gmail.com>
2024-05-05 02:10:29 +0300

    explain floyd-warshal in comments

---
+++ algorithms/floyd-warshall.java
@@ -0,0 +1,2 @@
+// The Floyd-Warshal algorithm is used to find the shortest path between all pairs of vertices in a weighted graph, including negative edge weights (but no negative cycles).
+// It computes the shortest paths in a dynamic programming manner by considering all possible paths through an intermediate vertex.
