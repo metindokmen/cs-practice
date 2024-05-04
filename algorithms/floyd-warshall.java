65655fecc8f09c31af05bcc1ea0f77c664ec1c59
kindmemento <kindmemento@gmail.com>
2024-05-05 02:11:41 +0300

    give real life example use case for floyd-warshall

--- algorithms/floyd-warshall.java
+++ algorithms/floyd-warshall.java
@@ -1,2 +1,5 @@
 // The Floyd-Warshal algorithm is used to find the shortest path between all pairs of vertices in a weighted graph, including negative edge weights (but no negative cycles).
 // It computes the shortest paths in a dynamic programming manner by considering all possible paths through an intermediate vertex.
+
+// Imagine a scenario where you need to find the shortest path between all pairs of cities in a transportation network, considering different direct routes between cities.
+// The Floyd-Warshall algorithm can be used to compute these shortest paths efficently.
