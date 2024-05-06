c9ed7067238bc2ed0f40d3dd407ce7bf61445a8c
kindmemento <kindmemento@gmail.com>
2024-05-06 15:34:25 +0300

    explain ford-fulkerson algorithm in comments

---
+++ algorithms/ford-fulkerson.java
@@ -0,0 +1,2 @@
+// The Ford-Fulkerson algorithm is used to find the maximum flow in a flow network, which is a directed graph where each edge has a capacity and represents the maximum amount of flow that can pass through it.
+// The algorithm iteratively finds augmenting paths (paths from source to sink with available capacity) and increases the flow along these paths until no more augmenting paths exist.
