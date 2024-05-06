336445a613c850c7b1a579b1aa6bdb76c3bef8dc
kindmemento <kindmemento@gmail.com>
2024-05-06 17:20:56 +0300

    explain shunting yard algorithm in comments

---
+++ algorithms/shunting_yard.java
@@ -0,0 +1,2 @@
+// The Shunting Yard algorithm is used to convert infix expressions (where operators are placed between operands) into postfix notation (operators follow their operands).
+// It uses a stack to temporarily hold operators and performs a series of operations based on the precedence and associativity of operators.
