53444828469a3561144b15ed731756b3f1dac597
kindmemento <kindmemento@gmail.com>
2024-05-06 15:35:30 +0300

    give real life example use case for ford-fullkerson algorithm

--- algorithms/ford-fulkerson.java
+++ algorithms/ford-fulkerson.java
@@ -1,2 +1,5 @@
 // The Ford-Fulkerson algorithm is used to find the maximum flow in a flow network, which is a directed graph where each edge has a capacity and represents the maximum amount of flow that can pass through it.
 // The algorithm iteratively finds augmenting paths (paths from source to sink with available capacity) and increases the flow along these paths until no more augmenting paths exist.
+
+// Consider a scenario where you need to find the maximum flow of traffic through a network of pipes or channels, such as water flowing through a network of pipes or cars travelling through roads.
+// The Ford-Fulkerson algorithm can be used to determine this maximum flow.
