e6fff99e5124f4c67b05eb8ed6e5b14ec1184f43
kindmemento <kindmemento@gmail.com>
2024-05-06 15:38:30 +0300

    add FordFulkerson public class, implement class method bfs

--- algorithms/ford-fulkerson.java
+++ algorithms/ford-fulkerson.java
@@ -3,3 +3,37 @@
 
 // Consider a scenario where you need to find the maximum flow of traffic through a network of pipes or channels, such as water flowing through a network of pipes or cars travelling through roads.
 // The Ford-Fulkerson algorithm can be used to determine this maximum flow.
+
+import java.util.*;
+
+public class FordFulkerson {
+    static final int V = 6; // Number of vertices in the graph
+
+    // Utility function to perform BFS and check if there is a path from source to sink
+    boolean bfs(int[][] residualGraph, int source, int sink, int[] parent) {
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
+        parent[source] = -1;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (int v = 0; v < V; v++) {
+                if (!visited[v] && residualGraph[u][v] > 0) {
+                    if (v == sink) {
+                        parent[v] = u;
+                        return true;
+                    }
+                    queue.add(v);
+                    parent[v] = u;
+                    visited[v] = true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+}
