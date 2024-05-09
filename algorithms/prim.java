69aa6300ddadffa5101fea1678cd3b17985809a1
kindmemento <kindmemento@gmail.com>
2024-05-09 18:37:13 +0300

    explain prim's algorithm in comments

---
+++ algorithms/prim.java
@@ -0,0 +1,3 @@
+// Prim's Algorithm is for minimum spanning tree (MST) construction.
+// Prim's algorithm is used to find the minimum spanning tree of a connected, undirected graph.
+// It's efficient for dense graphs represented using adjacency matrices or priority queues.
