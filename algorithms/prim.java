1d07828fe2ca07efb89b36e095c31aab03669c26
kindmemento <kindmemento@gmail.com>
2024-05-09 18:38:14 +0300

    give real life example use case for prim's algorithm

--- algorithms/prim.java
+++ algorithms/prim.java
@@ -1,3 +1,7 @@
 // Prim's Algorithm is for minimum spanning tree (MST) construction.
 // Prim's algorithm is used to find the minimum spanning tree of a connected, undirected graph.
 // It's efficient for dense graphs represented using adjacency matrices or priority queues.
+
+// Imagine you have a city map where cities are nodes and roads between them are weighted edges.
+// To minimize the total cost of constructing roads between all cities while ensuring all cities are connected,
+// Prim's algorithm can determine the optimal road network.
