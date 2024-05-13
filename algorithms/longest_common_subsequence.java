19da705f0ded97e6d8f0fc46021174655df4b6da
kindmemento <kindmemento@gmail.com>
2024-05-13 18:45:20 +0300

    explain lcs algorithm and give real life example use case

---
+++ algorithms/longest_common_subsequence.java
@@ -0,0 +1,5 @@
+// Longest Common Subsequence is used to find the longest subsequence that is common between two sequences (strings, arrays, etc.)
+
+// Consider two sequences of characters, such as DNA sequence or sentences.
+// The LCS algorithm can determine the longest sequence of characters that appears in both sequences.
+// This can be useful in applications like plagiarism detection, where it's necessary to find similarities between two pieces of text.
