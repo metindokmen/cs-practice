78ecb3c591d78a3443671605c89bb603d1ab28cf
kindmemento <kindmemento@gmail.com>
2024-05-16 15:04:19 +0300

    briefly explain djikstra's use case

---
+++ algorithms/dijkstra's_algorithm.java
@@ -0,0 +1,5 @@
+// You are given a directed or undirected weighted graph with n vertices and m edges.
+// The weights of all edges are non-negative.
+// You are also given a starting vertex s.
+// Below article discusses finding the lenghts of the shortest paths from a starting vertex s to all other vertices, and output the shortest paths themselves.
+
