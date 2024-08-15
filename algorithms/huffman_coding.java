587c3ede6c64cd19fda2d58956e14f65b11a747d
kindmemento <kindmemento@gmail.com>
2024-08-15 03:38:03 +0300

    give real life example use case for huffman coding

--- algorithms/huffman_coding.java
+++ algorithms/huffman_coding.java
@@ -1,2 +1,6 @@
 // Given a set of characters and their frequencies, construct a binary tree where the path from the root to each character is the code for that character.
 // The goal is to minimize the total length of the encoded message.
+
+// Imagine you are sending a large text file over a network where bandwidth is limited.
+// You want to reduce the size of the file without losing any information.
+// Huffman Coding helps by assigning shorter codes to more frequent characters, thus compressing the data efficiently.
