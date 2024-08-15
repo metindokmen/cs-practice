c205b5d6713cc9f38d52d7e8f5381fa077ee538f
kindmemento <kindmemento@gmail.com>
2024-08-15 03:37:12 +0300

    explain huffman coding in comments

---
+++ algorithms/huffman_coding.java
@@ -0,0 +1,2 @@
+// Given a set of characters and their frequencies, construct a binary tree where the path from the root to each character is the code for that character.
+// The goal is to minimize the total length of the encoded message.
