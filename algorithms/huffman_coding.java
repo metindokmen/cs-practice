8115a1c373297551e12f181f748145183c003ae1
kindmemento <kindmemento@gmail.com>
2024-08-15 03:39:46 +0300

    implement HuffmanNode class to represent a character and its frequency

--- algorithms/huffman_coding.java
+++ algorithms/huffman_coding.java
@@ -4,3 +4,21 @@
 // Imagine you are sending a large text file over a network where bandwidth is limited.
 // You want to reduce the size of the file without losing any information.
 // Huffman Coding helps by assigning shorter codes to more frequent characters, thus compressing the data efficiently.
+
+import java.util.PriorityQueue;
+
+class HuffmanNode implements Comparable<HuffmanNode> {
+    char character;
+    int frequency;
+    HuffmanNode left, right;
+
+    HuffmanNode(char character, int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+        left = right = null;
+    }
+
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
+}
