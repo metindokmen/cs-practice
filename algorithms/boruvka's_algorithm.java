32eb08f8c57426c74c50f01c13038e9ee8c4efae
kindmemento <kindmemento@gmail.com>
2024-08-16 23:42:53 +0300

    explain boruvka's algorithm

---
+++ algorithms/boruvka's_algorithm.java
@@ -0,0 +1,3 @@
+// Boruvka's Algorithm is a greedy algorithm used to find the MST of a graph.
+// An MST is a subset of the edges of a connected, edge-weighted graph that connects all vertices
+// without any cycles and with the minimum possible total edge weight.
