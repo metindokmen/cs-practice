acc8953d35062e3abee66332620ff84e9684ca0a
kindmemento <kindmemento@gmail.com>
2024-08-16 23:44:21 +0300

    give real life example for boruvka's algorithm

--- algorithms/boruvka's_algorithm.java
+++ algorithms/boruvka's_algorithm.java
@@ -1,3 +1,6 @@
 // Boruvka's Algorithm is a greedy algorithm used to find the MST of a graph.
 // An MST is a subset of the edges of a connected, edge-weighted graph that connects all vertices
 // without any cycles and with the minimum possible total edge weight.
+
+// Imagine you have several isolated islands (vertices), and your goal is to build bridges (edges) to connect all the islands with the least amount of construction cost.
+// Boruvka's algorithm would help you determine which bridges to build first (starting with the cheapest) to ensure that all islands become connected while minimizing the total cost.
