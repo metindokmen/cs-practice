1cae750017e6f6d1f7ace45c8f2e9a32d4ef4405
kindmemento <kindmemento@gmail.com>
2024-08-17 23:35:42 +0300

    explain union find data structure in comments

---
+++ data_structures/union_find.java
@@ -0,0 +1,5 @@
+// A Disjoin Set Union (DSU) or Union-Find is a data structure that keeps track of a set of elements partitioned into a number of disjoint (non-overlapping) subsets.
+// It suppors two primary operations efficiently:
+
+// 1. Find: Determine the subset a particular element is in.
+// 2. Union: Merge two subsets into a single subset.
