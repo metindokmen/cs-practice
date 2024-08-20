a5400f8541c1c4feb0df283024af49c916fbe9ea
kindmemento <kindmemento@gmail.com>
2024-08-20 06:07:49 +0300

    briefly explain hash maps in comments

---
+++ data_structures/hash_map.java
@@ -0,0 +1,3 @@
+// HashMap is a data structure in computer science that provides a fast and efficient way to store and retrieve data.
+// It's part of the Java Collections Framework and is typically used when you want to associate keys with values,
+// allowing quick lookup, insertion, and deletion of key-value pairs.
