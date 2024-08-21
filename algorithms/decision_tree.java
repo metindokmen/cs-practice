e71765665829ef248d874fd1be15fc4154c35891
kindmemento <kindmemento@gmail.com>
2024-08-21 07:58:38 +0300

    add a real life example use case for decision trees

--- algorithms/decision_tree.java
+++ algorithms/decision_tree.java
@@ -6,3 +6,11 @@
 // - Internal Nodes: Represent features that split data.
 // - Branches: Represent outcomes of feature tests.
 // - Leaf Nodes: Represent decisions or outcomes.
+
+// Imagine you want to decide whether to play tennis based on weather conditions. The features are:
+// - Outlook (Sunny, Overcast, Rainy)
+// - Tempreature (Hot, Mild, Cool)
+// - Humidity (High, Normal)
+// - Wind (Strong, Weak)
+
+// The decision tree can help you decide whether to play tennis by evaluating features.
