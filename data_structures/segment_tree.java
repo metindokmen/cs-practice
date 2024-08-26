9537bbc6e1ab62152a19eeca49c83ddc1d530127
kindmemento <kindmemento@gmail.com>
2024-08-27 00:58:55 +0300

    explain segment tree data structure

---
+++ data_structures/segment_tree.java
@@ -0,0 +1,5 @@
+// A Segment Tree is a data structure used to efficiently perform range queries and updated on an array.
+// It is particularly useful when you need to perform operations like finding the sum, minimum, or maximum over a range of elements,
+// and you also need to update elements frequently.
+// The Segment Tree allows you to do both in logarithmic time, making it a powerful tool for scenarios where both updates and queries need to be handled efficiently.
+
