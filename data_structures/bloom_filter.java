6d3fdc0b7a887f5643034dead1cccaa35b06b57d
kindmemento <kindmemento@gmail.com>
2024-08-30 23:37:08 +0300

    give a real life example use case for bloom filters

--- data_structures/bloom_filter.java
+++ data_structures/bloom_filter.java
@@ -2,3 +2,8 @@
 // It can return false positives but never false negatives.
 // This means it might indicate that an element is in the set even when it's not,
 // but if it says the element is not in the set, you can be sure that it isn't.
+
+// Imagine you are building an email system that needs to quickly check if an incoming email is spam.
+// You maintain a large list of known spam emails and want to quickly check if the sender's email address is in that list.
+// Using a Bloom Filter, you can quickly determine if the email address might be spam.
+// If the Bloom Filter says "no", you can be certain it isn't spam; if it says "yes", you can further investigate or apply more intensive checks.
