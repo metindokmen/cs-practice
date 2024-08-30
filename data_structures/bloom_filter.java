8fdd02f6085fad6fbf6022abdbf44912827c39e0
kindmemento <kindmemento@gmail.com>
2024-08-30 23:34:44 +0300

    explain bloom filter and its shortcomings

---
+++ data_structures/bloom_filter.java
@@ -0,0 +1,4 @@
+// A Bloom Filter is a probabilistic data structure that is used to test whether an element is a member of a set.
+// It can return false positives but never false negatives.
+// This means it might indicate that an element is in the set even when it's not,
+// but if it says the element is not in the set, you can be sure that it isn't.
