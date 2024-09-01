416c054bf416f05ff7a18ca279c83e4ec16ce629
kindmemento <kindmemento@gmail.com>
2024-09-01 20:22:17 +0300

    give a real life example for heaps

--- data_structures/heap.java
+++ data_structures/heap.java
@@ -1,3 +1,7 @@
 // A heap is a special tree-based data structure that satisfies the heap property.
 // In a max heap, for any given node, the value of the node is greater than or equal to the values of its children, and the highest value is at the root.
 // Conversely, in a min heap, the value of the node is less than or equal to the values of its children, with the smallest value at the root.
+
+// imagine you are organizing a series of tasks based on priority, where the most urgent task needs to be addressed first.
+// A max heap could be used to model this sscenario, where the highest-priority task is always at the top of the heap.
+// When you complete the most urgent task, you remove it from the heap, and the next most urgent task moves to the top.
