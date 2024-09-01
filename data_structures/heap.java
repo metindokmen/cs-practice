c0f4ea48425cc81b5f16bb46eb916f892c0cb391
kindmemento <kindmemento@gmail.com>
2024-09-01 20:23:48 +0300

    give use cases of heaps in dsa

--- data_structures/heap.java
+++ data_structures/heap.java
@@ -5,3 +5,8 @@
 // imagine you are organizing a series of tasks based on priority, where the most urgent task needs to be addressed first.
 // A max heap could be used to model this sscenario, where the highest-priority task is always at the top of the heap.
 // When you complete the most urgent task, you remove it from the heap, and the next most urgent task moves to the top.
+
+// Use Cases
+// 1. Priority Queue: Heaps are commonly used to implement priority queues.
+// 2. Scheduling Algorithms: In operating systems, heaps can be used to schedule processes.
+// 3. Graph Algorithms: Heaps are used in algorithms like Dijkstra's shortest path algorithm to efficiently find the minimum distance.
