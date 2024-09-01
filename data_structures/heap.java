0472a28dc9241a19400cf7053038a56c6bdbfe54
kindmemento <kindmemento@gmail.com>
2024-09-01 20:21:01 +0300

    explain heap data structure in comments

---
+++ data_structures/heap.java
@@ -0,0 +1,3 @@
+// A heap is a special tree-based data structure that satisfies the heap property.
+// In a max heap, for any given node, the value of the node is greater than or equal to the values of its children, and the highest value is at the root.
+// Conversely, in a min heap, the value of the node is less than or equal to the values of its children, with the smallest value at the root.
