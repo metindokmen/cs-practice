c8ec377e2e649b4f72e99237261ebd1f2eb025ce
kindmemento <kindmemento@gmail.com>
2024-09-02 13:15:05 +0300

    give a real life example for trie

--- data_structures/trie.java
+++ data_structures/trie.java
@@ -1,3 +1,9 @@
 // A Trie is a tree-like data structure that stores a dynamic set of strings, where the keys are usually strings.
 // It is used to store words in a way that facilitates fast prefix-based searches.
 // Each node in a trie represents a single character, and each path down the tree represents a word.
+
+// Imagine you are designing an autocomplete feature for a search engine.
+// As a user types in the search box, you want to suggest possible completions based on the current prefix.
+// A trie is an ideal data structure for this task because it allows you to quickly find all words that start with the given prefix.
+// For example, if a user types "cat", the trie can quicky suggest "cat", "cater", "catapult" et cetera, based on the stored words.
+
