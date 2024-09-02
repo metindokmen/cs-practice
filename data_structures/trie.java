7b38eefa6318d5f171951d513a41fe0ce86e979b
kindmemento <kindmemento@gmail.com>
2024-09-02 13:13:37 +0300

    explain trie data structure in comments

---
+++ data_structures/trie.java
@@ -0,0 +1,3 @@
+// A Trie is a tree-like data structure that stores a dynamic set of strings, where the keys are usually strings.
+// It is used to store words in a way that facilitates fast prefix-based searches.
+// Each node in a trie represents a single character, and each path down the tree represents a word.
