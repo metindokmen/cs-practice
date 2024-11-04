eb34affbe77b8a35b1cc7452ea598c179dee5e06
kindmemento <kindmemento@gmail.com>
2024-11-04 09:53:53 +0300

    add characteristics of linked lists

--- data_structures/linked_list.kt
+++ data_structures/linked_list.kt
@@ -1,3 +1,7 @@
 // A linked list is a linear data structure that stores data in nodes, which are connected by pointers.
 // Unlike arrays, linked lists are not stored in contiguous memory locations.
 
+// Characteristics of Linked List:
+// - Dynamic: Linked Lists can be easily resized by adding or removing nodes.
+// - Non-contiguous: Nodes are stored in random memory locations and connected by pointers.
+// - Sequential access: Nodes can only be accessed sequentially, starting from the head of the list.
