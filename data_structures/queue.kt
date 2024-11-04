ffdffb54ea768e4997da69ff0ce0ae53eff422cd
kindmemento <kindmemento@gmail.com>
2024-11-04 15:26:36 +0300

    explain queue and its operations

---
+++ data_structures/queue.kt
@@ -0,0 +1,9 @@
+// Queue is a fundamental data structure used for storing and managing data in a specific order.
+// It follows the principle of FIFO (First In, First Out), where the first element added to the queue is the first one to be removed.
+
+// Operations
+// - Enqueue: Adds an element to the rear of the queue.
+// - Dequeue: Removes an element from the front of the queue.
+// - Peek: Retrieves the front element without removing it.
+// - isEmpty: Checks if the queue is empty
+// - isFull: Checks if the queue is full
