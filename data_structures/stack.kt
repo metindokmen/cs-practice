335c496110a2aeb4fefc2ded94890704f6d3b8dc
kindmemento <kindmemento@gmail.com>
2024-11-04 12:10:38 +0300

    implement stack class with its constructors, add stubbed functions for each class method

--- data_structures/stack.kt
+++ data_structures/stack.kt
@@ -13,3 +13,31 @@
 // - Expression evaluation
 // - Backtracking
 // - Undo/redo operations
+
+
+// Node class representing each element in the stack
+data class Node<T>(val value: T, var next: Node<T>? = null)
+
+// Stack class implementing basic stack operations
+class Stack<T> {
+    private var top: Node<T>? = null
+    private var size: Int = 0
+
+    // @TODO: Add an element to the top of the stack
+    fun push() {}
+
+    // @TODO: Remove and return the top element of the stack
+    fun pop() {}
+
+    // @TODO: Return the top element without removing it
+    fun peek() {}
+
+    // @TODO: Return the number of elements in the stack
+    fun size() {}
+
+    // @TODO: Check if the stack is empty
+    fun isEmpty() {}
+
+    // @TODO: Display the stack elements
+    fun display() {}
+}
