b006863ff264ba4b2ad44b0fa4c943039c0720f7
kindmemento <kindmemento@gmail.com>
2024-11-04 09:52:39 +0300

    defina linked lists, highlight distinction from arrays

---
+++ data_structures/linked_list.kt
@@ -0,0 +1,3 @@
+// A linked list is a linear data structure that stores data in nodes, which are connected by pointers.
+// Unlike arrays, linked lists are not stored in contiguous memory locations.
+
