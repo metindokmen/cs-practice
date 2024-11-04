0b0e92744ac660be9388947a3d7a97b0eba9fb73
kindmemento <kindmemento@gmail.com>
2024-11-04 12:03:50 +0300

    list operations of stack

--- data_structures/stack.kt
+++ data_structures/stack.kt
@@ -1,2 +1,9 @@
 // Stack is a linear data structure that follows a particular order in which the operations are performed.
 // The order may be LIFO (Last In First Out) or FILO (First In Last Out).
+
+// Operations:
+// - Push: Adds an element to the top of the stack.
+// - Pop: Removes and returns the element at the top of the stack.
+// - Peek: Returns the element at the top of the stack without removing it.
+// - Size: Returns the number of elements in the stack.
+// - isEmpty: Checks if the stack is empty.
