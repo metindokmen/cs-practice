868ecb93e07a8f06b03c7a01f85f47ca05eab521
kindmemento <kindmemento@gmail.com>
2024-11-05 15:06:14 +0300

    explain tree data structure briefly

---
+++ data_structures/tree.kt
@@ -0,0 +1,2 @@
+// A Tree is a non-linear hierarchical data structure consisting of nodes connected by edges, with a top node called the root and nodes having child nodes.
+// It is used in computer science for organizing data efficiently.
