1359e77002e481eb1199dcb49338b4f69e7bb100
kindmemento <kindmemento@gmail.com>
2024-11-05 15:19:06 +0300

    explain list of traversal methods

--- data_structures/tree.kt
+++ data_structures/tree.kt
@@ -1,2 +1,8 @@
 // A Tree is a non-linear hierarchical data structure consisting of nodes connected by edges, with a top node called the root and nodes having child nodes.
 // It is used in computer science for organizing data efficiently.
+
+// Traversal of Tree
+// Tree traversal methods are used to visit and process nodes in a tree data structure.
+// - In-Order: Visit left subtree, current node, then right subtree.
+// - Pre-Order: Visit current node, left subtree, then right subtree.
+// - Post-Order: Visit left subtree, right subtree, then current node.
