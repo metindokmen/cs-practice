5ab5fcff831d01a803e2d0ea45706822f9b3b097
kindmemento <kindmemento@gmail.com>
2024-11-05 16:32:21 +0300

    define balanced tree

---
+++ data_structures/tree/balanced_tree.kt
@@ -0,0 +1,2 @@
+// A Balanced Tree (also called an AVL Tree) is a self-balancing binary search tree where the heights of the wto child subtrees of any node differ by at most one.
+// This implementation includes insert and rotation methods.
