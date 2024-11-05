0ac1f701ef59eaea15004196f7232b0ba6a48fd9
kindmemento <kindmemento@gmail.com>
2024-11-05 15:27:32 +0300

    list common types of trees

--- data_structures/tree.kt
+++ data_structures/tree.kt
@@ -1,7 +1,7 @@
 // A Tree is a non-linear hierarchical data structure consisting of nodes connected by edges, with a top node called the root and nodes having child nodes.
 // It is used in computer science for organizing data efficiently.
 
-// Traversal of Tree
+// Traversal of Tree:
 // Tree traversal methods are used to visit and process nodes in a tree data structure.
 // - In-Order: Visit left subtree, current node, then right subtree.
 // - Pre-Order: Visit current node, left subtree, then right subtree.
@@ -11,3 +11,12 @@
 // Classification of Trees refer to grouping trees based on certain characteristics or criteria.
 // This can involve categorizing trees based on their balance factor, degree of nodes, ordering properties, etc.
 // Below are some important classification of Tree.
+
+// By Degree: Trees can be classified based on the maximum number of children each node can have.
+// By Ordering: Trees can be classified based on the ordering of nodes and subtrees.
+// By Balance: Trees can be classified based on how well-balanced they are.
+
+// Common Trees
+// By Degree: Binary Tree, Ternary Tree, N-ary Tree
+// By Ordering: Binary Search Tree (BST)
+// By Balance: Balanced Tree, Unbalanced Tree
