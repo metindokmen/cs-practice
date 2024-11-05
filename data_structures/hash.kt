5d83673242e2cbb0403db16bdaa790e22a1b150f
kindmemento <kindmemento@gmail.com>
2024-11-05 13:54:26 +0300

    define hashing

---
+++ data_structures/hash.kt
@@ -0,0 +1,2 @@
+// Hashing is a technique that generates a fixed-size output (hash value) from an input of variable size using mathematical formulas called hash functions.
+// Hashing is used to determine an index or location for storing an item in a data structure, allowing for efficient retrieval and insertion.
