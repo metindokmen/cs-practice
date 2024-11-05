ecd09f13910bbbce532cf28e705d774a1d3ddca0
kindmemento <kindmemento@gmail.com>
2024-11-05 15:20:43 +0300

    explain classification of trees

--- data_structures/tree.kt
+++ data_structures/tree.kt
@@ -6,3 +6,8 @@
 // - In-Order: Visit left subtree, current node, then right subtree.
 // - Pre-Order: Visit current node, left subtree, then right subtree.
 // - Post-Order: Visit left subtree, right subtree, then current node.
+
+// Classification of Trees:
+// Classification of Trees refer to grouping trees based on certain characteristics or criteria.
+// This can involve categorizing trees based on their balance factor, degree of nodes, ordering properties, etc.
+// Below are some important classification of Tree.
