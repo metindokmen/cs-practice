bde7a373d8b419dd30ce63ab7d1be8768a59e671
kindmemento <kindmemento@gmail.com>
2024-11-05 16:33:03 +0300

    add avlTreeNode

--- data_structures/tree/balanced_tree.kt
+++ data_structures/tree/balanced_tree.kt
@@ -1,2 +1,8 @@
 // A Balanced Tree (also called an AVL Tree) is a self-balancing binary search tree where the heights of the wto child subtrees of any node differ by at most one.
 // This implementation includes insert and rotation methods.
+
+class AVLTreeNode(val value: Int) {
+    var left: AVLTreeNode? = null
+    var right: AVLTreeNode? = null
+    var height: Int = 1
+}
