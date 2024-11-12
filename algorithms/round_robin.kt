7d24532786475d16e8dcfdde860bc6c87853b50e
kindmemento <kindmemento@gmail.com>
2024-11-12 19:24:46 +0300

    explain round robin algorithm with a common use case in comments

---
+++ algorithms/round_robin.kt
@@ -0,0 +1,7 @@
+// The Round Robin algorithm is a load-balancing algorithm
+// that distributes requests sequentially among a list of servers,
+// cycling back to the first server once it reaches the last one.
+
+// This approach ensures even distribution of traffic across all servers
+// and is often used in scenarious like reverse-proxy load balancers
+// where each server in a replica set receives rougly the same amount of traffic over time.
